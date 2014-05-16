0008ce6b6532f01f0165e7e7d501755dbc1f72f1
Burcu Dogan <jbd@google.com>
2014-05-16 13:52:09 +0200

    Adding transactions.

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -18,12 +18,18 @@ var requiredScopes = []string{
 }
 
 var (
-	ErrNotFound = errors.New("datastore: no entity with the provided key has been found")
+	ErrRollbackNonTransactional = errors.New("datastore: cannot rollback non-transactional operation")
+	ErrAlreadyRolledBack        = errors.New("datastore: transaction already rolled back")
+	ErrNotFound                 = errors.New("datastore: no entity with the provided key has been found")
 )
 
 type Dataset struct {
-	ID string // project ID, value could be obtained from the Developer Console.
+	defaultTransaction *Transaction
+}
 
+type Transaction struct {
+	id        []byte
+	datasetID string
 	transport http.RoundTripper
 }
 
@@ -39,15 +45,21 @@ func NewDataset(projectID, clientEmail, pemFilename string) (dataset *Dataset, e
 	if err != nil {
 		return
 	}
-	return &Dataset{ID: projectID, transport: conf.NewTransport()}, nil
+	dataset = &Dataset{
+		defaultTransaction: &Transaction{
+			datasetID: projectID,
+			transport: conf.NewTransport(),
+		},
+	}
+	return
 }
 
 func (d *Dataset) NewIncompleteKey(kind string) *Key {
-	return newIncompleteKey(kind, d.ID, "default")
+	return newIncompleteKey(kind, d.defaultTransaction.datasetID, "default")
 }
 
 func (d *Dataset) NewIncompleteKeyWithNs(namespace, kind string) *Key {
-	return newIncompleteKey(kind, d.ID, namespace)
+	return newIncompleteKey(kind, d.defaultTransaction.datasetID, namespace)
 }
 
 func (d *Dataset) NewKey(kind string, ID int64) *Key {
@@ -55,38 +67,19 @@ func (d *Dataset) NewKey(kind string, ID int64) *Key {
 }
 
 func (d *Dataset) NewKeyWithNs(namespace, kind string, ID int64) *Key {
-	return newKey(kind, strconv.FormatInt(ID, 10), ID, d.ID, namespace)
+	return newKey(kind, strconv.FormatInt(ID, 10), ID, d.defaultTransaction.datasetID, namespace)
 }
 
 func (d *Dataset) Get(key *Key, dest interface{}) (err error) {
-	req := &pb.LookupRequest{
-		Key: []*pb.Key{keyToPbKey(key)},
-	}
-	resp := &pb.LookupResponse{}
-	client := gcloud.Client{Transport: d.transport}
-	if err = client.Call(d.newUrl("lookup"), req, resp); err != nil {
-		return
-	}
-	if len(resp.Found) == 0 {
-		return ErrNotFound
-	}
-	entityFromPbEntity(resp.Found[0].Entity, dest)
-	return
+	return d.defaultTransaction.Get(key, dest)
 }
 
-func (d *Dataset) Put(key *Key, src interface{}) (*Key, error) {
-	panic("not yet implemented")
+func (d *Dataset) Put(key *Key, src interface{}) (k *Key, err error) {
+	return d.defaultTransaction.Put(key, src)
 }
 
 func (d *Dataset) Delete(key *Key) (err error) {
-	mode := pb.CommitRequest_NON_TRANSACTIONAL
-	req := &pb.CommitRequest{
-		Mutation: &pb.Mutation{Delete: []*pb.Key{keyToPbKey(key)}},
-		Mode:     &mode,
-	}
-	resp := &pb.CommitResponse{}
-	client := gcloud.Client{Transport: d.transport}
-	return client.Call(d.newUrl("commit"), req, resp)
+	return d.defaultTransaction.Delete(key)
 }
 
 func (d *Dataset) AllocateIDs(namespace, kind string, n int) (keys []*Key, err error) {
@@ -99,8 +92,9 @@ func (d *Dataset) AllocateIDs(namespace, kind string, n int) (keys []*Key, err e
 	}
 	req := &pb.AllocateIdsRequest{Key: incompleteKeys}
 	resp := &pb.AllocateIdsResponse{}
-	client := gcloud.Client{Transport: d.transport}
-	if err = client.Call(d.newUrl("allocateIds"), req, resp); err != nil {
+
+	url := d.defaultTransaction.newUrl("allocateIds")
+	if err = d.defaultTransaction.newClient().Call(url, req, resp); err != nil {
 		return
 	}
 	// TODO(jbd): Return error if response doesn't include enough keys.
@@ -111,16 +105,107 @@ func (d *Dataset) AllocateIDs(namespace, kind string, n int) (keys []*Key, err e
 			created.GetPathElement()[0].GetKind(),
 			strconv.FormatInt(created.GetPathElement()[0].GetId(), 10),
 			created.GetPathElement()[0].GetId(),
-			d.ID,
+			d.defaultTransaction.datasetID,
 			created.GetPartitionId().GetNamespace())
 	}
 	return
 }
 
-func (d *Dataset) RunInTransaction(fn func() error) error {
+func (d *Dataset) NewTransaction() (*Transaction, error) {
+	transaction := &Transaction{
+		transport: d.defaultTransaction.transport,
+		datasetID: d.defaultTransaction.datasetID,
+	}
+
+	req := &pb.BeginTransactionRequest{}
+	resp := &pb.BeginTransactionResponse{}
+	url := d.defaultTransaction.newUrl("beginTransaction")
+	if err := d.defaultTransaction.newClient().Call(url, req, resp); err != nil {
+		return nil, err
+	}
+	transaction.id = resp.GetTransaction()
+	return transaction, nil
+}
+
+func (t *Transaction) IsTransactional() bool {
+	return len(t.id) > 0
+}
+
+func (t *Transaction) Commit() error {
+	if t.IsTransactional() {
+		return nil
+	}
+	req := &pb.CommitRequest{
+		Transaction: t.id,
+	}
+	resp := &pb.CommitResponse{}
+	if err := t.newClient().Call(t.newUrl("commit"), req, resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Transaction) Rollback() error {
+	if t.IsTransactional() {
+		return ErrRollbackNonTransactional
+	}
+	req := &pb.RollbackRequest{
+		Transaction: t.id,
+	}
+	resp := &pb.RollbackResponse{}
+	if err := t.newClient().Call(t.newUrl("rollback"), req, resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Transaction) Get(key *Key, dest interface{}) (err error) {
+	// TODO: add transactional impl
+	req := &pb.LookupRequest{
+		Key: []*pb.Key{keyToPbKey(key)},
+	}
+	resp := &pb.LookupResponse{}
+	if err = t.newClient().Call(t.newUrl("lookup"), req, resp); err != nil {
+		return
+	}
+	if len(resp.Found) == 0 {
+		return ErrNotFound
+	}
+	entityFromPbEntity(resp.Found[0].Entity, dest)
+	return
+}
+
+func (t *Transaction) Put(key *Key, src interface{}) (k *Key, err error) {
+	// TODO: add transactional impl
+	mode := pb.CommitRequest_NON_TRANSACTIONAL
+	req := &pb.CommitRequest{
+		Mode: &mode,
+		Mutation: &pb.Mutation{
+			Upsert: []*pb.Entity{entityToPbEntity(key, src)},
+		},
+	}
+	resp := &pb.CommitResponse{}
+	if err = t.newClient().Call(t.newUrl("commit"), req, resp); err != nil {
+		return
+	}
 	panic("not yet implemented")
 }
 
-func (d *Dataset) newUrl(method string) string {
-	return "https://www.googleapis.com/datastore/v1beta2/datasets/" + d.ID + "/" + method
+func (t *Transaction) Delete(key *Key) (err error) {
+	// TODO: add transactional impl
+	mode := pb.CommitRequest_NON_TRANSACTIONAL
+	req := &pb.CommitRequest{
+		Mutation: &pb.Mutation{Delete: []*pb.Key{keyToPbKey(key)}},
+		Mode:     &mode,
+	}
+	resp := &pb.CommitResponse{}
+	return t.newClient().Call(t.newUrl("commit"), req, resp)
+}
+
+func (t *Transaction) newClient() *gcloud.Client {
+	return &gcloud.Client{Transport: t.transport}
+}
+
+func (t *Transaction) newUrl(method string) string {
+	return "https://www.googleapis.com/datastore/v1beta2/datasets/" + t.datasetID + "/" + method
 }
