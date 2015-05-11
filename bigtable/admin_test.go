ccc330e33f6816fe627dc6dcc6ac0a9cb88f8baa
David Symonds <dsymonds@golang.org>
2015-05-11 11:09:13 +1000

    bigtable: Add admin integration test.
    Change-Id: I710be1bddab75814df1ce79feaa7bfac67cbb6a6

---
+++ bigtable/admin_test.go
@@ -0,0 +1,53 @@
+package bigtable
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/cloud/bigtable/bttest"
+)
+
+func TestAdminIntegration(t *testing.T) {
+	srv, err := bttest.NewServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer srv.Close()
+	t.Logf("bttest.Server running on %s", srv.Addr)
+
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+
+	adminClient, err := NewAdminClient(ctx, "proj", "zone", "cluster",
+		WithCredentials(nil), WithInsecureAddr(srv.Addr))
+	if err != nil {
+		t.Fatalf("NewAdminClient: %v", err)
+	}
+	defer adminClient.Close()
+
+	list := func() []string {
+		tbls, err := adminClient.Tables(ctx)
+		if err != nil {
+			t.Fatalf("Fetching list of tables: %v", err)
+		}
+		sort.Strings(tbls)
+		return tbls
+	}
+	if err := adminClient.CreateTable(ctx, "mytable"); err != nil {
+		t.Fatalf("Creating table: %v", err)
+	}
+	if err := adminClient.CreateTable(ctx, "myothertable"); err != nil {
+		t.Fatalf("Creating table: %v", err)
+	}
+	if got, want := list(), []string{"myothertable", "mytable"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("adminClient.Tables returned %#v, want %#v", got, want)
+	}
+	if err := adminClient.DeleteTable(ctx, "myothertable"); err != nil {
+		t.Fatalf("Deleting table: %v", err)
+	}
+	if got, want := list(), []string{"mytable"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("adminClient.Tables returned %#v, want %#v", got, want)
+	}
+}
