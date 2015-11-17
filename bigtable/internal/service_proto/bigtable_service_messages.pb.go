06f31442d0365efc35cbede4929899c361066cde
David Symonds <dsymonds@golang.org>
2015-11-17 15:39:57 +1100

    bigtable: Update protos to pick up compressed descriptors.
    Change-Id: I6f7a23267474c4e8327c7c61c1d4ff8ffe8f7caf
    Reviewed-on: https://code-review.googlesource.com/3765
    Reviewed-by: Dave Day <djd@golang.org>

--- bigtable/internal/service_proto/bigtable_service_messages.pb.go
+++ bigtable/internal/service_proto/bigtable_service_messages.pb.go
@@ -57,9 +57,10 @@ type ReadRowsRequest struct {
 	NumRowsLimit int64 `protobuf:"varint,7,opt,name=num_rows_limit" json:"num_rows_limit,omitempty"`
 }
 
-func (m *ReadRowsRequest) Reset()         { *m = ReadRowsRequest{} }
-func (m *ReadRowsRequest) String() string { return proto.CompactTextString(m) }
-func (*ReadRowsRequest) ProtoMessage()    {}
+func (m *ReadRowsRequest) Reset()                    { *m = ReadRowsRequest{} }
+func (m *ReadRowsRequest) String() string            { return proto.CompactTextString(m) }
+func (*ReadRowsRequest) ProtoMessage()               {}
+func (*ReadRowsRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
 type isReadRowsRequest_Target interface {
 	isReadRowsRequest_Target()
@@ -163,9 +164,10 @@ type ReadRowsResponse struct {
 	Chunks []*ReadRowsResponse_Chunk `protobuf:"bytes,2,rep,name=chunks" json:"chunks,omitempty"`
 }
 
-func (m *ReadRowsResponse) Reset()         { *m = ReadRowsResponse{} }
-func (m *ReadRowsResponse) String() string { return proto.CompactTextString(m) }
-func (*ReadRowsResponse) ProtoMessage()    {}
+func (m *ReadRowsResponse) Reset()                    { *m = ReadRowsResponse{} }
+func (m *ReadRowsResponse) String() string            { return proto.CompactTextString(m) }
+func (*ReadRowsResponse) ProtoMessage()               {}
+func (*ReadRowsResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *ReadRowsResponse) GetChunks() []*ReadRowsResponse_Chunk {
 	if m != nil {
@@ -184,9 +186,10 @@ type ReadRowsResponse_Chunk struct {
 	Chunk isReadRowsResponse_Chunk_Chunk `protobuf_oneof:"chunk"`
 }
 
-func (m *ReadRowsResponse_Chunk) Reset()         { *m = ReadRowsResponse_Chunk{} }
-func (m *ReadRowsResponse_Chunk) String() string { return proto.CompactTextString(m) }
-func (*ReadRowsResponse_Chunk) ProtoMessage()    {}
+func (m *ReadRowsResponse_Chunk) Reset()                    { *m = ReadRowsResponse_Chunk{} }
+func (m *ReadRowsResponse_Chunk) String() string            { return proto.CompactTextString(m) }
+func (*ReadRowsResponse_Chunk) ProtoMessage()               {}
+func (*ReadRowsResponse_Chunk) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1, 0} }
 
 type isReadRowsResponse_Chunk_Chunk interface {
 	isReadRowsResponse_Chunk_Chunk()
@@ -309,9 +312,10 @@ type SampleRowKeysRequest struct {
 	TableName string `protobuf:"bytes,1,opt,name=table_name" json:"table_name,omitempty"`
 }
 
-func (m *SampleRowKeysRequest) Reset()         { *m = SampleRowKeysRequest{} }
-func (m *SampleRowKeysRequest) String() string { return proto.CompactTextString(m) }
-func (*SampleRowKeysRequest) ProtoMessage()    {}
+func (m *SampleRowKeysRequest) Reset()                    { *m = SampleRowKeysRequest{} }
+func (m *SampleRowKeysRequest) String() string            { return proto.CompactTextString(m) }
+func (*SampleRowKeysRequest) ProtoMessage()               {}
+func (*SampleRowKeysRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 // Response message for BigtableService.SampleRowKeys.
 type SampleRowKeysResponse struct {
@@ -330,9 +334,10 @@ type SampleRowKeysResponse struct {
 	OffsetBytes int64 `protobuf:"varint,2,opt,name=offset_bytes" json:"offset_bytes,omitempty"`
 }
 
-func (m *SampleRowKeysResponse) Reset()         { *m = SampleRowKeysResponse{} }
-func (m *SampleRowKeysResponse) String() string { return proto.CompactTextString(m) }
-func (*SampleRowKeysResponse) ProtoMessage()    {}
+func (m *SampleRowKeysResponse) Reset()                    { *m = SampleRowKeysResponse{} }
+func (m *SampleRowKeysResponse) String() string            { return proto.CompactTextString(m) }
+func (*SampleRowKeysResponse) ProtoMessage()               {}
+func (*SampleRowKeysResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
 // Request message for BigtableService.MutateRow.
 type MutateRowRequest struct {
@@ -346,9 +351,10 @@ type MutateRowRequest struct {
 	Mutations []*google_bigtable_v11.Mutation `protobuf:"bytes,3,rep,name=mutations" json:"mutations,omitempty"`
 }
 
-func (m *MutateRowRequest) Reset()         { *m = MutateRowRequest{} }
-func (m *MutateRowRequest) String() string { return proto.CompactTextString(m) }
-func (*MutateRowRequest) ProtoMessage()    {}
+func (m *MutateRowRequest) Reset()                    { *m = MutateRowRequest{} }
+func (m *MutateRowRequest) String() string            { return proto.CompactTextString(m) }
+func (*MutateRowRequest) ProtoMessage()               {}
+func (*MutateRowRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
 
 func (m *MutateRowRequest) GetMutations() []*google_bigtable_v11.Mutation {
 	if m != nil {
@@ -383,9 +389,10 @@ type CheckAndMutateRowRequest struct {
 	FalseMutations []*google_bigtable_v11.Mutation `protobuf:"bytes,5,rep,name=false_mutations" json:"false_mutations,omitempty"`
 }
 
-func (m *CheckAndMutateRowRequest) Reset()         { *m = CheckAndMutateRowRequest{} }
-func (m *CheckAndMutateRowRequest) String() string { return proto.CompactTextString(m) }
-func (*CheckAndMutateRowRequest) ProtoMessage()    {}
+func (m *CheckAndMutateRowRequest) Reset()                    { *m = CheckAndMutateRowRequest{} }
+func (m *CheckAndMutateRowRequest) String() string            { return proto.CompactTextString(m) }
+func (*CheckAndMutateRowRequest) ProtoMessage()               {}
+func (*CheckAndMutateRowRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{5} }
 
 func (m *CheckAndMutateRowRequest) GetPredicateFilter() *google_bigtable_v11.RowFilter {
 	if m != nil {
@@ -415,9 +422,10 @@ type CheckAndMutateRowResponse struct {
 	PredicateMatched bool `protobuf:"varint,1,opt,name=predicate_matched" json:"predicate_matched,omitempty"`
 }
 
-func (m *CheckAndMutateRowResponse) Reset()         { *m = CheckAndMutateRowResponse{} }
-func (m *CheckAndMutateRowResponse) String() string { return proto.CompactTextString(m) }
-func (*CheckAndMutateRowResponse) ProtoMessage()    {}
+func (m *CheckAndMutateRowResponse) Reset()                    { *m = CheckAndMutateRowResponse{} }
+func (m *CheckAndMutateRowResponse) String() string            { return proto.CompactTextString(m) }
+func (*CheckAndMutateRowResponse) ProtoMessage()               {}
+func (*CheckAndMutateRowResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{6} }
 
 // Request message for BigtableService.ReadModifyWriteRowRequest.
 type ReadModifyWriteRowRequest struct {
@@ -432,13 +440,66 @@ type ReadModifyWriteRowRequest struct {
 	Rules []*google_bigtable_v11.ReadModifyWriteRule `protobuf:"bytes,3,rep,name=rules" json:"rules,omitempty"`
 }
 
-func (m *ReadModifyWriteRowRequest) Reset()         { *m = ReadModifyWriteRowRequest{} }
-func (m *ReadModifyWriteRowRequest) String() string { return proto.CompactTextString(m) }
-func (*ReadModifyWriteRowRequest) ProtoMessage()    {}
+func (m *ReadModifyWriteRowRequest) Reset()                    { *m = ReadModifyWriteRowRequest{} }
+func (m *ReadModifyWriteRowRequest) String() string            { return proto.CompactTextString(m) }
+func (*ReadModifyWriteRowRequest) ProtoMessage()               {}
+func (*ReadModifyWriteRowRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{7} }
 
 func (m *ReadModifyWriteRowRequest) GetRules() []*google_bigtable_v11.ReadModifyWriteRule {
 	if m != nil {
 		return m.Rules
 	}
 	return nil
+}
+
+func init() {
+	proto.RegisterType((*ReadRowsRequest)(nil), "google.bigtable.v1.ReadRowsRequest")
+	proto.RegisterType((*ReadRowsResponse)(nil), "google.bigtable.v1.ReadRowsResponse")
+	proto.RegisterType((*ReadRowsResponse_Chunk)(nil), "google.bigtable.v1.ReadRowsResponse.Chunk")
+	proto.RegisterType((*SampleRowKeysRequest)(nil), "google.bigtable.v1.SampleRowKeysRequest")
+	proto.RegisterType((*SampleRowKeysResponse)(nil), "google.bigtable.v1.SampleRowKeysResponse")
+	proto.RegisterType((*MutateRowRequest)(nil), "google.bigtable.v1.MutateRowRequest")
+	proto.RegisterType((*CheckAndMutateRowRequest)(nil), "google.bigtable.v1.CheckAndMutateRowRequest")
+	proto.RegisterType((*CheckAndMutateRowResponse)(nil), "google.bigtable.v1.CheckAndMutateRowResponse")
+	proto.RegisterType((*ReadModifyWriteRowRequest)(nil), "google.bigtable.v1.ReadModifyWriteRowRequest")
+}
+
+var fileDescriptor0 = []byte{
+	// 574 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x9c, 0x54, 0xd1, 0x6e, 0x12, 0x41,
+	0x14, 0x75, 0x5d, 0xd9, 0xd2, 0x5b, 0x52, 0xda, 0xb1, 0x36, 0x5b, 0x52, 0x8d, 0xd9, 0x17, 0x4d,
+	0x13, 0x97, 0x14, 0xb5, 0x1a, 0x1f, 0x4c, 0xa4, 0x49, 0xd3, 0xc4, 0x90, 0x34, 0xf4, 0xa1, 0x8f,
+	0x64, 0xd8, 0xbd, 0x2c, 0x13, 0x66, 0x77, 0x70, 0x67, 0x96, 0xca, 0x5f, 0xfa, 0x01, 0xbe, 0xfa,
+	0x1f, 0xce, 0x0c, 0x4b, 0x69, 0x10, 0x2a, 0xfa, 0x40, 0x02, 0x67, 0xce, 0xbd, 0xf7, 0x9c, 0x3b,
+	0x87, 0x81, 0x9b, 0x44, 0x88, 0x84, 0x63, 0x98, 0x08, 0x4e, 0xb3, 0x24, 0x14, 0x79, 0xd2, 0x8c,
+	0xb8, 0x28, 0xe2, 0x66, 0x9f, 0x25, 0x8a, 0xf6, 0x39, 0x36, 0x59, 0xa6, 0x30, 0xcf, 0x28, 0x6f,
+	0x4a, 0xcc, 0x27, 0x2c, 0xc2, 0xde, 0x38, 0x17, 0x4a, 0xdc, 0x9d, 0xf7, 0xe6, 0x70, 0x8a, 0x52,
+	0xd2, 0x04, 0x65, 0x68, 0xcf, 0x09, 0x29, 0x1b, 0xcf, 0x79, 0xe1, 0xe4, 0xb4, 0x71, 0xb9, 0xf9,
+	0xb0, 0x98, 0x2a, 0xba, 0x3c, 0xc9, 0x60, 0xb3, 0xee, 0xc1, 0x4f, 0x07, 0xea, 0x5d, 0xa4, 0x71,
+	0x57, 0xdc, 0xca, 0x2e, 0x7e, 0x2b, 0x50, 0x2a, 0x42, 0x00, 0x66, 0xbc, 0x8c, 0xa6, 0xe8, 0x3b,
+	0x2f, 0x9d, 0xd7, 0xdb, 0x64, 0x1f, 0xb6, 0x72, 0x71, 0xdb, 0x1b, 0xe1, 0xd4, 0x7f, 0xac, 0x81,
+	0xda, 0xe5, 0x23, 0x72, 0x0a, 0xdb, 0x06, 0xca, 0xb5, 0x02, 0xf4, 0x5d, 0x0d, 0xee, 0xb4, 0x8e,
+	0xc3, 0x3f, 0xc5, 0x86, 0xba, 0x75, 0xd7, 0x70, 0x74, 0xc9, 0x1b, 0xf0, 0x06, 0x8c, 0x6b, 0x65,
+	0x7e, 0xc5, 0xf2, 0x9f, 0xaf, 0xe1, 0x5f, 0x58, 0x12, 0x79, 0x01, 0x87, 0x94, 0x73, 0x33, 0x43,
+	0x7f, 0xac, 0x23, 0x8e, 0x74, 0xc2, 0xb2, 0xc4, 0xf7, 0x74, 0x79, 0x95, 0x1c, 0xc2, 0x6e, 0x56,
+	0xa4, 0xe6, 0x54, 0xf6, 0x38, 0x4b, 0x99, 0xf2, 0xb7, 0x34, 0xee, 0xb6, 0xab, 0xe0, 0x29, 0x9a,
+	0x27, 0xa8, 0x82, 0x1f, 0x0e, 0xec, 0x2d, 0xec, 0xc9, 0xb1, 0xc8, 0x24, 0x92, 0xfa, 0xc2, 0x8b,
+	0x31, 0x57, 0x23, 0x9f, 0xc0, 0x8b, 0x86, 0x45, 0x36, 0x92, 0xda, 0x9b, 0xab, 0x65, 0x9d, 0xac,
+	0x94, 0xb5, 0xd4, 0x26, 0x3c, 0x37, 0x25, 0x0d, 0x01, 0x15, 0xfb, 0x85, 0xb4, 0xa0, 0x66, 0xba,
+	0x46, 0x42, 0xeb, 0xcc, 0x94, 0xb4, 0xad, 0x77, 0x5a, 0x8d, 0x55, 0xad, 0x2e, 0x68, 0xca, 0xf8,
+	0x54, 0xef, 0xe3, 0xa9, 0x5e, 0x21, 0x4a, 0x54, 0xc6, 0x82, 0xdd, 0x6b, 0x55, 0x83, 0x07, 0x00,
+	0x91, 0x48, 0xb5, 0x1b, 0x8b, 0xba, 0x33, 0xb4, 0xbd, 0x05, 0x15, 0xab, 0x31, 0x38, 0x81, 0x83,
+	0x6b, 0x9a, 0x8e, 0x39, 0x6a, 0x31, 0x5f, 0x71, 0xfa, 0xd0, 0xad, 0x05, 0x9f, 0xe1, 0xd9, 0x12,
+	0x77, 0xdd, 0x0a, 0x0e, 0xa0, 0x26, 0x06, 0x03, 0x23, 0xa5, 0x3f, 0x55, 0x28, 0xad, 0x18, 0x37,
+	0x18, 0xc2, 0x5e, 0xa7, 0x50, 0x54, 0x99, 0xfa, 0x87, 0xd2, 0x51, 0x5f, 0x4a, 0x07, 0x69, 0xc2,
+	0x76, 0x6a, 0x0a, 0x99, 0x9e, 0xa6, 0x2d, 0xb8, 0xeb, 0xb2, 0xd1, 0x29, 0x49, 0xc1, 0x2f, 0x07,
+	0xfc, 0xf3, 0x21, 0x46, 0xa3, 0x2f, 0x59, 0xfc, 0x7f, 0x23, 0x3f, 0xc0, 0xde, 0x38, 0xc7, 0x98,
+	0x45, 0xba, 0xb6, 0x57, 0xa6, 0xcc, 0xdb, 0x24, 0x65, 0xef, 0x60, 0x57, 0xe5, 0x85, 0xfe, 0xdf,
+	0xdd, 0x09, 0x7e, 0xf2, 0x77, 0xc1, 0xe4, 0x3d, 0xd4, 0x07, 0x94, 0xcb, 0xfb, 0x65, 0x95, 0x0d,
+	0x7c, 0x9e, 0xc1, 0xd1, 0x0a, 0x9b, 0xe5, 0xad, 0x1c, 0xc1, 0xfe, 0xc2, 0x42, 0x4a, 0x55, 0x34,
+	0xc4, 0xd8, 0xda, 0xad, 0x06, 0xdf, 0xe1, 0xc8, 0x04, 0xb0, 0x23, 0x62, 0x36, 0x98, 0xde, 0xe4,
+	0xec, 0xdf, 0xf7, 0x73, 0x06, 0x95, 0xbc, 0xe0, 0x38, 0xbf, 0x8e, 0x57, 0xeb, 0x32, 0x7e, 0x7f,
+	0x84, 0xe6, 0xb7, 0x3f, 0xc2, 0xa1, 0x8e, 0xe3, 0x0a, 0x76, 0xfb, 0xb8, 0x5d, 0xfe, 0xb8, 0x9e,
+	0xbd, 0x5c, 0x9d, 0xf2, 0xe1, 0xba, 0x32, 0x2f, 0xcb, 0x95, 0xd3, 0xf7, 0xec, 0x13, 0xf3, 0xf6,
+	0x77, 0x00, 0x00, 0x00, 0xff, 0xff, 0xef, 0x18, 0xfb, 0x81, 0x1b, 0x05, 0x00, 0x00,
 }
--- bigtable/internal/table_service_proto/bigtable_table_service_messages.pb.go
+++ bigtable/internal/table_service_proto/bigtable_table_service_messages.pb.go
@@ -58,9 +58,10 @@ type CreateTableRequest struct {
 	InitialSplitKeys []string `protobuf:"bytes,4,rep,name=initial_split_keys" json:"initial_split_keys,omitempty"`
 }
 
-func (m *CreateTableRequest) Reset()         { *m = CreateTableRequest{} }
-func (m *CreateTableRequest) String() string { return proto.CompactTextString(m) }
-func (*CreateTableRequest) ProtoMessage()    {}
+func (m *CreateTableRequest) Reset()                    { *m = CreateTableRequest{} }
+func (m *CreateTableRequest) String() string            { return proto.CompactTextString(m) }
+func (*CreateTableRequest) ProtoMessage()               {}
+func (*CreateTableRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
 func (m *CreateTableRequest) GetTable() *google_bigtable_admin_table_v11.Table {
 	if m != nil {
@@ -74,9 +75,10 @@ type ListTablesRequest struct {
 	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 }
 
-func (m *ListTablesRequest) Reset()         { *m = ListTablesRequest{} }
-func (m *ListTablesRequest) String() string { return proto.CompactTextString(m) }
-func (*ListTablesRequest) ProtoMessage()    {}
+func (m *ListTablesRequest) Reset()                    { *m = ListTablesRequest{} }
+func (m *ListTablesRequest) String() string            { return proto.CompactTextString(m) }
+func (*ListTablesRequest) ProtoMessage()               {}
+func (*ListTablesRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 type ListTablesResponse struct {
 	// The tables present in the requested cluster.
@@ -84,9 +86,10 @@ type ListTablesResponse struct {
 	Tables []*google_bigtable_admin_table_v11.Table `protobuf:"bytes,1,rep,name=tables" json:"tables,omitempty"`
 }
 
-func (m *ListTablesResponse) Reset()         { *m = ListTablesResponse{} }
-func (m *ListTablesResponse) String() string { return proto.CompactTextString(m) }
-func (*ListTablesResponse) ProtoMessage()    {}
+func (m *ListTablesResponse) Reset()                    { *m = ListTablesResponse{} }
+func (m *ListTablesResponse) String() string            { return proto.CompactTextString(m) }
+func (*ListTablesResponse) ProtoMessage()               {}
+func (*ListTablesResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func (m *ListTablesResponse) GetTables() []*google_bigtable_admin_table_v11.Table {
 	if m != nil {
@@ -100,18 +103,20 @@ type GetTableRequest struct {
 	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 }
 
-func (m *GetTableRequest) Reset()         { *m = GetTableRequest{} }
-func (m *GetTableRequest) String() string { return proto.CompactTextString(m) }
-func (*GetTableRequest) ProtoMessage()    {}
+func (m *GetTableRequest) Reset()                    { *m = GetTableRequest{} }
+func (m *GetTableRequest) String() string            { return proto.CompactTextString(m) }
+func (*GetTableRequest) ProtoMessage()               {}
+func (*GetTableRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
 type DeleteTableRequest struct {
 	// The unique name of the table to be deleted.
 	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 }
 
-func (m *DeleteTableRequest) Reset()         { *m = DeleteTableRequest{} }
-func (m *DeleteTableRequest) String() string { return proto.CompactTextString(m) }
-func (*DeleteTableRequest) ProtoMessage()    {}
+func (m *DeleteTableRequest) Reset()                    { *m = DeleteTableRequest{} }
+func (m *DeleteTableRequest) String() string            { return proto.CompactTextString(m) }
+func (*DeleteTableRequest) ProtoMessage()               {}
+func (*DeleteTableRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
 
 type RenameTableRequest struct {
 	// The current unique name of the table.
@@ -121,9 +126,10 @@ type RenameTableRequest struct {
 	NewId string `protobuf:"bytes,2,opt,name=new_id" json:"new_id,omitempty"`
 }
 
-func (m *RenameTableRequest) Reset()         { *m = RenameTableRequest{} }
-func (m *RenameTableRequest) String() string { return proto.CompactTextString(m) }
-func (*RenameTableRequest) ProtoMessage()    {}
+func (m *RenameTableRequest) Reset()                    { *m = RenameTableRequest{} }
+func (m *RenameTableRequest) String() string            { return proto.CompactTextString(m) }
+func (*RenameTableRequest) ProtoMessage()               {}
+func (*RenameTableRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{5} }
 
 type CreateColumnFamilyRequest struct {
 	// The unique name of the table in which to create the new column family.
@@ -135,9 +141,10 @@ type CreateColumnFamilyRequest struct {
 	ColumnFamily *google_bigtable_admin_table_v11.ColumnFamily `protobuf:"bytes,3,opt,name=column_family" json:"column_family,omitempty"`
 }
 
-func (m *CreateColumnFamilyRequest) Reset()         { *m = CreateColumnFamilyRequest{} }
-func (m *CreateColumnFamilyRequest) String() string { return proto.CompactTextString(m) }
-func (*CreateColumnFamilyRequest) ProtoMessage()    {}
+func (m *CreateColumnFamilyRequest) Reset()                    { *m = CreateColumnFamilyRequest{} }
+func (m *CreateColumnFamilyRequest) String() string            { return proto.CompactTextString(m) }
+func (*CreateColumnFamilyRequest) ProtoMessage()               {}
+func (*CreateColumnFamilyRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{6} }
 
 func (m *CreateColumnFamilyRequest) GetColumnFamily() *google_bigtable_admin_table_v11.ColumnFamily {
 	if m != nil {
@@ -151,6 +158,45 @@ type DeleteColumnFamilyRequest struct {
 	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 }
 
-func (m *DeleteColumnFamilyRequest) Reset()         { *m = DeleteColumnFamilyRequest{} }
-func (m *DeleteColumnFamilyRequest) String() string { return proto.CompactTextString(m) }
-func (*DeleteColumnFamilyRequest) ProtoMessage()    {}
+func (m *DeleteColumnFamilyRequest) Reset()                    { *m = DeleteColumnFamilyRequest{} }
+func (m *DeleteColumnFamilyRequest) String() string            { return proto.CompactTextString(m) }
+func (*DeleteColumnFamilyRequest) ProtoMessage()               {}
+func (*DeleteColumnFamilyRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{7} }
+
+func init() {
+	proto.RegisterType((*CreateTableRequest)(nil), "google.bigtable.admin.table.v1.CreateTableRequest")
+	proto.RegisterType((*ListTablesRequest)(nil), "google.bigtable.admin.table.v1.ListTablesRequest")
+	proto.RegisterType((*ListTablesResponse)(nil), "google.bigtable.admin.table.v1.ListTablesResponse")
+	proto.RegisterType((*GetTableRequest)(nil), "google.bigtable.admin.table.v1.GetTableRequest")
+	proto.RegisterType((*DeleteTableRequest)(nil), "google.bigtable.admin.table.v1.DeleteTableRequest")
+	proto.RegisterType((*RenameTableRequest)(nil), "google.bigtable.admin.table.v1.RenameTableRequest")
+	proto.RegisterType((*CreateColumnFamilyRequest)(nil), "google.bigtable.admin.table.v1.CreateColumnFamilyRequest")
+	proto.RegisterType((*DeleteColumnFamilyRequest)(nil), "google.bigtable.admin.table.v1.DeleteColumnFamilyRequest")
+}
+
+var fileDescriptor0 = []byte{
+	// 368 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x94, 0x52, 0x5d, 0x4f, 0x2a, 0x31,
+	0x10, 0xcd, 0x5e, 0xb8, 0xe4, 0x32, 0xd7, 0x0f, 0xec, 0xd3, 0xc2, 0x83, 0x42, 0x13, 0x13, 0x4c,
+	0xcc, 0x6e, 0x44, 0xfd, 0x03, 0x60, 0x34, 0x46, 0x4d, 0x08, 0xf2, 0xbe, 0x29, 0xbb, 0xe3, 0xa6,
+	0xb1, 0xdb, 0xe2, 0xb6, 0x60, 0xf8, 0x03, 0xc6, 0x9f, 0x2d, 0xdb, 0x5d, 0x11, 0x12, 0x04, 0x7d,
+	0xeb, 0xf4, 0x9c, 0x76, 0xce, 0x9c, 0x39, 0x10, 0xc6, 0x4a, 0xc5, 0x02, 0xbd, 0x58, 0x09, 0x26,
+	0x63, 0x4f, 0xa5, 0xb1, 0x1f, 0x0a, 0x35, 0x89, 0xfc, 0x11, 0x8f, 0x0d, 0x1b, 0x09, 0xf4, 0xb9,
+	0x34, 0x98, 0x4a, 0x26, 0x7c, 0x5b, 0x06, 0x1a, 0xd3, 0x29, 0x0f, 0x31, 0x18, 0xa7, 0xca, 0xa8,
+	0x05, 0x2b, 0x58, 0x05, 0x13, 0xd4, 0x9a, 0xc5, 0xa8, 0x3d, 0xcb, 0x22, 0x87, 0x45, 0x93, 0x4f,
+	0xb6, 0xc7, 0xa2, 0x84, 0x4b, 0x2f, 0x3f, 0x4f, 0xcf, 0x1a, 0xc3, 0xdf, 0x8a, 0x88, 0x98, 0x61,
+	0xeb, 0x15, 0x64, 0x48, 0xde, 0x95, 0xbe, 0x39, 0x40, 0x7a, 0x29, 0x32, 0x83, 0xc3, 0x0c, 0x1a,
+	0xe0, 0xcb, 0x04, 0xb5, 0x21, 0x3b, 0x50, 0x96, 0x2c, 0x41, 0xd7, 0x69, 0x3a, 0xed, 0x2a, 0xa9,
+	0xc1, 0xbf, 0xfc, 0x21, 0x8f, 0xdc, 0x3f, 0xf6, 0xe6, 0x02, 0xfe, 0xda, 0x1b, 0xb7, 0x34, 0x2f,
+	0xff, 0x77, 0x8e, 0xbd, 0xcd, 0xe2, 0x3d, 0xfb, 0x39, 0x69, 0x00, 0xe1, 0x92, 0x1b, 0xce, 0x44,
+	0xa0, 0xc7, 0x82, 0x9b, 0xe0, 0x19, 0x67, 0xda, 0x2d, 0x37, 0x4b, 0xed, 0x2a, 0x6d, 0xc1, 0xc1,
+	0x3d, 0xd7, 0xc6, 0x12, 0xf5, 0x5a, 0x19, 0xf4, 0x0e, 0xc8, 0x32, 0x45, 0x8f, 0x95, 0xd4, 0x48,
+	0x2e, 0xa1, 0x62, 0xdb, 0xe8, 0x39, 0xab, 0xf4, 0x63, 0x2d, 0xf4, 0x08, 0xf6, 0x6f, 0xd0, 0x7c,
+	0x3f, 0x34, 0xa5, 0x40, 0xae, 0x50, 0xe0, 0x26, 0x63, 0x68, 0x07, 0xc8, 0x00, 0xb3, 0x7a, 0x83,
+	0x79, 0x7b, 0x50, 0x91, 0xf8, 0xba, 0xb0, 0x8e, 0xbe, 0x3b, 0x50, 0xcf, 0x1d, 0xef, 0x29, 0x31,
+	0x49, 0xe4, 0x35, 0x4b, 0xb8, 0x98, 0xad, 0x7f, 0xeb, 0x42, 0x2d, 0xb4, 0xa4, 0xe0, 0xc9, 0xb2,
+	0xbe, 0x16, 0xd0, 0x83, 0xdd, 0x15, 0xa4, 0x58, 0xc4, 0xe9, 0xb6, 0xe1, 0x97, 0x7b, 0xd2, 0x13,
+	0xa8, 0xe7, 0x23, 0x6e, 0x55, 0xd2, 0xbd, 0x05, 0x1a, 0xaa, 0x64, 0xcb, 0xef, 0xdd, 0x56, 0xb7,
+	0x00, 0xac, 0x1f, 0x8f, 0x79, 0xd0, 0x1f, 0x8a, 0x9c, 0xf7, 0xb3, 0xc0, 0xf5, 0x9d, 0x51, 0xc5,
+	0x26, 0xef, 0xfc, 0x23, 0x00, 0x00, 0xff, 0xff, 0xe1, 0x1c, 0x6f, 0x09, 0x56, 0x03, 0x00, 0x00,
+}
