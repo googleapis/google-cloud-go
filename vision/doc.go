0be5b78e8f89c61a4877d5fccf2818cb16b1f0e4
Jonathan Amsterdam <jba@google.com>
2016-08-11 09:55:13 -0400

    vision: improved doc overview
    Added explanations to bring it in line with the
    usage doc of other language clients.
    
    Change-Id: I6773d248787170beecf07a0fdcc4e3cdf9dbfe60
    Reviewed-on: https://code-review.googlesource.com/7212
    Reviewed-by: Dave Day <djd@golang.org>

--- vision/doc.go
+++ vision/doc.go
@@ -33,16 +33,63 @@ reduce image quality in the process. See
 https://cloud.google.com/vision/docs/image-best-practices#image_sizing for
 current file size limits.
 
-Use NewImageFromReader or NewImageFromGCS to create images for the Cloud Vision
-service. Creating an Image instance does not perform an API request.
+Creating an Image instance does not perform an API request.
+
+Use NewImageFromReader to obtain an image from any io.Reader, such as an open file:
+
+	f, err := os.Open("path/to/image.jpg")
+	if err != nil { ... }
+    defer f.Close()
+	img, err := vision.NewImageFromReader(f)
+	if err != nil { ... }
+
+Use NewImageFromGCS to refer to an image in Google Cloud Storage:
+
+	img := vision.NewImageFromGCS("gs://my-bucket/my-image.png")
 
 Annotating Images
 
 Client.Annotate is the most general method in the package. It can run multiple
-detections on multiple images in a single request.
+detections on multiple images with a single API call.
+
+To describe the detections you want to perform on an image, create an
+AnnotateRequest and specify the maximum number of results to return for each
+detection of interest. The exceptions are safe search and image properties,
+where a boolean is used instead.
+
+    resultSlice, err := client.Annotate(ctx, &vision.AnnotateRequest{
+        Image:      img,
+        MaxLogos:   5,
+        MaxTexts:   100,
+        SafeSearch: true,
+    })
+    if err != nil { ... }
+
+You can pass as many AnnotateRequests as desired to client.Annotate. The return
+value is a slice of an Annotations. Each Annotations value may contain an Error
+along with one or more successful results. The failed detections will have a nil annotation.
+
+    result := resultSlice[0]
+    if result.Error != nil { ... } // some detections failed
+    for _, logo := range result.Logos { ... }
+    for _, text := range result.Texts { ... }
+    if result.SafeSearch != nil { ... }
 
 Other methods on Client run a single detection on a single image. For instance,
-Client.DetectFaces will run face detection on the provided Image.
+Client.DetectFaces will run face detection on the provided Image. These methods
+return a single annotation of the appropriate type (for example, DetectFaces
+returns a FaceAnnotation). The error return value incorporates both API call
+errors and the detection errors stored in Annotations.Error, simplifying your
+logic.
+
+    faces, err := client.DetectFaces(ctx, 10) // maximum of 10 faces
+    if err != nil { ... }
+
+Here faces is a slice of FaceAnnotations. The Face field of each FaceAnnotation
+provides easy access to the positions of facial features:
+
+	fmt.Println(faces[0].Face.Nose.Tip)
+	fmt.Println(faces[0].Face.Eyes.Left.Pupil)
 
 This package is experimental and subject to API changes.
 */
