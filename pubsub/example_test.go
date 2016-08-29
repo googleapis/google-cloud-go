7e4474949dba93cf7341e26f29d521d6a0cbcde0
Tuo Shan <shantuo@google.com>
2016-08-29 16:04:58 -0700

    pubsub: add code snippets for most top-level methods.
    Change-Id: Ib61d256688b193f4ab4476432f07c0f3baa7b21d
    Reviewed-on: https://code-review.googlesource.com/7230
    Reviewed-by: Michael McGreevy <mcgreevy@golang.org>

--- pubsub/example_test.go
+++ pubsub/example_test.go
@@ -15,7 +15,8 @@
 package pubsub_test
 
 import (
-	"log"
+	"fmt"
+	"time"
 
 	"cloud.google.com/go/pubsub"
 	"golang.org/x/net/context"
@@ -25,17 +26,108 @@ func ExampleNewClient() {
 	ctx := context.Background()
 	_, err := pubsub.NewClient(ctx, "project-id")
 	if err != nil {
-		log.Fatal("new client:", err)
+		// TODO: Handle error.
 	}
 
 	// See the other examples to learn how to use the Client.
+}
+
+func ExampleClient_NewTopic() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	// Create a new topic with the given name.
+	topic, err := client.NewTopic(ctx, "topicName")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	_ = topic // TODO: use the topic.
+}
+
+func ExampleClient_Topics() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+	// List all topics.
+	it := client.Topics(ctx)
+	_ = it // See the TopicIterator example for its usage.
+}
+
+func ExampleClient_NewSubscription() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	// Create a new topic with the given name.
+	topic, err := client.NewTopic(ctx, "topicName")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	// Create a new subscription to the previously created topic
+	// with the given name.
+	sub, err := client.NewSubscription(ctx, "subName", topic, 10*time.Second, nil)
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	_ = sub // TODO: use the subscription.
+}
+
+func ExampleClient_Subscriptions() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+	// List all subscriptions of the project.
+	it := client.Subscriptions(ctx)
+	_ = it // See the SubscriptionIterator example for its usage.
+}
+
+func ExampleTopic_Delete() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	topic := client.Topic("topicName")
+	if err := topic.Delete(ctx); err != nil {
+		// TODO: Handle error.
+	}
+}
+
+func ExampleTopic_Exists() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	topic := client.Topic("topicName")
+	ok, err := topic.Exists(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+	if !ok {
+		// Topic doesn't exist.
+	}
 }
 
 func ExampleTopic_Publish() {
 	ctx := context.Background()
 	client, err := pubsub.NewClient(ctx, "project-id")
 	if err != nil {
-		log.Fatal("new client:", err)
+		// TODO: Handle error.
 	}
 
 	topic := client.Topic("topicName")
@@ -43,22 +135,72 @@ func ExampleTopic_Publish() {
 		Data: []byte("hello world"),
 	})
 	if err != nil {
-		log.Fatal(err)
+		// TODO: Handle error.
 	}
-	log.Printf("Published a message with a message ID: %s\n", msgIDs[0])
+	fmt.Printf("Published a message with a message ID: %s\n", msgIDs[0])
+}
+
+func ExampleTopic_Subscriptions() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+	topic := client.Topic("topic-name")
+	// List all subscriptions of the topic (maybe of multiple projects).
+	for subs := topic.Subscriptions(ctx); ; {
+		sub, err := subs.Next()
+		if err == pubsub.Done {
+			break
+		}
+		if err != nil {
+			// TODO: Handle error.
+		}
+		_ = sub // TODO: use the subscription.
+	}
+}
+
+func ExampleSubscription_Delete() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	sub := client.Subscription("subName")
+	if err := sub.Delete(ctx); err != nil {
+		// TODO: Handle error.
+	}
+}
+
+func ExampleSubscription_Exists() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	sub := client.Subscription("subName")
+	ok, err := sub.Exists(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+	if !ok {
+		// Subscription doesn't exist.
+	}
 }
 
 func ExampleSubscription_Pull() {
 	ctx := context.Background()
 	client, err := pubsub.NewClient(ctx, "project-id")
 	if err != nil {
-		log.Fatal("new client:", err)
+		// TODO: Handle error.
 	}
 
 	sub := client.Subscription("subName")
 	it, err := sub.Pull(ctx)
 	if err != nil {
-		log.Fatal(err)
+		// TODO: Handle error.
 	}
 
 	// Ensure that the iterator is closed down cleanly.
@@ -72,10 +214,10 @@ func ExampleSubscription_Pull() {
 			break
 		}
 		if err != nil {
-			log.Fatalf("advancing iterator: %v", err)
+			// TODO: Handle error.
 			break
 		}
-		log.Printf("message %d: %s\n", i, m.Data)
+		fmt.Printf("message %d: %s\n", i, m.Data)
 
 		// Acknowledge the message.
 		m.Done(true)
