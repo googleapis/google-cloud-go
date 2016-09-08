315bab9eb124e375a0124206517e0b8c17733bba
Takashi Matsuo <tmatsuo@google.com>
2016-09-08 12:07:27 -0700

    Stackdriver product rename

--- logging/apiv2/doc.go
+++ logging/apiv2/doc.go
@@ -17,6 +17,6 @@
 // Package logging is an experimental, auto-generated package for the
 // logging API.
 //
-// The Google Cloud Logging API lets you write log entries and manage your
+// The Stackdriver Logging API lets you write log entries and manage your
 // logs, log sinks and logs-based metrics.
 package logging // import "cloud.google.com/go/logging/apiv2"
