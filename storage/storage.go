ed4db6589336086bbad6c64c64ad067e362ba394
Jonathan Amsterdam <jba@google.com>
2016-09-25 12:43:38 -0400

    storage: package doc comment
    Change-Id: I96219d8302120c4eb91177e61d2bc178795b6956
    Reviewed-on: https://code-review.googlesource.com/7816
    Reviewed-by: Sarah Adams <shadams@google.com>

--- storage/storage.go
+++ storage/storage.go
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package storage contains a Google Cloud Storage client.
-//
-// This package is experimental and may make backwards-incompatible changes.
-package storage // import "cloud.google.com/go/storage"
+package storage
 
 import (
 	"bytes"
