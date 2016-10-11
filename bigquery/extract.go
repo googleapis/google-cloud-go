8e3be4216d671c150c31d68ee7938a9295d2ba3b
Jonathan Amsterdam <jba@google.com>
2016-10-11 07:18:28 -0400

    bigquery: add ExtractTo
    Add an Extractor type embedding an ExtractConfig, with a Run method.
    
    Add Table.ExtractTo, returning an Extractor.
    
    Change-Id: Ia977d8bfdf2caed0541599e08bc5a2200eff7c2c
    Reviewed-on: https://code-review.googlesource.com/8350
    Reviewed-by: Jun Mukai <mukai@google.com>
    Reviewed-by: Michael McGreevy <mcgreevy@golang.org>

---
+++ bigquery/extract.go
@@ -0,0 +1,80 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bigquery
+
+import (
+	"golang.org/x/net/context"
+	bq "google.golang.org/api/bigquery/v2"
+)
+
+// ExtractConfig holds the configuration for an extract job.
+type ExtractConfig struct {
+	// JobID is the ID to use for the extract job. If empty, a job ID will be automatically created.
+	JobID string
+
+	// Src is the table from which data will be extracted.
+	Src *Table
+
+	// Dst is the destination into which the data will be extracted.
+	Dst *GCSReference
+
+	// DisableHeader disables the printing of a header row in exported data.
+	DisableHeader bool
+}
+
+// An Extractor extracts data from a BigQuery table into Google Cloud Storage.
+type Extractor struct {
+	ExtractConfig
+	c *Client
+}
+
+// ExtractorTo returns an Extractor which can be used to extract data from a
+// BigQuery table into Google Cloud Storage.
+// The returned Extractor may optionally be further configured before its Run method is called.
+func (t *Table) ExtractorTo(dst *GCSReference) *Extractor {
+	return &Extractor{
+		c: t.c,
+		ExtractConfig: ExtractConfig{
+			Src: t,
+			Dst: dst,
+		},
+	}
+}
+
+// Run initiates an extract job.
+func (e *Extractor) Run(ctx context.Context) (*Job, error) {
+	job := &bq.Job{
+		Configuration: &bq.JobConfiguration{
+			Extract: &bq.JobConfigurationExtract{},
+		},
+	}
+
+	if e.JobID != "" {
+		job.JobReference = &bq.JobReference{
+			JobId:     e.JobID,
+			ProjectId: e.c.projectID,
+		}
+	}
+
+	e.Dst.customizeExtractDst(job.Configuration.Extract)
+	e.Src.customizeExtractSrc(job.Configuration.Extract)
+
+	if e.DisableHeader {
+		f := false
+		job.Configuration.Extract.PrintHeader = &f
+	}
+
+	return e.c.service.insertJob(ctx, job, e.c.projectID)
+}
--- bigquery/extract_test.go
+++ bigquery/extract_test.go
@@ -38,7 +38,7 @@ func defaultExtractJob() *bq.Job {
 	}
 }
 
-func TestExtract(t *testing.T) {
+func TestLegacyExtract(t *testing.T) {
 	s := &testService{}
 	c := &Client{
 		service:   s,
@@ -89,6 +89,68 @@ func TestExtract(t *testing.T) {
 
 	for _, tc := range testCases {
 		if _, err := c.Copy(context.Background(), tc.dst, tc.src, tc.options...); err != nil {
+			t.Errorf("err calling extract: %v", err)
+			continue
+		}
+		if !reflect.DeepEqual(s.Job, tc.want) {
+			t.Errorf("extracting: got:\n%v\nwant:\n%v", s.Job, tc.want)
+		}
+	}
+}
+
+func TestExtract(t *testing.T) {
+	s := &testService{}
+	c := &Client{
+		service:   s,
+		projectID: "project-id",
+	}
+
+	testCases := []struct {
+		dst    *GCSReference
+		src    *Table
+		config ExtractConfig
+		want   *bq.Job
+	}{
+		{
+			dst:  defaultGCS(),
+			src:  c.Dataset("dataset-id").Table("table-id"),
+			want: defaultExtractJob(),
+		},
+		{
+			dst:    defaultGCS(),
+			src:    c.Dataset("dataset-id").Table("table-id"),
+			config: ExtractConfig{DisableHeader: true},
+			want: func() *bq.Job {
+				j := defaultExtractJob()
+				f := false
+				j.Configuration.Extract.PrintHeader = &f
+				return j
+			}(),
+		},
+		{
+			dst: &GCSReference{
+				uris:              []string{"uri"},
+				Compression:       Gzip,
+				DestinationFormat: JSON,
+				FieldDelimiter:    "\t",
+			},
+			src: c.Dataset("dataset-id").Table("table-id"),
+			want: func() *bq.Job {
+				j := defaultExtractJob()
+				j.Configuration.Extract.Compression = "GZIP"
+				j.Configuration.Extract.DestinationFormat = "NEWLINE_DELIMITED_JSON"
+				j.Configuration.Extract.FieldDelimiter = "\t"
+				return j
+			}(),
+		},
+	}
+
+	for _, tc := range testCases {
+		ext := tc.src.ExtractorTo(tc.dst)
+		tc.config.Src = ext.Src
+		tc.config.Dst = ext.Dst
+		ext.ExtractConfig = tc.config
+		if _, err := ext.Run(context.Background()); err != nil {
 			t.Errorf("err calling extract: %v", err)
 			continue
 		}
