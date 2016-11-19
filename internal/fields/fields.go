097ca063245e3a035ee2968a759916188155ef23
Jonathan Amsterdam <jba@google.com>
2016-11-19 16:53:18 -0500

    fields: copy FieldsByNameFunc from reflect
    This CL is the first in a series whose final state will match
    https://code-review.googlesource.com/#/c/9330. The end goal is a
    function that will return all the fields of a struct type that are
    eligble for serialization, using the same rules as encoding/json.
    
    This CL is useless on its own. Its only purpose is to establish that
    the initial logic is taken directly from a function in the standard
    library.
    
    PS1 of this CL a copy of the FieldByNameFunc method from the reflect package.
    See https://raw.githubusercontent.com/golang/go/release-branch.go1.7/src/reflect/type.go
    for the original.
    
    PS2 an almost identical version of that function, modified only to use
    exported symbols of the reflect package.
    
    Change-Id: If824825b2921d9cb9eed15b9dfc92da480784cf0
    Reviewed-on: https://code-review.googlesource.com/9472
    Reviewed-by: Michael Darakananda <pongad@google.com>
    Reviewed-by: Jun Mukai <mukai@google.com>

---
+++ internal/fields/fields.go
@@ -0,0 +1,132 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fields
+
+import "reflect"
+
+// A fieldScan represents an item on the fieldByNameFunc scan work list.
+type fieldScan struct {
+	typ   reflect.Type
+	index []int
+}
+
+// fieldByNameFunc returns the struct field with a name that satisfies the
+// match function and a boolean to indicate if the field was found.
+func fieldByNameFunc(t reflect.Type, match func(string) bool) (result reflect.StructField, ok bool) {
+	// This uses the same condition that the Go language does: there must be a unique instance
+	// of the match at a given depth level. If there are multiple instances of a match at the
+	// same depth, they annihilate each other and inhibit any possible match at a lower level.
+	// The algorithm is breadth first search, one depth level at a time.
+
+	// The current and next slices are work queues:
+	// current lists the fields to visit on this depth level,
+	// and next lists the fields on the next lower level.
+	current := []fieldScan{}
+	next := []fieldScan{{typ: t}}
+
+	// nextCount records the number of times an embedded type has been
+	// encountered and considered for queueing in the 'next' slice.
+	// We only queue the first one, but we increment the count on each.
+	// If a struct type T can be reached more than once at a given depth level,
+	// then it annihilates itself and need not be considered at all when we
+	// process that next depth level.
+	var nextCount map[reflect.Type]int
+
+	// visited records the structs that have been considered already.
+	// Embedded pointer fields can create cycles in the graph of
+	// reachable embedded types; visited avoids following those cycles.
+	// It also avoids duplicated effort: if we didn't find the field in an
+	// embedded type T at level 2, we won't find it in one at level 4 either.
+	visited := map[reflect.Type]bool{}
+
+	for len(next) > 0 {
+		current, next = next, current[:0]
+		count := nextCount
+		nextCount = nil
+
+		// Process all the fields at this depth, now listed in 'current'.
+		// The loop queues embedded fields found in 'next', for processing during the next
+		// iteration. The multiplicity of the 'current' field counts is recorded
+		// in 'count'; the multiplicity of the 'next' field counts is recorded in 'nextCount'.
+		for _, scan := range current {
+			t := scan.typ
+			if visited[t] {
+				// We've looked through this type before, at a higher level.
+				// That higher level would shadow the lower level we're now at,
+				// so this one can't be useful to us. Ignore it.
+				continue
+			}
+			visited[t] = true
+			for i := 0; i < t.NumField(); i++ {
+				f := t.Field(i)
+				// Find name and type for field f.
+				var fname string
+				var ntyp reflect.Type
+				if !f.Anonymous {
+					fname = f.Name
+				} else {
+					// Anonymous field of type T or *T.
+					// Name taken from type.
+					ntyp = f.Type
+					if ntyp.Kind() == reflect.Ptr {
+						ntyp = ntyp.Elem()
+					}
+					fname = ntyp.Name()
+				}
+
+				// Does it match?
+				if match(fname) {
+					// Potential match
+					if count[t] > 1 || ok {
+						// Name appeared multiple times at this level: annihilate.
+						return reflect.StructField{}, false
+					}
+					result = t.Field(i)
+					result.Index = nil
+					result.Index = append(result.Index, scan.index...)
+					result.Index = append(result.Index, i)
+					ok = true
+					continue
+				}
+
+				// Queue embedded struct fields for processing with next level,
+				// but only if we haven't seen a match yet at this level and only
+				// if the embedded types haven't already been queued.
+				if ok || ntyp == nil || ntyp.Kind() != reflect.Struct {
+					continue
+				}
+				if nextCount[ntyp] > 0 {
+					nextCount[ntyp] = 2 // exact multiple doesn't matter
+					continue
+				}
+				if nextCount == nil {
+					nextCount = map[reflect.Type]int{}
+				}
+				nextCount[ntyp] = 1
+				if count[t] > 1 {
+					nextCount[ntyp] = 2 // exact multiple doesn't matter
+				}
+				var index []int
+				index = append(index, scan.index...)
+				index = append(index, i)
+				next = append(next, fieldScan{ntyp, index})
+			}
+		}
+		if ok {
+			break
+		}
+	}
+	return
+}
---
+++ internal/fields/fields_test.go
@@ -0,0 +1,56 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fields
+
+import (
+	"reflect"
+	"testing"
+)
+
+type embed1 struct {
+	Em     int
+	Dup    int
+	Shadow int
+}
+
+type embed2 struct {
+	Dup int
+}
+
+type S struct {
+	Exported   int
+	unexported int
+	Shadow     int // shadows S1.Shadow
+	embed1
+	*embed2
+}
+
+func TestFieldByNameFunc(t *testing.T) {
+	for _, test := range []struct {
+		name      string
+		wantIndex []int
+	}{
+		{"Exported", []int{0}},
+		{"unexported", []int{1}},
+		{"Shadow", []int{2}},
+		{"Em", []int{3, 0}}, // field in embedded struct
+		{"Dup", nil},        // duplicate fields at the same level annihilate each other
+	} {
+		got, _ := fieldByNameFunc(reflect.TypeOf(S{}), func(s string) bool { return s == test.name })
+		if !reflect.DeepEqual(got.Index, test.wantIndex) {
+			t.Errorf("%s: got %v, want %v", test.name, got.Index, test.wantIndex)
+		}
+	}
+}
