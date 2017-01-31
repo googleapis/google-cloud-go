dd09098543bc5e1171c212bba31c7b5e0beea39c
Sarah Adams <shadams@google.com>
2017-01-31 12:42:48 -0800

    kokoro: adds integration tests to build script
    Updates integration test credential-finding algorithm to include
    Application Default Credentials if GCLOUD_TESTS_GOLANG_KEY is
    unset.
    
    IAM permissions may need to be tweaked in order to give the application
    default credentials on the kokoro builder GCE instance appropriate
    permissions for our integration tests. I'll work with kokoro team to
    figure this out.
    
    Change-Id: I13dc78cd17629ad412c418c839b95474af6b4ee0
    Reviewed-on: https://code-review.googlesource.com/10650
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/kokoro/build.sh
+++ internal/kokoro/build.sh
@@ -14,23 +14,9 @@ GOCLOUD_HOME=$GOPATH/src/cloud.google.com/go
 mkdir -p $GOCLOUD_HOME
 
 # Move code into $GOPATH and get dependencies
-cp -R . $GOCLOUD_HOME
+cp -R ./* $GOCLOUD_HOME
 cd $GOCLOUD_HOME
 go get -v ./...
 
-# Don't print out encryption keys, etc
-set +x
-# TODO(shadams): uncomment after keystore is set up
-# set:
-# 	- encrypted_ba2d6f7723ed_key
-# 	- encrypted_ba2d6f7723ed_iv
-# 	- encrypted_ba2d6f7723ed_pass
-# from keystore.
-# openssl aes-256-cbc -K $encrypted_ba2d6f7723ed_key -iv $encrypted_ba2d6f7723ed_iv -pass pass:$encrypted_ba2d6f7723ed_pass -in kokoro-key.json.enc -out key.json -d
-set -x
-
 # Run tests
-# GCLOUD_TESTS_GOLANG_PROJECT_ID="dulcet-port-762" GCLOUD_TESTS_GOLANG_KEY="$(pwd)/key.json" go test -race -v ./...
-
-# Run tests (only unit until keystore is set up)
-go test -race -short -v ./...
+GCLOUD_TESTS_GOLANG_PROJECT_ID="dulcet-port-762" go test -race -v ./...
