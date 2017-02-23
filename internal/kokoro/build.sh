9b68cf4865e93f379a337b7e957f33de60397a48
Sarah Adams <shadams@google.com>
2017-02-23 10:50:28 -0800

    kokoro: support integration tests, move config to internal storage
    Adds encrypted service account credentials for integration tests in
    kokoro-key.
    Adds decryption logic for kokoro-key to build.sh.
    Moves kokoro-specific config to internal Google storage.
    
    Change-Id: I18fb51112be38825209311bdf7d661e9908143da
    Reviewed-on: https://code-review.googlesource.com/11170
    Reviewed-by: Ross Light <light@google.com>

--- internal/kokoro/build.sh
+++ internal/kokoro/build.sh
@@ -6,6 +6,9 @@ set -e
 # Display commands being run
 set -x
 
+# cd to project dir on Kokoro instance
+cd git/gocloud
+
 go version
 
 # Set $GOPATH
@@ -17,6 +20,19 @@ mkdir -p $GOCLOUD_HOME
 cp -R ./* $GOCLOUD_HOME
 cd $GOCLOUD_HOME
 go get -v ./...
+
+cd internal/kokoro
+# Don't print out encryption keys, etc
+set +x
+key=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_key)
+iv=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_iv)
+pass=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_pass)
+
+openssl aes-256-cbc -K $key -iv $iv -pass pass:$pass -in kokoro-key.json.enc -out key.json -d
+set -x
+
+export GCLOUD_TESTS_GOLANG_KEY="$(pwd)/key.json"
+cd $GOCLOUD_HOME
 
 # Run tests
 GCLOUD_TESTS_GOLANG_PROJECT_ID="dulcet-port-762" go test -race -v ./...
