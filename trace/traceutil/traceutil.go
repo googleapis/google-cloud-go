821e440fdbbfaa28c2a502bf6554c2a48f66a158
Jaana Burcu Dogan <jbd@google.com>
2017-03-04 20:37:38 -0800

    trace/traceutil: add HTTPClient
    HTTPClient is similar to http.Client but traces
    all outgoing requests automatically. Redirected
    requests are followed and the parent span in the
    current request context is honored.
    
    Fixes #540.
    
    Change-Id: Id58a4e74ec08ef837d02e43420ace9835f8f6259
    Reviewed-on: https://code-review.googlesource.com/11312
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Hana Kim <hakim@google.com>

---
+++ trace/traceutil/traceutil.go
@@ -0,0 +1,73 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package traceutil contains utilities for tracing.
+// This package is experimental and is subject to change.
+package traceutil
+
+import (
+	"net/http"
+
+	"cloud.google.com/go/trace"
+)
+
+type tracerTransport struct {
+	base http.RoundTripper
+}
+
+func (tt *tracerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	span := trace.FromContext(req.Context()).NewRemoteChild(req)
+	defer span.Finish()
+
+	return tt.base.RoundTrip(req)
+}
+
+// HTTPClient is an HTTP client that enhances http.Client
+// with automatic tracing support.
+type HTTPClient struct {
+	http.Client
+	tc *trace.Client
+}
+
+// Do behaves like (*http.Client).Do but automatically traces
+// outgoing requests if tracing is enabled for the current request.
+//
+// If req.Context() contains a traced *trace.Span, the outgoing request
+// is traced with the existing span. If not, the request is not traced.
+func (c *HTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return c.Client.Do(req)
+}
+
+// NewHTTPClient creates a new HTTPClient that will trace the outgoing
+// requests using tc. The attributes of this client are inherited from the
+// given http.Client. If orig is nil, http.DefaultClient is used.
+func NewHTTPClient(tc *trace.Client, orig *http.Client) *HTTPClient {
+	if orig == nil {
+		orig = http.DefaultClient
+	}
+	rt := orig.Transport
+	if rt == nil {
+		rt = http.DefaultTransport
+	}
+	client := http.Client{
+		Transport:     &tracerTransport{base: rt},
+		CheckRedirect: orig.CheckRedirect,
+		Jar:           orig.Jar,
+		Timeout:       orig.Timeout,
+	}
+	return &HTTPClient{
+		Client: client,
+		tc:     tc,
+	}
+}
---
+++ trace/traceutil/traceutil_test.go
@@ -0,0 +1,106 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package traceutil
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"cloud.google.com/go/trace"
+	"google.golang.org/api/option"
+)
+
+const (
+	testProjectID = "test-project"
+	traceHeader   = "X-Cloud-Trace-Context"
+)
+
+type noopTransport struct{}
+
+func (rt *noopTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	fmt.Println(req)
+	resp := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(strings.NewReader("{}")),
+	}
+	return resp, nil
+}
+
+type recorderTransport struct {
+	ch chan *http.Request
+}
+
+func (rt *recorderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	rt.ch <- req
+	resp := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(strings.NewReader("{}")),
+	}
+	return resp, nil
+}
+
+func newTestClient(rt http.RoundTripper) *trace.Client {
+	t, err := trace.NewClient(context.Background(), testProjectID, option.WithHTTPClient(&http.Client{Transport: rt}))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func TestNewHTTPClient(t *testing.T) {
+	rt := &recorderTransport{
+		ch: make(chan *http.Request, 1),
+	}
+
+	tc := newTestClient(&noopTransport{})
+	client := NewHTTPClient(tc, &http.Client{
+		Transport: rt,
+	})
+	req, _ := http.NewRequest("GET", "http://example.com", nil)
+
+	t.Run("NoTrace", func(t *testing.T) {
+		_, err := client.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		outgoing := <-rt.ch
+		if got, want := outgoing.Header.Get(traceHeader), ""; want != got {
+			t.Errorf("got trace header = %q; want none", got)
+		}
+	})
+
+	t.Run("Trace", func(t *testing.T) {
+		span := tc.NewSpan("/foo")
+
+		req = req.WithContext(trace.NewContext(req.Context(), span))
+		_, err := client.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		outgoing := <-rt.ch
+
+		s := tc.SpanFromHeader("/foo", outgoing.Header.Get(traceHeader))
+		if got, want := s.TraceID(), span.TraceID(); got != want {
+			t.Errorf("trace ID = %q; want %q", got, want)
+		}
+	})
+
+}
