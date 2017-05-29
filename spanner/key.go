af2c1bfef5320bb98ada3c4a12d06093ba8e7a8a
Jonathan Amsterdam <jba@google.com>
2017-05-29 18:12:06 -0400

    spanner: combine key and keyset files
    Move keyset.go into key.go, and similarly for the test files.
    
    Code motion only.
    
    Change-Id: Ib7fdfd25de02ba53f043fd755dc549036604de7f
    Reviewed-on: https://code-review.googlesource.com/13334
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Vikas Kedia <vikask@google.com>

--- spanner/key.go
+++ spanner/key.go
@@ -346,3 +346,55 @@ func (r KeyRange) keySetProto() (*sppb.KeySet, error) {
 	}
 	return &sppb.KeySet{Ranges: []*sppb.KeyRange{rp}}, nil
 }
+
+// A KeySet defines a collection of Cloud Spanner keys and/or key ranges. All the
+// keys are expected to be in the same table or index. The keys need not be sorted in
+// any particular way.
+//
+// An individual Key can act as a KeySet, as can a KeyRange. Use the KeySets function
+// to create a KeySet consisting of multiple Keys and KeyRanges. To obtain an empty
+// KeySet, call KeySets with no arguments.
+//
+// If the same key is specified multiple times in the set (for example if two
+// ranges, two keys, or a key and a range overlap), the Cloud Spanner backend behaves
+// as if the key were only specified once.
+type KeySet interface {
+	keySetProto() (*sppb.KeySet, error)
+}
+
+// AllKeys returns a KeySet that represents all Keys of a table or a index.
+func AllKeys() KeySet {
+	return all{}
+}
+
+type all struct{}
+
+func (all) keySetProto() (*sppb.KeySet, error) {
+	return &sppb.KeySet{All: true}, nil
+}
+
+// KeySets returns the union of the KeySets. If any of the KeySets is AllKeys, then
+// the resulting KeySet will be equivalent to AllKeys.
+func KeySets(keySets ...KeySet) KeySet {
+	u := make(union, len(keySets))
+	copy(u, keySets)
+	return u
+}
+
+type union []KeySet
+
+func (u union) keySetProto() (*sppb.KeySet, error) {
+	upb := &sppb.KeySet{}
+	for _, ks := range u {
+		pb, err := ks.keySetProto()
+		if err != nil {
+			return nil, err
+		}
+		if pb.All {
+			return pb, nil
+		}
+		upb.Keys = append(upb.Keys, pb.Keys...)
+		upb.Ranges = append(upb.Ranges, pb.Ranges...)
+	}
+	return upb, nil
+}
