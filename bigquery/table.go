69931d826ffbbcb4f8451b42d5cf7fc2ac6c7443
Jonathan Amsterdam <jba@google.com>
2017-06-09 10:31:37 -0400

    bigquery: get streaming buffer info
    TableMetadata now contains information about the streaming buffer.
    
    This can be used to poll for when the streaming buffer is empty.
    Polling isn't supported directly.
    
    Fixes #507, fixes #512.
    
    Change-Id: I8d4f026de07228a4747c51778a0068cc19b83047
    Reviewed-on: https://code-review.googlesource.com/13690
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Sai Cheemalapati <saicheems@google.com>

--- bigquery/table.go
+++ bigquery/table.go
@@ -64,6 +64,11 @@ type TableMetadata struct {
 
 	// The time-based partitioning settings for this table.
 	TimePartitioning *TimePartitioning
+
+	// Contains information regarding this table's streaming buffer, if one is
+	// present. This field will be nil if the table is not being streamed to or if
+	// there is no data in the streaming buffer.
+	StreamingBuffer *StreamingBuffer
 }
 
 // TableCreateDisposition specifies the circumstances under which destination table will be created.
@@ -101,9 +106,24 @@ const (
 type TableType string
 
 const (
-	RegularTable TableType = "TABLE"
-	ViewTable    TableType = "VIEW"
+	RegularTable  TableType = "TABLE"
+	ViewTable     TableType = "VIEW"
+	ExternalTable TableType = "EXTERNAL"
 )
+
+// StreamingBuffer holds information about the streaming buffer.
+type StreamingBuffer struct {
+	// A lower-bound estimate of the number of bytes currently in the streaming
+	// buffer.
+	EstimatedBytes uint64
+
+	// A lower-bound estimate of the number of rows currently in the streaming
+	// buffer.
+	EstimatedRows uint64
+
+	// The time of the oldest entry in the streaming buffer.
+	OldestEntryTime time.Time
+}
 
 func (t *Table) tableRefProto() *bq.TableReference {
 	return &bq.TableReference{
