e6cc5eed08d33dccee3114f69e4d89de0a78fe4f
Jonathan Amsterdam <jba@google.com>
2017-08-08 21:33:40 -0400

    storage: retry on REFUSED_STREAM
    Under high load, a REFUSED_STREAM error can be returned by the
    Go http2 implementation prior to Go 1.10. We retry the error
    with exponential backoff, as we do with other retryable errors.
    
    Fixes #701.
    
    Change-Id: I6e459876760d6666b0ab8e30cc4b5cf57b426f04
    Reviewed-on: https://code-review.googlesource.com/15630
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Michael Darakananda <pongad@google.com>

--- storage/go110.go
+++ storage/go110.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Google Inc. All Rights Reserved.
+// Copyright 2017 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,25 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build go1.10
+
 package storage
 
-import (
-	"cloud.google.com/go/internal"
-	gax "github.com/googleapis/gax-go"
-	"golang.org/x/net/context"
-)
+import "google.golang.org/api/googleapi"
 
-// runWithRetry calls the function until it returns nil or a non-retryable error, or
-// the context is done.
-func runWithRetry(ctx context.Context, call func() error) error {
-	return internal.Retry(ctx, gax.Backoff{}, func() (stop bool, err error) {
-		err = call()
-		if err == nil {
-			return true, nil
-		}
-		if shouldRetry(err) {
-			return false, nil
-		}
-		return true, err
-	})
+func shouldRetry(err error) bool {
+	switch e := err.(type) {
+	case *googleapi.Error:
+		// Retry on 429 and 5xx, according to
+		// https://cloud.google.com/storage/docs/exponential-backoff.
+		return e.Code == 429 || (e.Code >= 500 && e.Code < 600)
+	default:
+		return false
+	}
 }
