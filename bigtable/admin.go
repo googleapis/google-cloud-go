c0822307f5df87d8243dc9e4babcc68dd7b1ea31
Mayank Sharma <mayank.sharma@tokopedia.com>
2018-02-03 15:28:05 +0530

    bigtable: support for fetching a cluster
    review updates
    
    Changed test function name to TestInstanceUpdate
    testing display name of instance
    
    Review updates
    
    calling context cancel() in admin TestInstanceUpdate
    modify iAdminClient if condition to check nil
    updated variable name in admin.go
    
    Change-Id: Ic320c1b2fa78166f6fcb1f51d905feaee37aee7f
    Reviewed-on: https://code-review.googlesource.com/22711
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- bigtable/admin.go
+++ bigtable/admin.go
@@ -848,3 +848,23 @@ func (iac *InstanceAdminClient) Clusters(ctx context.Context, instanceId string)
 	}
 	return cis, nil
 }
+
+// GetCluster fetches a cluster in an instance
+func (iac *InstanceAdminClient) GetCluster(ctx context.Context, instanceID, clusterID string) (*ClusterInfo, error) {
+	ctx = mergeOutgoingMetadata(ctx, iac.md)
+	req := &btapb.GetClusterRequest{Name: "projects/" + iac.project + "/instances/" + instanceID + "/clusters" + clusterID}
+	c, err := iac.iClient.GetCluster(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	nameParts := strings.Split(c.Name, "/")
+	locParts := strings.Split(c.Location, "/")
+	cis := &ClusterInfo{
+		Name:       nameParts[len(nameParts)-1],
+		Zone:       locParts[len(locParts)-1],
+		ServeNodes: int(c.ServeNodes),
+		State:      c.State.String(),
+	}
+	return cis, nil
+}
