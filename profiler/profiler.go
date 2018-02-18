d9c2335b4f867debe23c606ca21666a4f6824c2c
Alexey Alexandrov <aalexand@google.com>
2018-02-17 22:47:03 -0800

    Read the project ID from $GOOGLE_CLOUD_PROJECT when it's set.
    Google Cloud Shell sets this environment variable so I'd like the agent
    to respect that. This will make the instructions for the quick start
    simpler.
    
    Change-Id: Ie5630f98187030092ef0f38f16f1d390005e4926
    Reviewed-on: https://code-review.googlesource.com/23771
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jianqiao Li <jianqiaoli@google.com>

--- profiler/profiler.go
+++ profiler/profiler.go
@@ -442,6 +442,15 @@ func initializeConfig(cfg Config) error {
 		config.ServiceVersion = os.Getenv("GAE_VERSION")
 	}
 
+	if projectID := os.Getenv("GOOGLE_CLOUD_PROJECT"); config.ProjectID == "" && projectID != "" {
+		// Cloud Shell and App Engine set this environment variable to the project
+		// ID, so use it if present. In case of App Engine the project ID is also
+		// available from the GCE metadata server, but by using the environment
+		// variable saves one request to the metadata server. The environment
+		// project ID is only used if no project ID is provided in the
+		// configuration.
+		config.ProjectID = projectID
+	}
 	if onGCE() {
 		var err error
 		if config.ProjectID == "" {
