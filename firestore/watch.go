4fdab9498a3beb35a40f238cae40209f315ac2fb
Jonathan Amsterdam <jba@google.com>
2018-03-11 16:03:11 -0400

    firestore: watch, part 3: queries
    This CL implements the machinery to handle query watches.
    
    In later CL's we'll add:
    
    - the exported surface for query watches
    - list of document changes
    - more tests
    
    Change-Id: Ifd9155252771474aa47085bf1cf1bd3ffc19b17c
    Reviewed-on: https://code-review.googlesource.com/25150
    Reviewed-by: Jean de Klerk <deklerk@google.com>

--- firestore/watch.go
+++ firestore/watch.go
@@ -18,8 +18,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"time"
 
+	"cloud.google.com/go/internal/btree"
 	"github.com/golang/protobuf/ptypes"
 	gax "github.com/googleapis/gax-go"
 	"golang.org/x/net/context"
@@ -27,6 +29,10 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+// LogWatchStreams controls whether watch stream status changes are logged.
+// This feature is EXPERIMENTAL and may disappear at any time.
+var LogWatchStreams bool = false
 
 // Implementation of realtime updates (a.k.a. watch).
 // This code is closely based on the Node.js implementation,
@@ -44,26 +50,63 @@ var defaultBackoff = gax.Backoff{
 
 // not goroutine-safe
 type watchStream struct {
-	ctx      context.Context
-	c        *Client
-	lc       pb.Firestore_ListenClient // the gRPC stream
-	target   *pb.Target                // document or query being watched
-	backoff  gax.Backoff               // for stream retries
-	err      error                     // sticky permanent error
-	readTime time.Time                 // time of most recent snapshot
-	current  bool                      // saw CURRENT, but not RESET; precondition for a snapshot
-	// Note: changeMap is trivial now (one entry), but will be more useful when query support is added.
-	changeMap   map[string]*DocumentSnapshot // doc changes by name
-	hasReturned bool                         // have we returned a snapshot yet?
+	ctx         context.Context
+	c           *Client
+	lc          pb.Firestore_ListenClient // the gRPC stream
+	target      *pb.Target                // document or query being watched
+	backoff     gax.Backoff               // for stream retries
+	err         error                     // sticky permanent error
+	readTime    time.Time                 // time of most recent snapshot
+	current     bool                      // saw CURRENT, but not RESET; precondition for a snapshot
+	hasReturned bool                      // have we returned a snapshot yet?
+
+	// An ordered tree where DocumentSnapshots are the keys.
+	docTree *btree.BTree
+	// Map of document name to DocumentSnapshot for the last returned snapshot.
+	docMap map[string]*DocumentSnapshot
+	// Map of document name to DocumentSnapshot for accumulated changes for the current snapshot.
+	// A nil value means the document was removed.
+	changeMap map[string]*DocumentSnapshot
 }
 
+const btreeDegree = 4
+
 func newWatchStreamForDocument(ctx context.Context, dr *DocumentRef) *watchStream {
-	return newWatchStream(ctx, dr.Parent.c, &pb.Target{
+	w := newWatchStream(ctx, dr.Parent.c, &pb.Target{
 		TargetType: &pb.Target_Documents{
 			Documents: &pb.Target_DocumentsTarget{[]string{dr.Path}},
 		},
 		TargetId: watchTargetID,
 	})
+	// Always called with the same document, so the less function always returns false.
+	w.docTree = btree.New(btreeDegree, func(_, _ interface{}) bool { return false })
+	return w
+}
+
+func newWatchStreamForQuery(ctx context.Context, q Query) (*watchStream, error) {
+	qp, err := q.toProto()
+	if err != nil {
+		return nil, err
+	}
+	target := &pb.Target{
+		TargetType: &pb.Target_Query{
+			Query: &pb.Target_QueryTarget{
+				Parent:    q.parentPath,
+				QueryType: &pb.Target_QueryTarget_StructuredQuery{qp},
+			},
+		},
+	}
+	w := newWatchStream(ctx, q.c, target)
+	compare := q.compareFunc()
+	w.docTree = btree.New(btreeDegree, func(a, b interface{}) bool {
+		c, err := compare(a.(*DocumentSnapshot), b.(*DocumentSnapshot))
+		if err != nil {
+			w.err = err
+			return false
+		}
+		return c < 0
+	})
+	return w, nil
 }
 
 func newWatchStream(ctx context.Context, c *Client, target *pb.Target) *watchStream {
@@ -72,30 +115,34 @@ func newWatchStream(ctx context.Context, c *Client, target *pb.Target) *watchStr
 		c:         c,
 		target:    target,
 		backoff:   defaultBackoff,
+		docMap:    map[string]*DocumentSnapshot{},
 		changeMap: map[string]*DocumentSnapshot{},
 	}
 }
 
 // Once nextSnapshot returns an error, it will always return the same error.
-func (s *watchStream) nextSnapshot() (*DocumentSnapshot, time.Time, error) {
+func (s *watchStream) nextSnapshot() (*btree.BTree, time.Time, error) {
 	if s.err != nil {
 		return nil, time.Time{}, s.err
 	}
-	for !s.handleNextMessage() {
-	}
-	if s.err != nil {
-		_ = s.close() // ignore error
-		return nil, time.Time{}, s.err
-	}
-	// changeMap should have only one item, which is the document snapshot we want to return.
-	var snap *DocumentSnapshot
-	for _, v := range s.changeMap {
-		snap = v
-		break
+	for {
+		// Process messages until we are in a consistent state.
+		for !s.handleNextMessage() {
+		}
+		if s.err != nil {
+			_ = s.close() // ignore error
+			return nil, time.Time{}, s.err
+		}
+		newDocTree := computeSnapshot(s.docTree, s.docMap, s.changeMap)
+		// Only return a snapshot if something has changed, or this is the first snapshot.
+		if !s.hasReturned || newDocTree != s.docTree {
+			s.docTree = newDocTree
+			break
+		}
 	}
 	s.changeMap = map[string]*DocumentSnapshot{}
 	s.hasReturned = true
-	return snap, s.readTime, nil
+	return s.docTree, s.readTime, nil
 }
 
 // Read a message from the stream and handle it. Return true when
@@ -110,8 +157,10 @@ func (s *watchStream) handleNextMessage() bool {
 	switch r := res.ResponseType.(type) {
 	case *pb.ListenResponse_TargetChange:
 		return s.handleTargetChange(r.TargetChange)
+
 	case *pb.ListenResponse_DocumentChange:
 		name := r.DocumentChange.Document.Name
+		s.logf("DocumentChange %q", name)
 		if hasWatchTargetID(r.DocumentChange.TargetIds) { // document changed
 			ref, err := pathToDoc(name, s.c)
 			if err == nil {
@@ -124,12 +173,25 @@ func (s *watchStream) handleNextMessage() bool {
 		} else if hasWatchTargetID(r.DocumentChange.RemovedTargetIds) { // document removed
 			s.changeMap[name] = nil
 		}
+
 	case *pb.ListenResponse_DocumentDelete:
+		s.logf("Delete %q", r.DocumentDelete.Document)
 		s.changeMap[r.DocumentDelete.Document] = nil
+
 	case *pb.ListenResponse_DocumentRemove:
+		s.logf("Remove %q", r.DocumentRemove.Document)
 		s.changeMap[r.DocumentRemove.Document] = nil
+
 	case *pb.ListenResponse_Filter:
-		panic("unimplemented")
+		s.logf("Filter %d", r.Filter.Count)
+		if int(r.Filter.Count) != s.currentSize() {
+			s.resetDocs() // Remove all the current results.
+			// The filter didn't match; close the stream so it will be re-opened on the next
+			// call to nextSnapshot.
+			_ = s.close() // ignore error
+			s.lc = nil
+		}
+
 	default:
 		s.err = fmt.Errorf("unknown response type %T", r)
 		return true
@@ -137,9 +199,11 @@ func (s *watchStream) handleNextMessage() bool {
 	return false
 }
 
+// Return true iff in a consistent state, or there is a permanent error.
 func (s *watchStream) handleTargetChange(tc *pb.TargetChange) bool {
 	switch tc.TargetChangeType {
 	case pb.TargetChange_NO_CHANGE:
+		s.logf("TargetNoChange %d", len(tc.TargetIds))
 		if len(tc.TargetIds) == 0 && tc.ReadTime != nil && s.current {
 			// Everything is up-to-date, so we are ready to return a snapshot.
 			rt, err := ptypes.Timestamp(tc.ReadTime)
@@ -149,35 +213,37 @@ func (s *watchStream) handleTargetChange(tc *pb.TargetChange) bool {
 			}
 			s.readTime = rt
 			s.target.ResumeType = &pb.Target_ResumeToken{tc.ResumeToken}
-			// We only actually return the snapshot if something has changed (or this is the
-			// first snapshot).
-			return !s.hasReturned || len(s.changeMap) > 0
+			return true
 		}
 
 	case pb.TargetChange_ADD:
+		s.logf("TargetAdd")
 		if tc.TargetIds[0] != watchTargetID {
 			s.err = errors.New("unexpected target ID sent by server")
 			return true
 		}
 
 	case pb.TargetChange_REMOVE:
+		s.logf("TargetRemove")
 		// We should never see a remove.
 		if tc.Cause != nil {
 			s.err = status.Error(codes.Code(tc.Cause.Code), tc.Cause.Message)
 		} else {
-			s.err = status.Error(codes.Internal, "firestore: internal error (client)")
+			s.err = status.Error(codes.Internal, "firestore: client saw REMOVE")
 		}
 		return true
 
 	// The targets reflect all changes committed before the targets were added
 	// to the stream.
 	case pb.TargetChange_CURRENT:
+		s.logf("TargetCurrent")
 		s.current = true
 
 	// The targets have been reset, and a new initial state for the targets will be
 	// returned in subsequent changes. Whatever changes have happened so far no
 	// longer matter.
 	case pb.TargetChange_RESET:
+		s.logf("TargetReset")
 		s.resetDocs()
 
 	default:
@@ -196,6 +262,83 @@ func (s *watchStream) resetDocs() {
 	s.target.ResumeType = nil // clear resume token
 	s.current = false
 	s.changeMap = map[string]*DocumentSnapshot{}
+	// Mark each document as deleted. If documents are not deleted, they
+	// will be send again by the server.
+	it := s.docTree.BeforeIndex(0)
+	for it.Next() {
+		s.changeMap[it.Key.(*DocumentSnapshot).Ref.Path] = nil
+	}
+}
+
+func (s *watchStream) currentSize() int {
+	_, adds, deletes := extractChanges(s.docMap, s.changeMap)
+	return len(s.docMap) + len(adds) - len(deletes)
+}
+
+// Return the changes that have occurred since the last snapshot.
+func extractChanges(docMap, changeMap map[string]*DocumentSnapshot) (updates, adds []*DocumentSnapshot, deletes []string) {
+	for name, doc := range changeMap {
+		switch {
+		case doc == nil:
+			if _, ok := docMap[name]; ok {
+				deletes = append(deletes, name)
+			}
+		case docMap[name] != nil:
+			updates = append(updates, doc)
+		default:
+			adds = append(adds, doc)
+		}
+	}
+	return updates, adds, deletes
+}
+
+// For development only.
+// TODO(jba): remove.
+func assert(b bool) {
+	if !b {
+		panic("assertion failed")
+	}
+}
+
+// Applies the mutations in changeMap to both the document tree and the
+// document lookup map. Modifies docMap in place and returns a new docTree.
+// If there were no changes, returns docTree unmodified.
+func computeSnapshot(docTree *btree.BTree, docMap, changeMap map[string]*DocumentSnapshot) *btree.BTree {
+	updatedTree := docTree
+	assert(docTree.Len() == len(docMap))
+	// TODO(jba): also return the ordered set of changes.
+	updates, adds, deletes := extractChanges(docMap, changeMap)
+	if len(adds) > 0 || len(deletes) > 0 {
+		updatedTree = docTree.Clone()
+	}
+	for _, name := range deletes {
+		oldDoc := docMap[name]
+		assert(oldDoc != nil)
+		delete(docMap, name)
+		_, found := updatedTree.Delete(oldDoc)
+		assert(found)
+	}
+	for _, newDoc := range adds {
+		name := newDoc.Ref.Path
+		assert(docMap[name] == nil)
+		docMap[name] = newDoc
+		updatedTree.Set(newDoc, nil)
+	}
+	for _, newDoc := range updates {
+		name := newDoc.Ref.Path
+		oldDoc := docMap[name]
+		assert(oldDoc != nil)
+		if !newDoc.UpdateTime.Equal(oldDoc.UpdateTime) {
+			if updatedTree == docTree {
+				updatedTree = docTree.Clone()
+			}
+			docMap[name] = newDoc
+			updatedTree.Delete(oldDoc)
+			updatedTree.Set(newDoc, nil)
+		}
+	}
+	assert(updatedTree.Len() == len(docMap))
+	return updatedTree
 }
 
 func hasWatchTargetID(ids []int32) bool {
@@ -205,6 +348,12 @@ func hasWatchTargetID(ids []int32) bool {
 		}
 	}
 	return false
+}
+
+func (s *watchStream) logf(format string, args ...interface{}) {
+	if LogWatchStreams {
+		log.Printf(format, args...)
+	}
 }
 
 // Close the stream. From this point on, calls to nextSnapshot will return
--- firestore/watch_test.go
+++ firestore/watch_test.go
@@ -15,9 +15,13 @@
 package firestore
 
 import (
+	"sort"
 	"testing"
+	"time"
 
+	"cloud.google.com/go/internal/btree"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 	gax "github.com/googleapis/gax-go"
 	"golang.org/x/net/context"
 	pb "google.golang.org/genproto/googleapis/firestore/v1beta1"
@@ -62,5 +66,196 @@ func TestWatchRecv(t *testing.T) {
 	_, err := ws.recv()
 	if got, want := status.Code(err), codes.AlreadyExists; got != want {
 		t.Fatalf("got %s, want %s", got, want)
+	}
+}
+
+func TestComputeSnapshot(t *testing.T) {
+	c := &Client{
+		projectID:  "projID",
+		databaseID: "(database)",
+	}
+	tm := time.Now()
+	i := 0
+	doc := func(path, value string) *DocumentSnapshot {
+		i++
+		return &DocumentSnapshot{
+			Ref:        c.Doc(path),
+			proto:      &pb.Document{Fields: map[string]*pb.Value{"foo": strval(value)}},
+			UpdateTime: tm.Add(time.Duration(i) * time.Second), // need unique time for updates
+		}
+	}
+	val := func(d *DocumentSnapshot) string { return d.proto.Fields["foo"].GetStringValue() }
+	less := func(a, b interface{}) bool { return val(a.(*DocumentSnapshot)) < val(b.(*DocumentSnapshot)) }
+
+	type dmap map[string]*DocumentSnapshot
+
+	ds1 := doc("C/d1", "a")
+	ds2 := doc("C/d2", "b")
+	ds2c := doc("C/d2", "c")
+	docTree := btree.New(4, less)
+	docMap := dmap{}
+	// The following test cases are not independent; each builds on the output of the previous.
+	for _, test := range []struct {
+		desc      string
+		changeMap dmap
+		want      []*DocumentSnapshot
+	}{
+		{
+			"no changes",
+			nil,
+			nil,
+		},
+		{
+			"add a doc",
+			dmap{ds1.Ref.Path: ds1},
+			[]*DocumentSnapshot{ds1},
+		},
+		{
+			"add, remove",
+			dmap{ds1.Ref.Path: nil, ds2.Ref.Path: ds2},
+			[]*DocumentSnapshot{ds2},
+		},
+		{
+			"add back, modify",
+			dmap{ds1.Ref.Path: ds1, ds2c.Ref.Path: ds2c},
+			[]*DocumentSnapshot{ds1, ds2c},
+		},
+	} {
+		docTree = computeSnapshot(docTree, docMap, test.changeMap)
+		got := treeDocs(docTree)
+		if diff := testDiff(got, test.want); diff != "" {
+			t.Fatalf("%s: %s", test.desc, diff)
+		}
+		mgot := mapDocs(docMap, less)
+		if diff := testDiff(got, mgot); diff != "" {
+			t.Fatalf("%s: docTree and docMap disagree: %s", test.desc, diff)
+		}
+	}
+
+	// Verify that if there are no changes, the returned docTree is identical to the first arg.
+	// docTree already has ds2c.
+	got := computeSnapshot(docTree, docMap, dmap{ds2c.Ref.Path: ds2c})
+	if got != docTree {
+		t.Error("returned docTree != arg docTree")
+	}
+}
+
+func treeDocs(bt *btree.BTree) []*DocumentSnapshot {
+	var ds []*DocumentSnapshot
+	it := bt.BeforeIndex(0)
+	for it.Next() {
+		ds = append(ds, it.Key.(*DocumentSnapshot))
+	}
+	return ds
+}
+
+func mapDocs(m map[string]*DocumentSnapshot, less func(a, b interface{}) bool) []*DocumentSnapshot {
+	var ds []*DocumentSnapshot
+	for _, d := range m {
+		ds = append(ds, d)
+	}
+	sort.Slice(ds, func(i, j int) bool { return less(ds[i], ds[j]) })
+	return ds
+}
+
+func TestWatchStream(t *testing.T) {
+	// Preliminary, very basic tests. Will expand and turn into cross-language tests
+	// later.
+	ctx := context.Background()
+	c, srv := newMock(t)
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	baseTime := time.Now()
+	doc := func(path string, value int, tm time.Time) *DocumentSnapshot {
+		ref := c.Doc(path)
+		ts, err := ptypes.TimestampProto(tm)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return &DocumentSnapshot{
+			Ref: ref,
+			proto: &pb.Document{
+				Name:       ref.Path,
+				Fields:     map[string]*pb.Value{"foo": intval(value)},
+				CreateTime: ts,
+				UpdateTime: ts,
+			},
+			CreateTime: tm,
+			UpdateTime: tm,
+		}
+	}
+	change := func(ds *DocumentSnapshot) *pb.ListenResponse {
+		return &pb.ListenResponse{ResponseType: &pb.ListenResponse_DocumentChange{&pb.DocumentChange{
+			Document:  ds.proto,
+			TargetIds: []int32{watchTargetID},
+		}}}
+	}
+
+	del := func(ds *DocumentSnapshot) *pb.ListenResponse {
+		return &pb.ListenResponse{ResponseType: &pb.ListenResponse_DocumentDelete{&pb.DocumentDelete{
+			Document: ds.Ref.Path,
+		}}}
+	}
+
+	q := Query{c: c, collectionID: "x"}
+	current := &pb.ListenResponse{ResponseType: &pb.ListenResponse_TargetChange{&pb.TargetChange{
+		TargetChangeType: pb.TargetChange_CURRENT,
+	}}}
+	noChange := &pb.ListenResponse{ResponseType: &pb.ListenResponse_TargetChange{&pb.TargetChange{
+		TargetChangeType: pb.TargetChange_NO_CHANGE,
+		ReadTime:         ptypes.TimestampNow(),
+	}}}
+	doc1 := doc("C/d1", 1, baseTime)
+	doc1a := doc("C/d1", 2, baseTime.Add(time.Second))
+	doc2 := doc("C/d2", 3, baseTime)
+	for _, test := range []struct {
+		desc      string
+		responses []interface{}
+		want      []*DocumentSnapshot
+	}{
+		{
+			"no changes: empty btree",
+			[]interface{}{current, noChange},
+			nil,
+		},
+		{
+			"add a doc",
+			[]interface{}{change(doc1), current, noChange},
+			[]*DocumentSnapshot{doc1},
+		},
+		{
+			"add a doc, then remove it",
+			[]interface{}{change(doc1), del(doc1), current, noChange},
+			[]*DocumentSnapshot(nil),
+		},
+		{
+			"add a doc, then add another one",
+			[]interface{}{change(doc1), change(doc2), current, noChange},
+			[]*DocumentSnapshot{doc1, doc2},
+		},
+		{
+			"add a doc, then change it",
+			[]interface{}{change(doc1), change(doc1a), current, noChange},
+			[]*DocumentSnapshot{doc1a},
+		},
+	} {
+		ws, err := newWatchStreamForQuery(ctx, q)
+		if err != nil {
+			t.Fatal(err)
+		}
+		request := &pb.ListenRequest{
+			Database:     "projects/projectID/databases/(default)",
+			TargetChange: &pb.ListenRequest_AddTarget{ws.target},
+		}
+		srv.addRPC(request, test.responses)
+		tree, _, err := ws.nextSnapshot()
+		if err != nil {
+			t.Fatalf("%s: %v", test.desc, err)
+		}
+		got := treeDocs(tree)
+		if diff := testDiff(got, test.want); diff != "" {
+			t.Errorf("%s: %s", test.desc, diff)
+		}
 	}
 }
