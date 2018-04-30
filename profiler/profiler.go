cc6c79498bc2796ceb470fb3b62f226b88825ffb
Jianqiao Li <jianqiaoli@google.com>
2018-04-30 11:11:43 -0700

    profiler: increase the initial backoff to 1 minute
    Change-Id: I83938528d1f612bdfb6d8081c54468053c55ca50
    Reviewed-on: https://code-review.googlesource.com/27070
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Alexey Alexandrov <aalexand@google.com>

--- profiler/profiler.go
+++ profiler/profiler.go
@@ -89,7 +89,7 @@ const (
 	instanceLabel    = "instance"
 	scope            = "https://www.googleapis.com/auth/monitoring.write"
 
-	initialBackoff = time.Second
+	initialBackoff = time.Minute
 	// Ensure the agent will recover within 1 hour.
 	maxBackoff        = time.Hour
 	backoffMultiplier = 1.3 // Backoff envelope increases by this factor on each retry.
