37573575e82f1f774a6e76e3e367d5fc23c04073
Jean de Klerk <deklerk@google.com>
2018-05-01 16:59:41 -0700

    redis: add v1 beta redis client
    Change-Id: I03de1db31c1361db33939da679a17980a88ecdb0
    Reviewed-on: https://code-review.googlesource.com/27230
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- regen-gapic.sh
+++ regen-gapic.sh
@@ -30,6 +30,7 @@ google/cloud/dialogflow/artman_dialogflow_v2.yaml
 google/cloud/language/artman_language_v1.yaml
 google/cloud/language/artman_language_v1beta2.yaml
 google/cloud/oslogin/artman_oslogin_v1beta.yaml
+google/cloud/redis/artman_redis_v1beta1.yaml
 google/cloud/speech/artman_speech_v1.yaml
 google/cloud/speech/artman_speech_v1beta1.yaml
 google/cloud/speech/artman_speech_v1p1beta1.yaml
