3045090e3d55473dbbb3e0e7769297af8cc683a6
jqll <jianqiaoli@google.com>
2018-08-30 15:04:55 -0700

    profiler: some style fixes to mute linter complaints
    Change-Id: I1fff4174de8d1df7742cb9f447a7ed2a7e57ae58
    Reviewed-on: https://code-review.googlesource.com/32512
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Alexey Alexandrov <aalexand@google.com>

--- profiler/busybench/busybench.go
+++ profiler/busybench/busybench.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Busybench is a tool that runs a benchmark with the profiler enabled.
 package main
 
 import (
