076a23a2a4071ecac2c5da3cf4afbe37806639d2
Jean de Klerk <deklerk@google.com>
2018-10-09 11:14:44 -0600

    internal: remove unused kokoro resources
    In preparation for an upcoming change that adds kokoro integration testing.
    
    Change-Id: I6c6d224ac3f1283ba81bec498961c7f2d3cb7802
    Reviewed-on: https://code-review.googlesource.com/c/34150
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Eno Compton <enocom@google.com>

--- internal/kokoro/build.sh
+++ internal/kokoro/build.sh
@@ -21,24 +21,5 @@ cp -R ./* $GOCLOUD_HOME
 cd $GOCLOUD_HOME
 go get -v -t ./...
 
-# # Don't run integration tests until we can protect against code from 
-# # untrusted forks reading and storing our service account key.
-# cd internal/kokoro
-# # Don't print out encryption keys, etc
-# set +x
-# key=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_key)
-# iv=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_iv)
-# pass=$(cat $KOKORO_ARTIFACTS_DIR/keystore/*_encrypted_ba2d6f7723ed_pass)
-
-# openssl aes-256-cbc -K $key -iv $iv -pass pass:$pass -in kokoro-key.json.enc -out key.json -d
-# set -x
-
-# export GCLOUD_TESTS_GOLANG_KEY="$(pwd)/key.json"
-# export GCLOUD_TESTS_GOLANG_PROJECT_ID="dulcet-port-762"
-# cd $GOCLOUD_HOME
-
 # Run tests and tee output to log file, to be pushed to GCS as artifact.
 go test -race -v -short ./... 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
-
-# Make sure README.md is up to date.
-#make -C internal/readme test diff
