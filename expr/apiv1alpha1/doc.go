8364f05d4ffa37faf9567400d62aa0cc6459463c
Jean de Klerk <deklerk@google.com>
2018-11-01 10:56:47 -0600

    expr: generate expr library
    Change-Id: Ibbd4f85dc34b15382ebede58648ec4eb2a3f2186
    Reviewed-on: https://code-review.googlesource.com/c/35092
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Eno Compton <enocom@google.com>

---
+++ expr/apiv1alpha1/doc.go
@@ -0,0 +1,44 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// AUTO-GENERATED CODE. DO NOT EDIT.
+
+// Package expr is an auto-generated package for the
+// Common Expression Language.
+//
+//   NOTE: This package is in alpha. It is not stable, and is likely to change.
+//
+// Defines common types for the Common Expression Language.
+package expr // import "cloud.google.com/go/expr/apiv1alpha1"
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
+)
+
+func insertMetadata(ctx context.Context, mds ...metadata.MD) context.Context {
+	out, _ := metadata.FromOutgoingContext(ctx)
+	out = out.Copy()
+	for _, md := range mds {
+		for k, v := range md {
+			out[k] = append(out[k], v...)
+		}
+	}
+	return metadata.NewOutgoingContext(ctx, out)
+}
+
+// DefaultAuthScopes reports the default set of authentication scopes to use with this package.
+func DefaultAuthScopes() []string {
+	return []string{}
+}
--- regen-gapic.sh
+++ regen-gapic.sh
@@ -23,6 +23,7 @@
 set -ex
 
 APIS=(
+google/api/expr/artman_cel.yaml
 google/iam/artman_iam_admin.yaml
 google/cloud/asset/artman_cloudasset_v1beta1.yaml
 google/iam/credentials/artman_iamcredentials_v1.yaml
