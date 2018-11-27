41590e5e6d7a5a30921e686fbc57c45545d8bf29
Jean de Klerk <deklerk@google.com>
2018-11-26 18:28:13 -0800

    spanner: refactor bindParams into convertParams
    This CL refactors bindParams into convertParams. The former would accept a
    request, type assert on it, and in-place modify its contents to add Param
    and ParamTypes. The latter simply returns the types (no in-place modification).
    Not only is this a lot simpler, it also makes it easier to use with future
    new types of request structs.
    
    Change-Id: I50568278a5179f9f79b5c14fff306f029e3546de
    Reviewed-on: https://code-review.googlesource.com/c/35813
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Eno Compton <enocom@google.com>

--- spanner/batch.go
+++ spanner/batch.go
@@ -147,31 +147,33 @@ func (t *BatchReadOnlyTransaction) PartitionQuery(ctx context.Context, statement
 		return nil, err
 	}
 	sid, client := sh.getID(), sh.getClient()
-	var (
-		resp       *sppb.PartitionResponse
-		partitions []*Partition
-	)
+	params, paramTypes, err := statement.convertParams()
+	if err != nil {
+		return nil, err
+	}
+
 	// request Partitions
 	req := &sppb.PartitionQueryRequest{
 		Session:          sid,
 		Transaction:      ts,
 		Sql:              statement.SQL,
 		PartitionOptions: opt.toProto(),
+		Params:           params,
+		ParamTypes:       paramTypes,
 	}
-	if err := statement.bindParams(req); err != nil {
-		return nil, err
-	}
-	resp, err = client.PartitionQuery(ctx, req)
+	resp, err := client.PartitionQuery(ctx, req)
+
 	// prepare ExecuteSqlRequest
 	r := &sppb.ExecuteSqlRequest{
 		Session:     sid,
 		Transaction: ts,
 		Sql:         statement.SQL,
+		Params:      params,
+		ParamTypes:  paramTypes,
 	}
-	if err := statement.bindParams(r); err != nil {
-		return nil, err
-	}
+
 	// generate Partitions
+	var partitions []*Partition
 	for _, p := range resp.GetPartitions() {
 		partitions = append(partitions, &Partition{
 			pt:   p.PartitionToken,
