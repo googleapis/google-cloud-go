c9a31f5e195aecaa0a854443833d039bee5cf94c
Jean de Klerk <deklerk@google.com>
2019-01-22 14:09:46 -0700

    internal: switch to modules for CI
    Change-Id: I6945568dcf19d9de75a5e3d3448d84a1e1113129
    Reviewed-on: https://code-review.googlesource.com/c/37673
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Eno Compton <enocom@google.com>

--- internal/kokoro/vet.sh
+++ internal/kokoro/vet.sh
@@ -12,9 +12,11 @@ if [[ `go version` != *"go1.11"* ]]; then
     exit 0
 fi
 
+export GO111MODULE=on
+
 # Fail if a dependency was added without the necessary go.mod/go.sum change
 # being part of the commit.
-GO111MODULE=on go mod tidy
+go mod tidy
 git diff go.mod | tee /dev/stderr | (! read)
 git diff go.sum | tee /dev/stderr | (! read)
 
