c1cf806de067f030d2ff26f153e7950386b22178
Jean de Klerk <deklerk@google.com>
2019-02-08 11:07:28 -0700

    spanner: further reduce min retry backoff
    This CL further reduces the min retry backoff from 100ms to 20ms, per
    recommendation of spanner team.
    
    Updates #1215.
    
    Change-Id: I3311de21bdc100fb48d78e40caebc6edc7506e83
    Reviewed-on: https://code-review.googlesource.com/c/38210
    Reviewed-by: Chris Broadfoot <cbro@google.com>

--- spanner/internal/backoff/backoff.go
+++ spanner/internal/backoff/backoff.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// minBackoff is the minimum backoff used by default.
-	minBackoff = 100 * time.Millisecond
+	minBackoff = 20 * time.Millisecond
 	// maxBackoff is the maximum backoff used by default.
 	maxBackoff = 32 * time.Second
 	// jitter is the jitter factor.
