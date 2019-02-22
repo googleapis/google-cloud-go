25417a16406ad4ee18579ddfb574f2774336ea1c
poy <poy@google.com>
2019-02-22 11:43:01 -0700

    datastore: adds sentinel value DetectProjectID
    When creating a Datastore client via NewClient, the sentinel value
    DetectProjectID can be used in place of a project ID. This instructs
    NewClient to use the given credentials to find the project ID.
    
    There are limitations to this approach. For example a JWT does not have
    the project ID encoded. NewClient is unable to be too helpful in
    detecting where the user went wrong as option.ClientOption is opaque and
    does now allow for inspection. DetectProjectID has some docs for a user
    here.
    
    NewClient defers to the environment variable DATASTORE_PROJECT_ID if it
    is set. Otherwise, it will parse and retrieve the credentials via
    google.golang.org/api/transport.Creds(). This implies that if the user
    is using default credentials, then NewClient will read the project ID
    from them.
    
    Change-Id: Id364e8586d7e39a3d8385ac5746417b084a49dcf
    Reviewed-on: https://code-review.googlesource.com/c/38450
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Chris Broadfoot <cbro@google.com>
    Reviewed-by: Jean de Klerk <deklerk@google.com>

--- datastore/integration_test.go
+++ datastore/integration_test.go
@@ -31,6 +31,7 @@ import (
 
 	"cloud.google.com/go/internal/testutil"
 	"cloud.google.com/go/rpcreplay"
+	"golang.org/x/oauth2"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
@@ -1255,4 +1256,34 @@ func testMutate(t *testing.T, mutate func(ctx context.Context, client *Client, m
 	if got, want := status.Code(err), codes.NotFound; got != want {
 		t.Errorf("Update non-existing key: got %s, want %s", got, want)
 	}
+}
+
+func TestDetectProjectID(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Integration tests skipped in short mode")
+	}
+	ctx := context.Background()
+
+	creds := testutil.Credentials(ctx, ScopeDatastore)
+	ts := fakets{}
+	if creds == nil {
+		t.Skip("Integration tests skipped. See CONTRIBUTING.md for details")
+	}
+
+	// Use creds with project ID.
+	if _, err := NewClient(ctx, DetectProjectID, option.WithCredentials(creds)); err != nil {
+		t.Errorf("NewClient: %v", err)
+	}
+
+	// Try to use creds without project ID.
+	_, err := NewClient(ctx, DetectProjectID, option.WithTokenSource(ts))
+	if err == nil && err.Error() != "datastore: see the docs on DetectProjectID" {
+		t.Errorf("expected an error while using TokenSource that does not have a project ID")
+	}
+}
+
+type fakets struct{}
+
+func (f fakets) Token() (*oauth2.Token, error) {
+	return nil, errors.New("shouldn't see this")
 }
