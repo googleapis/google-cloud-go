5abbbec590672bda4ae9a12d6ff38760682212ee
Jean de Klerk <deklerk@google.com>
2019-03-07 12:12:18 -0700

    securitycenter: generate v1 client
    Change-Id: Id06b882ea00cb500444910674a39976a9e35ccbb
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/38770
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Noah Dietz <ndietz@google.com>

--- regen-gapic.sh
+++ regen-gapic.sh
@@ -44,6 +44,7 @@ google/cloud/redis/artman_redis_v1.yaml
 google/cloud/scheduler/artman_cloudscheduler_v1beta1.yaml
 google/cloud/scheduler/artman_cloudscheduler_v1.yaml
 google/cloud/securitycenter/artman_securitycenter_v1beta1.yaml
+google/cloud/securitycenter/artman_securitycenter_v1.yaml
 google/cloud/speech/artman_speech_v1.yaml
 google/cloud/speech/artman_speech_v1p1beta1.yaml
 google/cloud/talent/artman_talent_v4beta1.yaml
