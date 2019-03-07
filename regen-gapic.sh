2e23e9e9b40e6d90f2d748643d1934549dd85bb9
Jean de Klerk <deklerk@google.com>
2019-03-07 12:14:16 -0700

    cloudscheduler: generate v1 client
    Change-Id: I35a49179733e97ca42cb858142d90474d79a0e69
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/38771
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Noah Dietz <ndietz@google.com>

--- regen-gapic.sh
+++ regen-gapic.sh
@@ -42,6 +42,7 @@ google/cloud/oslogin/artman_oslogin_v1beta.yaml
 google/cloud/redis/artman_redis_v1beta1.yaml
 google/cloud/redis/artman_redis_v1.yaml
 google/cloud/scheduler/artman_cloudscheduler_v1beta1.yaml
+google/cloud/scheduler/artman_cloudscheduler_v1.yaml
 google/cloud/securitycenter/artman_securitycenter_v1beta1.yaml
 google/cloud/speech/artman_speech_v1.yaml
 google/cloud/speech/artman_speech_v1p1beta1.yaml
