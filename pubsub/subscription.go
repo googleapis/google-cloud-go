09ad026a62f0561b7f7e276569eda11a6afc9773
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-05-03 15:21:29 -0700

    pubsub: add EXPERIMENTAL notice to ExpirationPolicy
    Post-merge updates suggested by Jean, from CL 40772.
    
    Change-Id: Ia41af0a129fc28b86d007ff39496c89fec8bb137
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/40890
    Reviewed-by: Jean de Klerk <deklerk@google.com>
    Reviewed-by: kokoro <noreply+kokoro@google.com>

--- pubsub/subscription.go
+++ pubsub/subscription.go
@@ -152,9 +152,8 @@ type SubscriptionConfig struct {
 	// operations on the subscription. If `expiration_policy` is not set, a
 	// *default policy* with `ttl` of 31 days will be used. The minimum allowed
 	// value for `expiration_policy.ttl` is 1 day.
-	// BETA: This feature is part of a beta release. This API might be
-	// changed in backward-incompatible ways and is not recommended for production
-	// use. It is not subject to any SLA or deprecation policy.
+	//
+	// It is EXPERIMENTAL and subject to change or removal without notice.
 	ExpirationPolicy time.Duration
 
 	// The set of labels for the subscription.
