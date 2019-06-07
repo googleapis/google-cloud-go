58a5319e6100b59200146a94ac6fea62b7fed54a
Jean de Klerk <deklerk@google.com>
2019-06-07 17:02:15 -0600

    pubsub: add ordered keys
    Note: this is available as a beta. Users can access it with
    `go get cloud.google.com/go/pubsub@v1.1.0-beta.ordered.keys`.
    
    Note: design doc is available at go/pubsub-ordered-keys-go.
    
    For users, this change involves:
    
    - PublishSettings.NumGoroutines and ReceiveSettings.NumGoroutines should
    both result in about double the number of goroutines, since an additional data
    structure (publish/receive scheduler) uses that value. Documentation has
    been adjusted accordingly to describe it more as a scaling factor than
    an exact number.
    - Default ReceiveSettings.NumGoroutines is increased from 1 to 10. It's
    generally a better experience to have multiple workers than one. And,
    some tests - like TestStreamingPullFlowControl - implicitly require it.
    - Since ordered keys require only a single outstanding RPC at once, it is
    possible to send ordered key messages to Topic.Publish (and subsequently to
    PublishScheduler.Add) faster than the bundler can publish them to the
    Pub/Sub service, resulting in a backed up queue of Pub/Sub bundles. Each
    item in the bundler queue is a goroutine, which means users may see thousands
    of goroutines in debugging they might do.
    - There may be slightly slower processing speeds due to additional data
    structures.
    
    A future CL will include a Resume method.
    
    Change-Id: Ib710944d557970290cf12321d1fdbd9b1699231d
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/41770
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jean de Klerk <deklerk@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- pubsub/streaming_pull_test.go
+++ pubsub/streaming_pull_test.go
@@ -162,7 +162,7 @@ func TestStreamingPullCancel(t *testing.T) {
 		m.Ack()
 	})
 	if got := atomic.LoadInt32(&n); got != 0 {
-		t.Errorf("Receive returned with %d callbacks still running", got)
+		t.Fatalf("Receive returned with %d callbacks still running", got)
 	}
 	if err != nil {
 		t.Fatalf("Receive got <%v>, want nil", err)
@@ -266,8 +266,13 @@ func TestStreamingPullFlowControl(t *testing.T) {
 	}()
 	// Here, two callbacks are active. Receive should be blocked in the flow
 	// control acquire method on the third message.
-	<-activec
-	<-activec
+	for i := 0; i < 2; i++ {
+		select {
+		case <-activec:
+		case <-time.After(time.Second):
+			t.Fatalf("timed out waiting for message %d", i+1)
+		}
+	}
 	select {
 	case <-activec:
 		t.Fatal("third callback in progress")
