b12e4ceecfca5316495bcf9e2530d089f27f7962
Tyler Bui-Palsulich <tbp@google.com>
2019-07-01 15:59:10 -0400

    automl: add client
    Change-Id: Id4475bf33a4da4df315b4e761fc4bbde28af3d30
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/42370
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jean de Klerk <deklerk@google.com>

--- regen-gapic.sh
+++ regen-gapic.sh
@@ -28,6 +28,7 @@ google/iam/artman_iam_admin.yaml
 google/cloud/asset/artman_cloudasset_v1beta1.yaml
 google/cloud/asset/artman_cloudasset_v1.yaml
 google/iam/credentials/artman_iamcredentials_v1.yaml
+google/cloud/automl/artman_automl_v1beta1.yaml
 google/cloud/bigquery/datatransfer/artman_bigquerydatatransfer.yaml
 google/cloud/bigquery/storage/artman_bigquerystorage_v1beta1.yaml
 google/cloud/dataproc/artman_dataproc_v1.yaml
