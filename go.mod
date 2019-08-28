323381ea063adc6f2a6f3d56efc92e4ca88b3afa
Chris Broadfoot <cbro@golang.org>
2019-08-28 14:29:10 -0700

    iam: add support for v3 policy and IAM conditions
    Adds a V3() method that returns a Policy3, which is like Policy but
    always uses requestedPolicyVersion=3 and policy.version=3.
    
    Change-Id: I62bc6f89422ec932fd43adfb93db8142b2ffe503
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/44730
    Reviewed-by: Frank Natividad <franknatividad@google.com>
    Reviewed-by: Chris Broadfoot <cbro@google.com>
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	cloud.google.com/go/bigquery v1.4.0
 	cloud.google.com/go/datastore v1.1.0
 	cloud.google.com/go/pubsub v1.2.0
-	cloud.google.com/go/storage v1.5.0
+	cloud.google.com/go/storage v1.6.0
 	github.com/golang/mock v1.4.0
 	github.com/golang/protobuf v1.3.3
 	github.com/google/go-cmp v0.4.0
@@ -17,12 +17,11 @@ require (
 	go.opencensus.io v0.22.3
 	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6
 	golang.org/x/lint v0.0.0-20200130185559-910be7a94367
-	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0
+	golang.org/x/net v0.0.0-20200226051749-491c5fce7268
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	golang.org/x/text v0.3.2
-	golang.org/x/tools v0.0.0-20200225022059-a0ec867d517c
-	google.golang.org/api v0.18.0
+	golang.org/x/tools v0.0.0-20200225230052-807dcd883420
+	google.golang.org/api v0.19.0
 	google.golang.org/genproto v0.0.0-20200225123651-fc8f55426688
 	google.golang.org/grpc v1.27.1
 	honnef.co/go/tools v0.0.1-2020.1.3
--- secretmanager/apiv1beta1/iam.go
+++ secretmanager/apiv1beta1/iam.go
@@ -15,45 +15,12 @@
 package secretmanager
 
 import (
-	"context"
-
 	"cloud.google.com/go/iam"
-	iampb "google.golang.org/genproto/googleapis/iam/v1"
 )
 
 // IAM returns a handle to inspect and change permissions of the resource
 // indicated by the given resource path. Name should be of the format
 // `projects/my-project/secrets/my-secret`.
 func (c *Client) IAM(name string) *iam.Handle {
-	return iam.InternalNewHandleClient(&iamClient{c}, name)
-}
-
-// iamClient implements the Get/Set/Test IAM methods.
-type iamClient struct {
-	c *Client
-}
-
-func (c *iamClient) Get(ctx context.Context, resource string) (*iampb.Policy, error) {
-	return c.c.GetIamPolicy(ctx, &iampb.GetIamPolicyRequest{
-		Resource: resource,
-	})
-}
-
-func (c *iamClient) Set(ctx context.Context, resource string, p *iampb.Policy) error {
-	_, err := c.c.SetIamPolicy(ctx, &iampb.SetIamPolicyRequest{
-		Policy:   p,
-		Resource: resource,
-	})
-	return err
-}
-
-func (c *iamClient) Test(ctx context.Context, resource string, perms []string) ([]string, error) {
-	resp, err := c.c.TestIamPermissions(ctx, &iampb.TestIamPermissionsRequest{
-		Resource:    resource,
-		Permissions: perms,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return resp.Permissions, nil
+	return iam.InternalNewHandleGRPCClient(c.client, name)
 }
