a94241f64edde1729fc152cc9bb38c1123e01edd
Jean de Klerk <deklerk@google.com>
2019-09-16 11:52:18 -0600

    recommender: add v1beta1 client
    Change-Id: Iecf97e05fa495a85aec4fe84170499c1bc9b6f7e
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/45590
    Reviewed-by: Emmanuel Odeke <emm.odeke@gmail.com>
    Reviewed-by: Noah Dietz <ndietz@google.com>

--- gapics.sh
+++ gapics.sh
@@ -37,6 +37,7 @@ google/cloud/oslogin/artman_oslogin_v1.yaml
 google/cloud/oslogin/artman_oslogin_v1beta.yaml
 google/cloud/phishingprotection/artman_phishingprotection_v1beta1.yaml
 google/cloud/recaptchaenterprise/artman_recaptchaenterprise_v1beta1.yaml
+google/cloud/recommender/artman_recommender_v1beta1.yaml
 google/cloud/redis/artman_redis_v1beta1.yaml
 google/cloud/redis/artman_redis_v1.yaml
 google/cloud/scheduler/artman_cloudscheduler_v1beta1.yaml
