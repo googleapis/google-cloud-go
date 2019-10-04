ed7b09c68546f4ebfb12facd873be17fc44eff2c
neguse <negcee@gmail.com>
2019-10-04 22:55:27 +0900

    spanner/spansql: parse OPTIONS of column definition in DDL. Fixes #1693
    Add parse `option_def` such as `OPTIONS (allow_commit_timestamp = {true | null})`.
    ref. cloud.google.com/spanner/docs/data-definition-language
    
    Change-Id: Icefe76d3da11842f76ea983afc349bf6fc87cecd
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/46390
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: David Symonds <dsymonds@golang.org>

--- spanner/spansql/parser_test.go
+++ spanner/spansql/parser_test.go
@@ -219,7 +219,8 @@ func TestParseDDL(t *testing.T) {
 			System STRING(MAX) NOT NULL,  # This is a comment.
 			RepoPath STRING(MAX) NOT NULL,  -- This is another comment.
 			Count INT64, /* This is a
-			              * multiline comment. */
+						  * multiline comment. */
+			UpdatedAt TIMESTAMP OPTIONS (allow_commit_timestamp = true),
 		) PRIMARY KEY(System, RepoPath);
 		CREATE UNIQUE INDEX MyFirstIndex ON FooBar (
 			Count DESC
@@ -250,6 +251,7 @@ func TestParseDDL(t *testing.T) {
 					{Name: "System", Type: Type{Base: String, Len: MaxLen}, NotNull: true},
 					{Name: "RepoPath", Type: Type{Base: String, Len: MaxLen}, NotNull: true},
 					{Name: "Count", Type: Type{Base: Int64}},
+					{Name: "UpdatedAt", Type: Type{Base: Timestamp}, AllowCommitTimestamp: boolAddr(true)},
 				},
 				PrimaryKey: []KeyPart{
 					{Column: "System"},
