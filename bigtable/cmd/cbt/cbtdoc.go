dc40778695a553dcbebc7d3399a034313b72c487
Colleen Shaffrey <cshaffrey@google.com>
2019-10-04 13:54:07 -0400

    bigtable - tweak formatting in cbt reference
    Change-Id: Id03d43e8349db3c7f12143a6c67ef6ed00c2d09a
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/46430
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Gary Elliott <garyelliott@google.com>

--- bigtable/cmd/cbt/cbtdoc.go
+++ bigtable/cmd/cbt/cbtdoc.go
@@ -71,9 +71,7 @@ The options are:
     -creds string
         Path to the credentials file. If set, uses the application credentials in this file
 
-Examples:
-  cbt -instance=my-instance ls
-  cbt -project=my-project listinstances
+Example:  cbt -instance=my-instance ls
 
 Use "cbt help \<command>" for more information about a command.
 
@@ -87,6 +85,7 @@ that you must put quotes around values that contain spaces or operators. It also
 if a value is arbitrary bytes, you need to prefix it with a dollar sign and use single quotes.
 
 Example:
+
 cbt -project my-project -instance my-instance lookup my-table $'\224\257\312W\365:\205d\333\2471\315\'
 
 
