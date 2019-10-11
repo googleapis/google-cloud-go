97e99caee271c6c99837738e7877388ac92d8ad4
Chris Cotter <cjcotter@google.com>
2019-10-11 18:25:40 -0400

    all: remove superfluous license check
    Licenses are also checked in license_test.go, which is a better check
    since it has better error reporting.
    
    Change-Id: I278229ac426a798185e9282919ef73710b3a46ae
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/46870
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- internal/kokoro/vet.sh
+++ internal/kokoro/vet.sh
@@ -44,9 +44,6 @@ done
 git diff go.mod | tee /dev/stderr | (! read)
 git diff go.sum | tee /dev/stderr | (! read)
 
-# Look at all .go files (ignoring .pb.go files) and make sure they have a Copyright. Fail if any don't.
-find . -type f -name "*.go" ! -name "*.pb.go" -exec grep -L "\(Copyright [0-9]\{4,\}\)" {} \; 2>&1 | tee /dev/stderr | (! read)
-
 gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
 goimports -l . 2>&1 | tee /dev/stderr | (! read)
 
