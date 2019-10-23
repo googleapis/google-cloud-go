3f8decafab055446fcfa39b7aa865e31c1bfdbe0
Jean de Klerk <deklerk@google.com>
2019-10-23 14:37:16 -0600

    spanner: change package name to not have _test_test
    This is making glaze very angry during the g3 import.
    
    Change-Id: I7d51f2ba19b284509eb29da7621e5ba00a1c20aa
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/47350
    Reviewed-by: Noah Dietz <ndietz@google.com>

--- spanner/internal/testutil/inmem_spanner_server_test.go
+++ spanner/internal/testutil/inmem_spanner_server_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testutil_test_test
+package testutil_test
 
 import (
 	"strconv"
