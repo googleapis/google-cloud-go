3c0246be18ab214a1011542eab478a6fe1651a1d
Seth Hollyman <shollyman@google.com>
2019-10-29 22:04:12 +0000

    bigtable: release bigtable/v1.1.0
    This change will be tagged bigtable/v1.1.0
    
    Change-Id: I60780bc8ceddd1e5301e8b479b5a5ee6a4c1422e
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/47690
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jean de Klerk <deklerk@google.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -26,7 +26,7 @@ import (
 
 // Repo is the current version of the client libraries in this
 // repo. It should be a date in YYYYMMDD format.
-const Repo = "20191028"
+const Repo = "20191029"
 
 // Go returns the Go runtime version. The returned string
 // has no whitespace.
