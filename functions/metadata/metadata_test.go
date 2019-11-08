4f267988f0315cc9da01b629a9e70d0bb3ea1192
Julie Hockett <juliehockett@google.com>
2019-11-07 16:21:33 -0800

    functions/metadata: handle string resources
    Some legacy events arrive with the resource field of the metadata as a
    string value in the JSON, rather than the struct. Handle these appropriately
    in a custom JSON unmarshaller.
    
    Change-Id: I5634ee6be8ee80d73a93cd8d79b3a3adbe588716
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/48050
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- functions/metadata/metadata_test.go
+++ functions/metadata/metadata_test.go
@@ -16,8 +16,12 @@ package metadata
 
 import (
 	"context"
+	"encoding/json"
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestMetadata(t *testing.T) {
@@ -41,5 +45,78 @@ func TestMetadataError(t *testing.T) {
 	}
 	if _, err := FromContext(NewContext(context.Background(), nil)); err == nil {
 		t.Errorf("FromContext got no error, wanted an error")
+	}
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	ts, err := time.Parse("2006-01-02T15:04:05Z07:00", "2019-11-04T23:01:10.112Z")
+	if err != nil {
+		t.Fatalf("Error parsing time: %v.", err)
+	}
+	var tests = []struct {
+		name string
+		data []byte
+		want Metadata
+	}{
+		{
+			name: "MetadataWithResource",
+			data: []byte(`{
+				"eventId": "1234567",
+				"timestamp": "2019-11-04T23:01:10.112Z",
+				"eventType": "google.pubsub.topic.publish",
+				"resource": {
+						"service": "pubsub.googleapis.com",
+						"name": "mytopic",
+						"type": "type.googleapis.com/google.pubsub.v1.PubsubMessage"
+				},
+				"data": {
+						"@type": "type.googleapis.com/google.pubsub.v1.PubsubMessage",
+						"attributes": null,
+						"data": "test data"
+						}
+				}`),
+			want: Metadata{
+				EventID:   "1234567",
+				Timestamp: ts,
+				EventType: "google.pubsub.topic.publish",
+				Resource: &Resource{
+					Service: "pubsub.googleapis.com",
+					Name:    "mytopic",
+					Type:    "type.googleapis.com/google.pubsub.v1.PubsubMessage",
+				},
+			},
+		},
+		{
+			name: "MetadataWithString",
+			data: []byte(`{
+				"eventId": "1234567",
+				"timestamp": "2019-11-04T23:01:10.112Z",
+				"eventType": "google.pubsub.topic.publish",
+				"resource": "projects/myproject/mytopic",
+				"data": {
+						"@type": "type.googleapis.com/google.pubsub.v1.PubsubMessage",
+						"attributes": null,
+						"data": "test data"
+						}
+				}`),
+			want: Metadata{
+				EventID:   "1234567",
+				Timestamp: ts,
+				EventType: "google.pubsub.topic.publish",
+				Resource: &Resource{
+					RawPath: "projects/myproject/mytopic",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		var m Metadata
+		if err := json.Unmarshal(tc.data, &m); err != nil {
+			t.Errorf("UnmarshalJSON(%s) error: %v", tc.name, err)
+		}
+		if !cmp.Equal(m, tc.want) {
+			t.Errorf("UnmarshalJSON(%s) error: got %v, want %v", tc.name, m, tc.want)
+		}
 	}
 }
