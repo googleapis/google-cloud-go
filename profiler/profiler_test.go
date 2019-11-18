ab67d6766dacc1b2a17f021ea3bf1af576b6683f
Maggie Nolan <nolanmar@google.com>
2019-11-18 12:34:57 -0800

    profiler: log when a profile creation attempt begins
    This change also includes modifications necessary to fix a failure under "go test -race" in TestAgentWithServer which was revealed after adding the log statement.
    
    Change-Id: Ia2428ba5495082a233e50acc4e0a46c9ee6e91f2
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/48630
    Reviewed-by: Jianqiao Li <jianqiaoli@google.com>

--- profiler/profiler_test.go
+++ profiler/profiler_test.go
@@ -48,13 +48,13 @@ import (
 )
 
 const (
-	testProjectID       = "test-project-ID"
-	testInstance        = "test-instance"
-	testZone            = "test-zone"
-	testService         = "test-service"
-	testSvcVersion      = "test-service-version"
-	testProfileDuration = time.Second * 10
-	testServerTimeout   = time.Second * 15
+	testProjectID                = "test-project-ID"
+	testInstance                 = "test-instance"
+	testZone                     = "test-zone"
+	testService                  = "test-service"
+	testSvcVersion               = "test-service-version"
+	testProfileDuration          = time.Second * 10
+	testProfileCollectionTimeout = time.Second * 15
 )
 
 func createTestDeployment() *pb.Deployment {
@@ -738,18 +738,15 @@ func TestInitializeConfig(t *testing.T) {
 type fakeProfilerServer struct {
 	count       int
 	gotProfiles map[string][]byte
-	done        chan bool
 }
 
 func (fs *fakeProfilerServer) CreateProfile(ctx context.Context, in *pb.CreateProfileRequest) (*pb.Profile, error) {
 	fs.count++
-	switch fs.count {
+	switch fs.count % 2 {
 	case 1:
 		return &pb.Profile{Name: "testCPU", ProfileType: pb.ProfileType_CPU, Duration: ptypes.DurationProto(testProfileDuration)}, nil
-	case 2:
-		return &pb.Profile{Name: "testHeap", ProfileType: pb.ProfileType_HEAP}, nil
 	default:
-		select {}
+		return &pb.Profile{Name: "testHeap", ProfileType: pb.ProfileType_HEAP}, nil
 	}
 }
 
@@ -759,7 +756,6 @@ func (fs *fakeProfilerServer) UpdateProfile(ctx context.Context, in *pb.UpdatePr
 		fs.gotProfiles["CPU"] = in.Profile.ProfileBytes
 	case pb.ProfileType_HEAP:
 		fs.gotProfiles["HEAP"] = in.Profile.ProfileBytes
-		fs.done <- true
 	}
 	return in.Profile, nil
 }
@@ -918,27 +914,29 @@ func hog(dt time.Duration, hogger func(mu1, mu2 *sync.Mutex, start time.Time, dt
 }
 
 func TestAgentWithServer(t *testing.T) {
-	oldDialGRPC, oldConfig := dialGRPC, config
+	oldDialGRPC, oldConfig, oldProfilingDone := dialGRPC, config, profilingDone
 	defer func() {
-		dialGRPC, config = oldDialGRPC, oldConfig
+		dialGRPC, config, profilingDone = oldDialGRPC, oldConfig, oldProfilingDone
 	}()
+
+	profilingDone = make(chan bool)
 
 	srv, err := testutil.NewServer()
 	if err != nil {
 		t.Fatalf("testutil.NewServer(): %v", err)
 	}
-	fakeServer := &fakeProfilerServer{gotProfiles: map[string][]byte{}, done: make(chan bool)}
+	fakeServer := &fakeProfilerServer{gotProfiles: map[string][]byte{}}
 	pb.RegisterProfilerServiceServer(srv.Gsrv, fakeServer)
-
 	srv.Start()
 
 	dialGRPC = gtransport.DialInsecure
 	if err := Start(Config{
-		Service:   testService,
-		ProjectID: testProjectID,
-		APIAddr:   srv.Addr,
-		Instance:  testInstance,
-		Zone:      testZone,
+		Service:     testService,
+		ProjectID:   testProjectID,
+		APIAddr:     srv.Addr,
+		Instance:    testInstance,
+		Zone:        testZone,
+		numProfiles: 2,
 	}); err != nil {
 		t.Fatalf("Start(): %v", err)
 	}
@@ -947,9 +945,9 @@ func TestAgentWithServer(t *testing.T) {
 	go profileeLoop(quitProfilee)
 
 	select {
-	case <-fakeServer.done:
-	case <-time.After(testServerTimeout):
-		t.Errorf("got timeout after %v, want fake server done", testServerTimeout)
+	case <-profilingDone:
+	case <-time.After(testProfileCollectionTimeout):
+		t.Errorf("got timeout after %v, want profile collection done", testProfileCollectionTimeout)
 	}
 	quitProfilee <- true
 
