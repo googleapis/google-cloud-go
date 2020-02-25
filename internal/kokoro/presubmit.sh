f271c4526d3af4967d85fd48d2d1c4fbb9eb86e2
Cody Oss <codyoss@google.com>
2020-02-25 13:49:43 -0700

    internal: report error in any module
    Fixes #1802
    
    Change-Id: I89ccca8e994493246183cfb3ccbf77f4bc6bd459
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/52510
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- internal/kokoro/presubmit.sh
+++ internal/kokoro/presubmit.sh
@@ -51,20 +51,20 @@ mkdir $KOKORO_ARTIFACTS_DIR/tests
 # Takes the kokoro output log (raw stdout) and creates a machine-parseable xml
 # file (xUnit). Then it exits with whatever exit code the last command had.
 create_junit_xml() {
-  last_status_code=$?
-
   cat $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt \
     | go-junit-report > $KOKORO_ARTIFACTS_DIR/tests/sponge_log.xml
-
-  exit $last_status_code
 }
 
 trap create_junit_xml EXIT ERR
 
+exit_code=0
 # Run tests and tee output to log file, to be pushed to GCS as artifact.
 for i in `find . -name go.mod`; do
   pushd `dirname $i`;
     go test -race -v -timeout 15m -short ./... 2>&1 \
       | tee -a $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
+    exit_code=$(($exit_code + $?))
   popd;
 done
+
+exit $exit_code
