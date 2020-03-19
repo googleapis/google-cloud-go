b580ccd2f35fa1c00b5cb0f521f073b5e63813d7
Chris Cotter <cjcotter@google.com>
2020-03-19 11:21:55 -0400

    storage: fix response header check in reader.
    Fix a small bug where the wrong header was being checked for.
    
    Change-Id: I8c20fc38c6e8f7859a9f34564cd8c2dce3a3e437
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/53570
    Reviewed-by: Frank Natividad <franknatividad@google.com>
    Reviewed-by: kokoro <noreply+kokoro@google.com>

--- storage/reader.go
+++ storage/reader.go
@@ -232,7 +232,7 @@ func (o *ObjectHandle) NewRangeReader(ctx context.Context, offset, length int64)
 		body = emptyBody
 	}
 	var metaGen int64
-	if res.Header.Get("X-Goog-Generation") != "" {
+	if res.Header.Get("X-Goog-Metageneration") != "" {
 		metaGen, err = strconv.ParseInt(res.Header.Get("X-Goog-Metageneration"), 10, 64)
 		if err != nil {
 			return nil, err
