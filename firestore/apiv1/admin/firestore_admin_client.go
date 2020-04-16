2b934c77d5b4481f82600bf56294982c0279b241
noahdietz <ndietz@google.com>
2020-04-15 19:41:06 -0700

    firestore/v1: add exported SetGoogleClientInfo wrapper to manual file
    Make the generated setGoogleClientInfo package internal.
    
    Add exported SetGoogleClientInfo wrapper to manual for nested admin client
    Make the generated setGoogleClientInfo package internal for nested admin client
    
    Remove firestore/v1 from post-processing list per #1924.
    
    Change-Id: I0b33e230ce317029c54e102a6e101441c44affd6
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/54810
    Reviewed-by: Cody Oss <codyoss@google.com>
    Reviewed-by: kokoro <noreply+kokoro@google.com>

--- firestore/apiv1/admin/firestore_admin_client.go
+++ firestore/apiv1/admin/firestore_admin_client.go
@@ -183,7 +183,7 @@ func NewFirestoreAdminClient(ctx context.Context, opts ...option.ClientOption) (
 
 		firestoreAdminClient: adminpb.NewFirestoreAdminClient(connPool),
 	}
-	c.SetGoogleClientInfo()
+	c.setGoogleClientInfo()
 
 	c.LROClient, err = lroauto.NewOperationsClient(ctx, gtransport.WithConnPool(connPool))
 	if err != nil {
@@ -211,10 +211,10 @@ func (c *FirestoreAdminClient) Close() error {
 	return c.connPool.Close()
 }
 
-// SetGoogleClientInfo sets the name and version of the application in
+// setGoogleClientInfo sets the name and version of the application in
 // the `x-goog-api-client` header passed on each request. Intended for
 // use by Google-written clients.
-func (c *FirestoreAdminClient) SetGoogleClientInfo(keyval ...string) {
+func (c *FirestoreAdminClient) setGoogleClientInfo(keyval ...string) {
 	kv := append([]string{"gl-go", versionGo()}, keyval...)
 	kv = append(kv, "gapic", versionClient, "gax", gax.Version, "grpc", grpc.Version)
 	c.xGoogMetadata = metadata.Pairs("x-goog-api-client", gax.XGoogHeader(kv...))
