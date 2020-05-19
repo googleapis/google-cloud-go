4dc3a2bbc28fbd9cfec1af66a2921ca9bc74e5e9
Maggie Nolan <nolanmar@google.com>
2020-05-19 13:12:41 -0700

    profiler: update retry command used in E2E test to backoff after failed attempts
    Change-Id: Ia24b521f702c21d0042887ad2aec8f47b6899c07
    Reviewed-on: https://code-review.googlesource.com/c/gocloud/+/56670
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Kalyana Chadalavada <kchadalavada@google.com>

--- profiler/proftest/proftest.go
+++ profiler/proftest/proftest.go
@@ -56,6 +56,7 @@ trap "sleep 300 && poweroff" EXIT
 
 retry() {
   for i in {1..3}; do
+    [ $i == 1 ] || sleep 10  # Backing off after a failed attempt.
     "${@}" && return 0
   done
   return 1
