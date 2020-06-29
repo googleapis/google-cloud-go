5db27250d55d057b373474258268b5458b9146da
Hengfeng Li <hengfeng@google.com>
2020-06-29 20:23:24 +1000

    Rename the variable.

--- spanner/client.go
+++ spanner/client.go
@@ -150,6 +150,7 @@ func NewClientWithConfig(ctx context.Context, database string, config ClientConf
 	}
 
 	// Prepare gRPC channels.
+	hasNumChannelsConfig := config.NumChannels > 0
 	if config.NumChannels == 0 {
 		config.NumChannels = numChannels
 	}
@@ -172,7 +173,7 @@ func NewClientWithConfig(ctx context.Context, database string, config ClientConf
 	if err != nil {
 		return nil, err
 	}
-	if pool.Num() != config.NumChannels {
+	if hasNumChannelsConfig && pool.Num() != config.NumChannels {
 		pool.Close()
 		return nil, spannerErrorf(codes.InvalidArgument, "Connection pool mismatch: NumChannels=%v, WithGRPCConnectionPool=%v. Only set one of these options, or set both to the same value.", config.NumChannels, pool.Num())
 	}
