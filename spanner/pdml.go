39c99cb8f6fb3ae052b9d14ca05b30271185308d
Hengfeng Li <hengfeng@google.com>
2020-08-24 16:27:54 +1000

    fix(spanner): update PDML to take sessions from pool (#2736)
    * fix(spanner): update PDML to take sessions from pool

--- spanner/pdml.go
+++ spanner/pdml.go
@@ -48,20 +48,15 @@ func (c *Client) partitionedUpdate(ctx context.Context, statement Statement, opt
 	if err := checkNestedTxn(ctx); err != nil {
 		return 0, err
 	}
-	var (
-		s  *session
-		sh *sessionHandle
-	)
-	// Create session.
-	s, err = c.sc.createSession(ctx)
+
+	sh, err := c.idleSessions.take(ctx)
 	if err != nil {
 		return 0, toSpannerError(err)
 	}
-	// Delete the session at the end of the request. If the PDML statement
-	// timed out or was cancelled, the DeleteSession request might not succeed,
-	// but the session will eventually be garbage collected by the server.
-	defer s.delete(ctx)
-	sh = &sessionHandle{session: s}
+	if sh != nil {
+		defer sh.recycle()
+	}
+
 	// Create the parameters and the SQL request, but without a transaction.
 	// The transaction reference will be added by the executePdml method.
 	params, paramTypes, err := statement.convertParams()
