432d32c56305b5dad5cac4cb6b09d392a67e5b95
Chen Liang <code@chen.technology>
2020-09-19 23:32:48 +0000

    spanner: expose Row's raw value

---
+++ spanner/row_raw_value.go
@@ -0,0 +1,16 @@
+package spanner
+
+import (
+	proto3 "github.com/golang/protobuf/ptypes/struct"
+	sppb "google.golang.org/genproto/googleapis/spanner/v1"
+)
+
+// Fields returns raw grpc protobuf fields
+func (r *Row) Fields() []*sppb.StructType_Field {
+	return r.fields
+}
+
+// Values returns raw grpc protobuf row values
+func (r *Row) Values() []*proto3.Value {
+	return r.values
+}
