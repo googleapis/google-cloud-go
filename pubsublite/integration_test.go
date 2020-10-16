48f09b3c791818e4602c9594f19ce243d73887cf
tmdiep <tmdiep@google.com>
2020-10-15 21:30:24 -0400

    feat(pubsublite): Admin client and integration tests

---
+++ pubsublite/integration_test.go
@@ -0,0 +1,247 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+
+package pubsublite
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/internal/testutil"
+	"google.golang.org/api/iterator"
+)
+
+const gibi = 1 << 30
+
+var (
+	rng *rand.Rand
+
+	// A random zone is selected for each integration test run.
+	supportedZones = []string{
+		"us-central1-a",
+		"us-central1-b",
+		"us-central1-c",
+		"europe-west1-b",
+		"europe-west1-d",
+	}
+)
+
+func initIntegrationTest(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Integration tests skipped in short mode")
+	}
+	if testutil.ProjID() == "" {
+		t.Skip("Integration tests skipped. See CONTRIBUTING.md for details")
+	}
+	rng = testutil.NewRand(time.Now())
+}
+
+func cleanUpTopic(ctx context.Context, t *testing.T, client *Client, name TopicPath) {
+	if err := client.DeleteTopic(ctx, name); err != nil {
+		t.Errorf("Failed to delete topic %s: %v", name, err)
+	}
+}
+
+func cleanUpSubscription(ctx context.Context, t *testing.T, client *Client, name SubscriptionPath) {
+	if err := client.DeleteSubscription(ctx, name); err != nil {
+		t.Errorf("Failed to delete subscription %s: %v", name, err)
+	}
+}
+
+func randomLiteZone() string {
+	i := rng.Int63() % int64(len(supportedZones))
+	return supportedZones[i]
+}
+
+func TestResourceAdminOperations(t *testing.T) {
+	initIntegrationTest(t)
+
+	ctx := context.Background()
+	proj := testutil.ProjID()
+	zone := randomLiteZone()
+	region, _ := ZoneToRegion(zone)
+	resourceID := fmt.Sprintf("go-test-admin-%d", rng.Int63())
+	locationPath := LocationPath{Project: proj, Zone: zone}
+	topicPath := TopicPath{Project: proj, Zone: zone, TopicID: resourceID}
+	subscriptionPath := SubscriptionPath{Project: proj, Zone: zone, SubscriptionID: resourceID}
+	t.Logf("Topic path: %s", topicPath)
+
+	client, err := NewAdminClient(ctx, region)
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// Topic admin operations.
+	newTopicConfig := &TopicConfig{
+		Name:                       topicPath,
+		PartitionCount:             2,
+		PublishCapacityMiBPerSec:   4,
+		SubscribeCapacityMiBPerSec: 4,
+		PerPartitionBytes:          30 * gibi,
+		RetentionDuration:          time.Duration(24 * time.Hour),
+	}
+
+	gotTopicConfig, err := client.CreateTopic(ctx, newTopicConfig)
+	if err != nil {
+		t.Fatalf("Failed to create topic: %v", err)
+	}
+	defer cleanUpTopic(ctx, t, client, topicPath)
+	if !testutil.Equal(gotTopicConfig, newTopicConfig) {
+		t.Errorf("CreateTopic() got: %v, want: %v", gotTopicConfig, newTopicConfig)
+	}
+
+	if gotTopicConfig, err := client.Topic(ctx, topicPath); err != nil {
+		t.Errorf("Failed to get topic: %v", err)
+	} else if !testutil.Equal(gotTopicConfig, newTopicConfig) {
+		t.Errorf("Topic() got: %v, want: %v", gotTopicConfig, newTopicConfig)
+	}
+
+	if gotTopicPartitions, err := client.TopicPartitions(ctx, topicPath); err != nil {
+		t.Errorf("Failed to get topic partitions: %v", err)
+	} else if gotTopicPartitions != newTopicConfig.PartitionCount {
+		t.Errorf("TopicPartitions() got: %v, want: %v", gotTopicPartitions, newTopicConfig.PartitionCount)
+	}
+
+	topicIt := client.Topics(ctx, locationPath)
+	var foundTopic *TopicConfig
+	for {
+		topic, err := topicIt.Next()
+		if err == iterator.Done {
+			break
+		}
+		if testutil.Equal(topic.Name, topicPath) {
+			foundTopic = topic
+			break
+		}
+	}
+	if foundTopic == nil {
+		t.Error("Topics() did not return topic config")
+	} else if !testutil.Equal(foundTopic, newTopicConfig) {
+		t.Errorf("Topics() found config: %v, want: %v", foundTopic, newTopicConfig)
+	}
+
+	topicUpdate1 := &TopicConfigToUpdate{
+		Name:                       topicPath,
+		PublishCapacityMiBPerSec:   6,
+		SubscribeCapacityMiBPerSec: 8,
+	}
+	wantUpdatedTopicConfig1 := &TopicConfig{
+		Name:                       topicPath,
+		PartitionCount:             2,
+		PublishCapacityMiBPerSec:   6,
+		SubscribeCapacityMiBPerSec: 8,
+		PerPartitionBytes:          30 * gibi,
+		RetentionDuration:          time.Duration(24 * time.Hour),
+	}
+	if gotTopicConfig, err := client.UpdateTopic(ctx, topicUpdate1); err != nil {
+		t.Errorf("Failed to update topic: %v", err)
+	} else if !testutil.Equal(gotTopicConfig, wantUpdatedTopicConfig1) {
+		t.Errorf("UpdateTopic() got: %v, want: %v", gotTopicConfig, wantUpdatedTopicConfig1)
+	}
+
+	topicUpdate2 := &TopicConfigToUpdate{
+		Name:              topicPath,
+		PerPartitionBytes: 35 * gibi,
+		RetentionDuration: InfiniteRetention,
+	}
+	wantUpdatedTopicConfig2 := &TopicConfig{
+		Name:                       topicPath,
+		PartitionCount:             2,
+		PublishCapacityMiBPerSec:   6,
+		SubscribeCapacityMiBPerSec: 8,
+		PerPartitionBytes:          35 * gibi,
+	}
+	if gotTopicConfig, err := client.UpdateTopic(ctx, topicUpdate2); err != nil {
+		t.Errorf("Failed to update topic: %v", err)
+	} else if !testutil.Equal(gotTopicConfig, wantUpdatedTopicConfig2) {
+		t.Errorf("UpdateTopic() got: %v, want: %v", gotTopicConfig, wantUpdatedTopicConfig2)
+	}
+
+	// Subscription admin operations.
+	newSubsConfig := &SubscriptionConfig{
+		Name:                subscriptionPath,
+		Topic:               topicPath,
+		DeliveryRequirement: DeliverImmediately,
+	}
+
+	gotSubsConfig, err := client.CreateSubscription(ctx, newSubsConfig)
+	if err != nil {
+		t.Fatalf("Failed to create subscription: %v", err)
+	}
+	defer cleanUpSubscription(ctx, t, client, subscriptionPath)
+	if !testutil.Equal(gotSubsConfig, newSubsConfig) {
+		t.Errorf("CreateSubscription() got: %v, want: %v", gotSubsConfig, newSubsConfig)
+	}
+
+	if gotSubsConfig, err := client.Subscription(ctx, subscriptionPath); err != nil {
+		t.Errorf("Failed to get subscription: %v", err)
+	} else if !testutil.Equal(gotSubsConfig, newSubsConfig) {
+		t.Errorf("Subscription() got: %v, want: %v", gotSubsConfig, newSubsConfig)
+	}
+
+	subsIt := client.Subscriptions(ctx, locationPath)
+	var foundSubs *SubscriptionConfig
+	for {
+		subs, err := subsIt.Next()
+		if err == iterator.Done {
+			break
+		}
+		if testutil.Equal(subs.Name, subscriptionPath) {
+			foundSubs = subs
+			break
+		}
+	}
+	if foundSubs == nil {
+		t.Error("Subscriptions() did not return subscription config")
+	} else if !testutil.Equal(foundSubs, gotSubsConfig) {
+		t.Errorf("Subscriptions() found config: %v, want: %v", foundSubs, gotSubsConfig)
+	}
+
+	if subsPathIt, err := client.TopicSubscriptions(ctx, topicPath); err != nil {
+		t.Errorf("Failed to list topic subscriptions: %v", err)
+	} else {
+		foundSubsPath := false
+		for {
+			subsPath, err := subsPathIt.Next()
+			if err == iterator.Done {
+				break
+			}
+			if testutil.Equal(subsPath, subscriptionPath) {
+				foundSubsPath = true
+				break
+			}
+		}
+		if !foundSubsPath {
+			t.Error("TopicSubscriptions() did not return subscription path")
+		}
+	}
+
+	subsUpdate := &SubscriptionConfigToUpdate{
+		Name:                subscriptionPath,
+		DeliveryRequirement: DeliverAfterStored,
+	}
+	wantUpdatedSubsConfig := &SubscriptionConfig{
+		Name:                subscriptionPath,
+		Topic:               topicPath,
+		DeliveryRequirement: DeliverAfterStored,
+	}
+	if gotSubsConfig, err := client.UpdateSubscription(ctx, subsUpdate); err != nil {
+		t.Errorf("Failed to update subscription: %v", err)
+	} else if !testutil.Equal(gotSubsConfig, wantUpdatedSubsConfig) {
+		t.Errorf("UpdateSubscription() got: %v, want: %v", gotSubsConfig, wantUpdatedSubsConfig)
+	}
+}
---
+++ pubsublite/pubsublite.go
@@ -0,0 +1,224 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+
+package pubsublite
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+
+	vkit "cloud.google.com/go/pubsublite/apiv1"
+	pb "google.golang.org/genproto/googleapis/cloud/pubsublite/v1"
+)
+
+// Client provides admin operations for Google Pub/Sub Lite resources within a
+// Google Cloud region. A Client may be shared by multiple goroutines.
+type Client struct {
+	// The Google Cloud region that this Client manages resources for.
+	Region string
+	admin  *vkit.AdminClient
+}
+
+// NewAdminClient creates a new Cloud Pub/Sub Lite client to perform admin
+// operations for resources within a given region.
+// See https://cloud.google.com/pubsub/lite/docs/locations for the list of
+// regions and zones where Google Pub/Sub Lite is available.
+func NewAdminClient(ctx context.Context, region string, opts ...option.ClientOption) (c *Client, err error) {
+	if err := ValidateRegion(region); err != nil {
+		return nil, err
+	}
+	options := []option.ClientOption{option.WithEndpoint(region + "-pubsublite.googleapis.com:443")}
+	admin, err := vkit.NewAdminClient(ctx, options...)
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: %v", err)
+	}
+	return &Client{Region: region, admin: admin}, nil
+}
+
+// CreateTopic creates a new topic from the given config.
+func (c *Client) CreateTopic(ctx context.Context, config *TopicConfig) (*TopicConfig, error) {
+	req := &pb.CreateTopicRequest{
+		Parent:  config.Name.Location().String(),
+		Topic:   config.toProto(),
+		TopicId: config.Name.TopicID,
+	}
+	topicpb, err := c.admin.CreateTopic(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to create topic: %v", err)
+	}
+	return protoToTopicConfig(topicpb)
+}
+
+// UpdateTopic updates an existing topic from the given config and returns the
+// new topic config.
+func (c *Client) UpdateTopic(ctx context.Context, config *TopicConfigToUpdate) (*TopicConfig, error) {
+	topicpb, err := c.admin.UpdateTopic(ctx, config.toUpdateRequest())
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to update topic: %v", err)
+	}
+	return protoToTopicConfig(topicpb)
+}
+
+// DeleteTopic deletes a topic.
+func (c *Client) DeleteTopic(ctx context.Context, topic TopicPath) error {
+	if err := c.admin.DeleteTopic(ctx, &pb.DeleteTopicRequest{Name: topic.String()}); err != nil {
+		return fmt.Errorf("pubsublite: failed to delete topic: %v", err)
+	}
+	return nil
+}
+
+// Topic retrieves the configuration of a topic.
+func (c *Client) Topic(ctx context.Context, topic TopicPath) (*TopicConfig, error) {
+	topicpb, err := c.admin.GetTopic(ctx, &pb.GetTopicRequest{Name: topic.String()})
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to retrieve topic config: %v", err)
+	}
+	return protoToTopicConfig(topicpb)
+}
+
+// TopicPartitions returns the number of partitions for a topic.
+func (c *Client) TopicPartitions(ctx context.Context, topic TopicPath) (int64, error) {
+	partitions, err := c.admin.GetTopicPartitions(ctx, &pb.GetTopicPartitionsRequest{Name: topic.String()})
+	if err != nil {
+		return 0, fmt.Errorf("pubsublite: failed to retrieve topic partitions: %v", err)
+	}
+	return partitions.GetPartitionCount(), nil
+}
+
+// TopicSubscriptions retrieves the list of subscription paths for a topic.
+func (c *Client) TopicSubscriptions(ctx context.Context, topic TopicPath) (*SubscriptionPathIterator, error) {
+	subsPathIt := c.admin.ListTopicSubscriptions(ctx, &pb.ListTopicSubscriptionsRequest{Name: topic.String()})
+	return &SubscriptionPathIterator{it: subsPathIt}, nil
+}
+
+// Topics retrieves the list of topic configs for a given project and zone.
+func (c *Client) Topics(ctx context.Context, location LocationPath) *TopicIterator {
+	return &TopicIterator{
+		it: c.admin.ListTopics(ctx, &pb.ListTopicsRequest{Parent: location.String()}),
+	}
+}
+
+// CreateSubscription creates a new subscription from the given config.
+func (c *Client) CreateSubscription(ctx context.Context, config *SubscriptionConfig) (*SubscriptionConfig, error) {
+	req := &pb.CreateSubscriptionRequest{
+		Parent:         config.Name.Location().String(),
+		Subscription:   config.toProto(),
+		SubscriptionId: config.Name.SubscriptionID,
+	}
+	subspb, err := c.admin.CreateSubscription(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to create subscription: %v", err)
+	}
+	return protoToSubscriptionConfig(subspb)
+}
+
+// UpdateSubscription updates an existing subscription from the given config and
+// returns the new subscription config.
+func (c *Client) UpdateSubscription(ctx context.Context, config *SubscriptionConfigToUpdate) (*SubscriptionConfig, error) {
+	subspb, err := c.admin.UpdateSubscription(ctx, config.toUpdateRequest())
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to update subscription: %v", err)
+	}
+	return protoToSubscriptionConfig(subspb)
+}
+
+// DeleteSubscription deletes a subscription.
+func (c *Client) DeleteSubscription(ctx context.Context, subscription SubscriptionPath) error {
+	if err := c.admin.DeleteSubscription(ctx, &pb.DeleteSubscriptionRequest{Name: subscription.String()}); err != nil {
+		return fmt.Errorf("pubsublite: failed to delete subscription: %v", err)
+	}
+	return nil
+}
+
+// Subscription retrieves the configuration of a subscription.
+func (c *Client) Subscription(ctx context.Context, subscription SubscriptionPath) (*SubscriptionConfig, error) {
+	subspb, err := c.admin.GetSubscription(ctx, &pb.GetSubscriptionRequest{Name: subscription.String()})
+	if err != nil {
+		return nil, fmt.Errorf("pubsublite: failed to retrieve subscription config: %v", err)
+	}
+	return protoToSubscriptionConfig(subspb)
+}
+
+// Subscriptions retrieves the list of subscription configs for a given project
+// and zone.
+func (c *Client) Subscriptions(ctx context.Context, location LocationPath) *SubscriptionIterator {
+	return &SubscriptionIterator{
+		it: c.admin.ListSubscriptions(ctx, &pb.ListSubscriptionsRequest{Parent: location.String()}),
+	}
+}
+
+// Close releases any resources held by the client when it is no longer
+// required. If the client is available for the lifetime of the program, then
+// Close need not be called at exit.
+func (c *Client) Close() error {
+	return c.admin.Close()
+}
+
+// TopicIterator is an iterator that returns a list of topic configs.
+type TopicIterator struct {
+	it *vkit.TopicIterator
+}
+
+// Next returns the next topic config. The second return value will be
+// iterator.Done if there are no more topic configs.
+func (t *TopicIterator) Next() (*TopicConfig, error) {
+	topicpb, err := t.it.Next()
+	if err != nil {
+		if err != iterator.Done {
+			err = fmt.Errorf("pubsublite: failed to list topics: %v", err)
+		}
+		return nil, err
+	}
+	return protoToTopicConfig(topicpb)
+}
+
+// SubscriptionIterator is an iterator that returns a list of subscription
+// configs.
+type SubscriptionIterator struct {
+	it *vkit.SubscriptionIterator
+}
+
+// Next returns the next subscription config. The second return value will be
+// iterator.Done if there are no more subscription configs.
+func (s *SubscriptionIterator) Next() (*SubscriptionConfig, error) {
+	subspb, err := s.it.Next()
+	if err != nil {
+		if err != iterator.Done {
+			err = fmt.Errorf("pubsublite: failed to list subscriptions: %v", err)
+		}
+		return nil, err
+	}
+	return protoToSubscriptionConfig(subspb)
+}
+
+// SubscriptionPathIterator is an iterator that returns a list of subscription
+// paths.
+type SubscriptionPathIterator struct {
+	it *vkit.StringIterator
+}
+
+// Next returns the next subscription path. The second return value will be
+// iterator.Done if there are no more subscription paths.
+func (sp *SubscriptionPathIterator) Next() (SubscriptionPath, error) {
+	subsPath, err := sp.it.Next()
+	if err != nil {
+		if err != iterator.Done {
+			err = fmt.Errorf("pubsublite: failed to list topic subscriptions: %v", err)
+		}
+		return SubscriptionPath{}, err
+	}
+	return ParseSubscriptionPath(subsPath)
+}
