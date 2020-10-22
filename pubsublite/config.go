04abefd6ebd042ad517f3a986dbe44dc6f271d6f
tmdiep <tmdiep@google.com>
2020-10-22 17:43:50 -0400

    Comments for valid values of PerPartitionBytes and RetentionDuration

--- pubsublite/config.go
+++ pubsublite/config.go
@@ -51,12 +51,12 @@ type TopicConfig struct {
 	// The provisioned storage, in bytes, per partition. If the number of bytes
 	// stored in any of the topic's partitions grows beyond this value, older
 	// messages will be dropped to make room for newer ones, regardless of the
-	// value of `RetentionDuration`.
+	// value of `RetentionDuration`. Must be > 0.
 	PerPartitionBytes int64
 
 	// How long a published message is retained. If set to `InfiniteRetention`,
 	// messages will be retained as long as the bytes retained for each partition
-	// is below `PerPartitionBytes`.
+	// is below `PerPartitionBytes`. Otherwise, must be > 0.
 	RetentionDuration time.Duration
 }
 
