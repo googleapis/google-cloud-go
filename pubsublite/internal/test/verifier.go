4ff8602143b8a3fcc94c78437062b5a2fb760e79
tmdiep <tmdiep@google.com>
2020-11-17 17:58:30 +1100

    feat(pubsublite): Refactoring and unit tests for retryableStream (#3160)
    Minor refactoring and unit tests for retryableStream. Eliminated test flakiness in mock server.

--- pubsublite/internal/test/verifier.go
+++ pubsublite/internal/test/verifier.go
@@ -15,6 +15,7 @@ package test
 
 import (
 	"container/list"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -30,26 +31,70 @@ const (
 	blockWaitTimeout = 30 * time.Second
 )
 
-type rpcMetadata struct {
-	wantRequest   interface{}
-	retResponse   interface{}
-	retErr        error
-	blockResponse chan struct{}
+// Barrier is used to perform two-way synchronization betwen the server and
+// client (test) to ensure tests are deterministic.
+type Barrier struct {
+	// Used to block until the server is ready to send the response.
+	serverBlock chan struct{}
+	// Used to block until the client wants the server to send the response.
+	clientBlock chan struct{}
+	err         error
 }
 
-// wait until the `blockResponse` is released by the test, or a timeout occurs.
-// Returns immediately if there was no block.
-func (r *rpcMetadata) wait() error {
-	if r.blockResponse == nil {
-		return nil
+func newBarrier() *Barrier {
+	return &Barrier{
+		serverBlock: make(chan struct{}),
+		clientBlock: make(chan struct{}),
 	}
+}
+
+// Release should be called by the test.
+func (b *Barrier) Release() {
+	// Wait for the server to reach the barrier.
+	select {
+	case <-time.After(blockWaitTimeout):
+		// Note: avoid returning a retryable code to quickly terminate the test.
+		b.err = status.Errorf(codes.FailedPrecondition, "mockserver: server did not reach barrier within %v", blockWaitTimeout)
+	case <-b.serverBlock:
+	}
+
+	// Then close the client block.
+	close(b.clientBlock)
+}
+
+func (b *Barrier) serverWait() error {
+	if b.err != nil {
+		return b.err
+	}
+
+	// Close the server block to signal the server reaching the point where it is
+	// ready to send the response.
+	close(b.serverBlock)
+
+	// Wait for the test to release the client block.
 	select {
 	case <-time.After(blockWaitTimeout):
 		// Note: avoid returning a retryable code to quickly terminate the test.
 		return status.Errorf(codes.FailedPrecondition, "mockserver: test did not unblock response within %v", blockWaitTimeout)
-	case <-r.blockResponse:
+	case <-b.clientBlock:
 		return nil
 	}
+}
+
+type rpcMetadata struct {
+	wantRequest interface{}
+	retResponse interface{}
+	retErr      error
+	barrier     *Barrier
+}
+
+// wait until the barrier is released by the test, or a timeout occurs.
+// Returns immediately if there was no block.
+func (r *rpcMetadata) wait() error {
+	if r.barrier == nil {
+		return nil
+	}
+	return r.barrier.serverWait()
 }
 
 // RPCVerifier stores an queue of requests expected from the client, and the
@@ -71,6 +116,15 @@ func NewRPCVerifier(t *testing.T) *RPCVerifier {
 }
 
 // Push appends a new {request, response, error} tuple.
+//
+// Valid combinations for unary and streaming RPCs:
+// - {request, response, nil}
+// - {request, nil, error}
+//
+// Additional combinations for streams only:
+// - {nil, response, nil}: send a response without a request (e.g. messages).
+// - {nil, nil, error}: break the stream without a request.
+// - {request, nil, nil}: expect a request, but don't send any response.
 func (v *RPCVerifier) Push(wantRequest interface{}, retResponse interface{}, retErr error) {
 	v.mu.Lock()
 	defer v.mu.Unlock()
@@ -82,21 +136,21 @@ func (v *RPCVerifier) Push(wantRequest interface{}, retResponse interface{}, ret
 	})
 }
 
-// PushWithBlock is like Push, but returns a channel that the test should close
-// when it would like the response to be sent to the client. This is useful for
-// synchronizing with work that needs to be done on the client.
-func (v *RPCVerifier) PushWithBlock(wantRequest interface{}, retResponse interface{}, retErr error) chan struct{} {
+// PushWithBarrier is like Push, but returns a barrier that the test should call
+// Release when it would like the response to be sent to the client. This is
+// useful for synchronizing with work that needs to be done on the client.
+func (v *RPCVerifier) PushWithBarrier(wantRequest interface{}, retResponse interface{}, retErr error) *Barrier {
 	v.mu.Lock()
 	defer v.mu.Unlock()
 
-	block := make(chan struct{})
+	barrier := newBarrier()
 	v.rpcs.PushBack(&rpcMetadata{
-		wantRequest:   wantRequest,
-		retResponse:   retResponse,
-		retErr:        retErr,
-		blockResponse: block,
+		wantRequest: wantRequest,
+		retResponse: retResponse,
+		retErr:      retErr,
+		barrier:     barrier,
 	})
-	return block
+	return barrier
 }
 
 // Pop validates the received request with the next {request, response, error}
@@ -158,7 +212,11 @@ func (v *RPCVerifier) Flush() {
 	for elem := v.rpcs.Front(); elem != nil; elem = elem.Next() {
 		v.numCalls++
 		rpc, _ := elem.Value.(*rpcMetadata)
-		v.t.Errorf("call(%d): did not receive expected request:\n%v", v.numCalls, rpc.wantRequest)
+		if rpc.wantRequest != nil {
+			v.t.Errorf("call(%d): did not receive expected request:\n%v", v.numCalls, rpc.wantRequest)
+		} else {
+			v.t.Errorf("call(%d): unsent response:\n%v, err = (%v)", v.numCalls, rpc.retResponse, rpc.retErr)
+		}
 	}
 	v.rpcs.Init()
 }
@@ -195,17 +253,21 @@ func (sv *streamVerifiers) Pop() (*RPCVerifier, error) {
 	return v, nil
 }
 
-// keyedStreamVerifiers stores indexed streamVerifiers.
+func (sv *streamVerifiers) Flush() {
+	for elem := sv.verifiers.Front(); elem != nil; elem = elem.Next() {
+		v, _ := elem.Value.(*RPCVerifier)
+		v.Flush()
+	}
+}
+
+// keyedStreamVerifiers stores indexed streamVerifiers. Examples of keys:
+// "streamType:topic_path:partition".
 type keyedStreamVerifiers struct {
 	verifiers map[string]*streamVerifiers
 }
 
 func newKeyedStreamVerifiers() *keyedStreamVerifiers {
 	return &keyedStreamVerifiers{verifiers: make(map[string]*streamVerifiers)}
-}
-
-func (kv *keyedStreamVerifiers) Reset() {
-	kv.verifiers = make(map[string]*streamVerifiers)
 }
 
 func (kv *keyedStreamVerifiers) Push(key string, v *RPCVerifier) {
@@ -223,4 +285,98 @@ func (kv *keyedStreamVerifiers) Pop(key string) (*RPCVerifier, error) {
 		return nil, status.Error(codes.FailedPrecondition, "mockserver: unexpected connection with no configured responses")
 	}
 	return sv.Pop()
+}
+
+func (kv *keyedStreamVerifiers) Flush() {
+	for _, sv := range kv.verifiers {
+		sv.Flush()
+	}
+}
+
+// Verifiers contains RPCVerifiers for unary RPCs and streaming RPCs.
+type Verifiers struct {
+	t  *testing.T
+	mu sync.Mutex
+
+	// Global list of verifiers for all unary RPCs.
+	GlobalVerifier *RPCVerifier
+	// Stream verifiers by key.
+	streamVerifiers       *keyedStreamVerifiers
+	activeStreamVerifiers []*RPCVerifier
+}
+
+// NewVerifiers creates a new instance of Verifiers for a test.
+func NewVerifiers(t *testing.T) *Verifiers {
+	return &Verifiers{
+		t:               t,
+		GlobalVerifier:  NewRPCVerifier(t),
+		streamVerifiers: newKeyedStreamVerifiers(),
+	}
+}
+
+// streamType is used as a key prefix for keyedStreamVerifiers.
+type streamType string
+
+const (
+	publishStreamType    streamType = "publish"
+	subscribeStreamType  streamType = "subscribe"
+	commitStreamType     streamType = "commit"
+	assignmentStreamType streamType = "assignment"
+)
+
+func keyPartition(st streamType, path string, partition int) string {
+	return fmt.Sprintf("%s:%s:%d", st, path, partition)
+}
+
+func key(st streamType, path string) string {
+	return fmt.Sprintf("%s:%s", st, path)
+}
+
+// AddPublishStream adds verifiers for a publish stream.
+func (tv *Verifiers) AddPublishStream(topic string, partition int, streamVerifier *RPCVerifier) {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+	tv.streamVerifiers.Push(keyPartition(publishStreamType, topic, partition), streamVerifier)
+}
+
+// AddSubscribeStream adds verifiers for a subscribe stream.
+func (tv *Verifiers) AddSubscribeStream(subscription string, partition int, streamVerifier *RPCVerifier) {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+	tv.streamVerifiers.Push(keyPartition(subscribeStreamType, subscription, partition), streamVerifier)
+}
+
+// AddCommitStream adds verifiers for a commit stream.
+func (tv *Verifiers) AddCommitStream(subscription string, partition int, streamVerifier *RPCVerifier) {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+	tv.streamVerifiers.Push(keyPartition(commitStreamType, subscription, partition), streamVerifier)
+}
+
+// AddAssignmentStream adds verifiers for an assignment stream.
+func (tv *Verifiers) AddAssignmentStream(subscription string, streamVerifier *RPCVerifier) {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+	tv.streamVerifiers.Push(key(assignmentStreamType, subscription), streamVerifier)
+}
+
+func (tv *Verifiers) popStreamVerifier(key string) (*RPCVerifier, error) {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+	v, err := tv.streamVerifiers.Pop(key)
+	if v != nil {
+		tv.activeStreamVerifiers = append(tv.activeStreamVerifiers, v)
+	}
+	return v, err
+}
+
+func (tv *Verifiers) flush() {
+	tv.mu.Lock()
+	defer tv.mu.Unlock()
+
+	tv.GlobalVerifier.Flush()
+	tv.streamVerifiers.Flush()
+	for _, v := range tv.activeStreamVerifiers {
+		v.Flush()
+	}
 }
