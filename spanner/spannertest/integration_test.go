6fdd1568f4b535ee473ab3e53564b34187f11816
sryoya <sryoya0814@gmail.com>
2020-12-01 10:28:19 +0900

    recovered the test that had been deleted by mistake

--- spanner/spannertest/integration_test.go
+++ spanner/spannertest/integration_test.go
@@ -880,6 +880,16 @@ func TestIntegration_ReadsAndQueries(t *testing.T) {
 			},
 		},
 		{
+			// Another GROUP BY, but referring to an alias.
+			// Group by ID oddness, SUM over Tenure.
+			`SELECT ID&0x01 AS odd, SUM(Tenure) FROM Staff GROUP BY odd`,
+			nil,
+			[][]interface{}{
+				{int64(0), int64(19)}, // Daniel(ID=2, Tenure=11), Teal'c(ID=4, Tenure=8)
+				{int64(1), int64(25)}, // Jack(ID=1, Tenure=10), Sam(ID=3, Tenure=9), George(ID=5, Tenure=6)
+			},
+		},
+		{
 			`SELECT AVG(Height) FROM Staff WHERE ID <= 2`,
 			nil,
 			[][]interface{}{
