15dac73e4ea8cb90c4e1ba87b587a9d7e73d9337
Nicole Zhu <69952136+nicoleczhu@users.noreply.github.com>
2021-01-12 12:18:04 -0800

    ci(all): turn on testing significant changes since last merge (#3537)
    This now correctly tracks significant changes from the latest PR merged into master.

--- internal/kokoro/continuous.sh
+++ internal/kokoro/continuous.sh
@@ -107,7 +107,7 @@ exit_code=0
 
 if [[ $KOKORO_JOB_NAME == *"continuous"* ]]; then
   # Continuous jobs only run root tests & tests in submodules changed by the PR
-  SIGNIFICANT_CHANGES=$(git --no-pager diff --name-only master..HEAD | grep -Ev '(\.md$|^\.github)' || true)
+  SIGNIFICANT_CHANGES=$(git --no-pager diff --name-only $KOKORO_GIT_COMMIT^ | grep -Ev '(\.md$|^\.github)' || true)
   # CHANGED_DIRS is the list of significant top-level directories that changed,
   # but weren't deleted by the current PR. CHANGED_DIRS will be empty when run on master.
   CHANGED_DIRS=$(echo "$SIGNIFICANT_CHANGES" | tr ' ' '\n' | grep "/" | cut -d/ -f1 | sort -u | tr '\n' ' ' | xargs ls -d 2>/dev/null || true)
