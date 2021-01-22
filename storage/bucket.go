ea52a99fb38f8550cd730feb75feb901a4ce8524
Alexey Andreev <ava1280@yandex.ru>
2021-01-23 00:11:17 +0300

    feat(storage): add projection parameter for BucketHandle.Objects() (#3549)
    Co-authored-by: Chris Cotter <cjcotter@google.com>

--- storage/bucket.go
+++ storage/bucket.go
@@ -1204,7 +1204,11 @@ func (it *ObjectIterator) Next() (*ObjectAttrs, error) {
 func (it *ObjectIterator) fetch(pageSize int, pageToken string) (string, error) {
 	req := it.bucket.c.raw.Objects.List(it.bucket.name)
 	setClientHeader(req.Header())
-	req.Projection("full")
+	projection := it.query.Projection
+	if projection == ProjectionDefault {
+		projection = ProjectionFull
+	}
+	req.Projection(projection.String())
 	req.Delimiter(it.query.Delimiter)
 	req.Prefix(it.query.Prefix)
 	req.StartOffset(it.query.StartOffset)
