0965705444bd2951e390425f1e04a998c9f239aa
Cody Oss <6331106+codyoss@users.noreply.github.com>
2021-02-01 08:15:25 -0700

    feat(mediatranslation): start generating apiv1beta1 (#3636)

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -965,6 +965,14 @@ var microgenGapicConfigs = []*microgenConfig{
 		apiServiceConfigPath:  "google/cloud/domains/v1beta1/domains_v1beta1.yaml",
 		releaseLevel:          "beta",
 	},
+	{
+		inputDirectoryPath:    "google/cloud/mediatranslation/v1beta1",
+		pkg:                   "mediatranslation",
+		importPath:            "cloud.google.com/go/mediatranslation/apiv1beta1",
+		gRPCServiceConfigPath: "google/cloud/mediatranslation/v1beta1/mediatranslation_grpc_service_config.json",
+		apiServiceConfigPath:  "google/cloud/mediatranslation/v1beta1/mediatranslation_v1beta1.yaml",
+		releaseLevel:          "beta",
+	},
 
 	// Non-Cloud APIs
 	{
