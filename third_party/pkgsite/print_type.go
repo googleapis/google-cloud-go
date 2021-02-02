ded60723f1d1f99c76b0f81288fc57f5c02d6ff8
Tyler Bui-Palsulich <tbp@google.com>
2021-02-02 15:23:59 -0500

    fix: don't link struct type field names

--- third_party/pkgsite/print_type.go
+++ third_party/pkgsite/print_type.go
@@ -183,6 +183,14 @@ func generateAnchorLinks(decl ast.Decl, toURL func(string, string) string) map[*
 			ignore[node.Name] = true // E.g., "func NoLink() int"
 		case *ast.TypeSpec:
 			ignore[node.Name] = true // E.g., "type NoLink int"
+			switch t := node.Type.(type) {
+			case *ast.StructType:
+				for _, f := range t.Fields.List {
+					for _, fName := range f.Names {
+						ignore[fName] = true
+					}
+				}
+			}
 		case *ast.ValueSpec:
 			for _, n := range node.Names {
 				ignore[n] = true // E.g., "var NoLink1, NoLink2 int"
