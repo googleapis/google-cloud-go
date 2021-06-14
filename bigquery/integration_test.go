5cf735c0af940ffecb2b5e299f3170eb3e0548e4
shollyman <shollyman@google.com>
2021-06-14 13:14:50 -0700

    feat(bigquery): support job deletion (#3935)
    * feat(bigquery): support job deletion

--- bigquery/integration_test.go
+++ bigquery/integration_test.go
@@ -2582,6 +2582,29 @@ func TestIntegration_ListJobs(t *testing.T) {
 	}
 }
 
+func TestIntegration_DeleteJob(t *testing.T) {
+	if client == nil {
+		t.Skip("Integration tests skipped")
+	}
+	ctx := context.Background()
+
+	q := client.Query("SELECT 17 as foo")
+	q.Location = "us-east1"
+
+	job, err := q.Run(ctx)
+	if err != nil {
+		t.Fatalf("job Run failure: %v", err)
+	}
+	_, err = job.Wait(ctx)
+	if err != nil {
+		t.Fatalf("job completion failure: %v", err)
+	}
+
+	if err := job.Delete(ctx); err != nil {
+		t.Fatalf("job.Delete failed: %v", err)
+	}
+}
+
 const tokyo = "asia-northeast1"
 
 func TestIntegration_Location(t *testing.T) {
