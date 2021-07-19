375470cb53e65d40cc4755563af63b41d65020a2
Cody Oss <codyoss@google.com>
2021-07-19 13:52:35 -0600

    fix(internal): detect module for properly generating docs URLs
    Fixes: #4447

--- internal/gapicgen/execv/gocmd/gocmd.go
+++ internal/gapicgen/execv/gocmd/gocmd.go
@@ -90,3 +90,15 @@ func Vet(dir string) error {
 	c.Dir = dir
 	return c.Run()
 }
+
+func CurrentMod(dir string) (string, error) {
+	log.Println("detecting current module")
+	c := execv.Command("go", "list", "-m")
+	c.Dir = dir
+	var out []byte
+	var err error
+	if out, err = c.Output(); err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
