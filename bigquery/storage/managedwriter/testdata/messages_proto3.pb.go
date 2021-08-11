e58a850ad04dc4739796d691e8ef88531651ebd6
Seth Hollyman <shollyman@google.com>
2021-08-11 00:02:26 +0000

    goimports again

--- bigquery/storage/managedwriter/testdata/messages_proto3.pb.go
+++ bigquery/storage/managedwriter/testdata/messages_proto3.pb.go
@@ -21,12 +21,13 @@
 package testdata
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/golang/protobuf/proto"
 	wrappers "github.com/golang/protobuf/ptypes/wrappers"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
