e9f9c59c7d4291042babee0ab872b5411c1f5697
Daniel Sanche <sanche@google.com>
2021-08-11 11:35:39 -0700

    chore(tests): fixed kubectl curl link

--- internal/kokoro/environment.sh
+++ internal/kokoro/environment.sh
@@ -70,7 +70,7 @@ fi
 
 # If Kubernetes, install kubectl component
 if [[ $ENVIRONMENT == *"kubernetes"* ]]; then
-  curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+  curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   chmod +x kubectl
   mkdir -p ~/.local/bin
   mv ./kubectl ~/.local/bin
