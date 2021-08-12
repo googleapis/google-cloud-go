d2b70f65a90b6d291e69ab86f1e554575e801967
Seth Hollyman <shollyman@google.com>
2021-08-12 20:25:20 +0000

    remove benchmarks copy from adapt

--- bigquery/storage/managedwriter/adapt/protoconversion_test.go
+++ bigquery/storage/managedwriter/adapt/protoconversion_test.go
@@ -16,13 +16,9 @@ package adapt
 
 import (
 	"encoding/json"
-	"fmt"
 	"reflect"
 	"testing"
-	"time"
 
-	"cloud.google.com/go/bigquery"
-	"cloud.google.com/go/bigquery/storage/managedwriter/testdata"
 	"github.com/google/go-cmp/cmp"
 	storagepb "google.golang.org/genproto/googleapis/cloud/bigquery/storage/v1beta2"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -32,7 +28,6 @@ import (
 	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/dynamicpb"
-	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 func TestSchemaToProtoConversion(t *testing.T) {
@@ -354,191 +349,4 @@ func TestProtoJSONSerialization(t *testing.T) {
 		t.Fatalf("mismatched json: got\n%q\nwant\n%q", gotBytes, sampleRecord)
 	}
 
-}
-
-var benchDescriptor protoreflect.Descriptor
-
-func BenchmarkStorageSchemaToDescriptor(b *testing.B) {
-	syntaxLabels := []string{"proto2", "proto3"}
-	for _, bm := range []struct {
-		name string
-		in   bigquery.Schema
-	}{
-		{
-			name: "SingleField",
-			in: bigquery.Schema{
-				{Name: "field", Type: bigquery.StringFieldType},
-			},
-		},
-		{
-			name: "NestedRecord",
-			in: bigquery.Schema{
-				{Name: "field1", Type: bigquery.StringFieldType},
-				{Name: "field2", Type: bigquery.IntegerFieldType},
-				{Name: "field3", Type: bigquery.BooleanFieldType},
-				{
-					Name: "field4",
-					Type: bigquery.RecordFieldType,
-					Schema: bigquery.Schema{
-						{Name: "recordfield1", Type: bigquery.GeographyFieldType},
-						{Name: "recordfield2", Type: bigquery.TimestampFieldType},
-					},
-				},
-			},
-		},
-		{
-			name: "SimpleMessage",
-			in:   testdata.SimpleMessageSchema,
-		},
-		{
-			name: "GithubArchiveSchema",
-			in:   testdata.GithubArchiveSchema,
-		},
-	} {
-		for _, s := range syntaxLabels {
-			b.Run(fmt.Sprintf("%s-%s", bm.name, s), func(b *testing.B) {
-				convSchema, err := BQSchemaToStorageTableSchema(bm.in)
-				if err != nil {
-					b.Errorf("%q: schema conversion fail: %v", bm.name, err)
-				}
-				for n := 0; n < b.N; n++ {
-					dc := make(dependencyCache)
-					benchDescriptor, err = storageSchemaToDescriptorInternal(convSchema, "root", &dc, s == "proto3")
-					if err != nil {
-						b.Errorf("failed to convert %q: %v", bm.name, err)
-					}
-				}
-			})
-		}
-	}
-}
-
-var staticBytes []byte
-
-func BenchmarkStaticProtoSerialization(b *testing.B) {
-	for _, bm := range []struct {
-		name    string
-		in      bigquery.Schema
-		syntax  string
-		setterF func() protoreflect.ProtoMessage
-	}{
-		{
-			name: "SimpleMessageProto2",
-			setterF: func() protoreflect.ProtoMessage {
-				return &testdata.SimpleMessageProto2{
-					Name:  proto.String(fmt.Sprintf("test-%d", time.Now().UnixNano())),
-					Value: proto.Int64(time.Now().UnixNano()),
-				}
-			},
-		},
-		{
-			name: "SimpleMessageProto3",
-			setterF: func() protoreflect.ProtoMessage {
-				return &testdata.SimpleMessageProto3{
-					Name:  fmt.Sprintf("test-%d", time.Now().UnixNano()),
-					Value: &wrapperspb.Int64Value{Value: time.Now().UnixNano()},
-				}
-			},
-		},
-		{
-			name: "GithubArchiveProto2",
-			setterF: func() protoreflect.ProtoMessage {
-				nowNano := time.Now().UnixNano()
-				return &testdata.GithubArchiveMessageProto2{
-					Type:    proto.String("SomeEvent"),
-					Public:  proto.Bool(nowNano%2 == 0),
-					Payload: proto.String(fmt.Sprintf("stuff %d", nowNano)),
-					Repo: &testdata.GithubArchiveRepoProto2{
-						Id:   proto.Int64(nowNano),
-						Name: proto.String("staticname"),
-						Url:  proto.String(fmt.Sprintf("foo.com/%d", nowNano)),
-					},
-					Actor: &testdata.GithubArchiveEntityProto2{
-						Id:         proto.Int64(nowNano % 1000),
-						Login:      proto.String(fmt.Sprintf("login-%d", nowNano%1000)),
-						GravatarId: proto.String(fmt.Sprintf("grav-%d", nowNano%1000000)),
-						AvatarUrl:  proto.String(fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)),
-						Url:        proto.String(fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)),
-					},
-					Org: &testdata.GithubArchiveEntityProto2{
-						Id:         proto.Int64(nowNano % 1000),
-						Login:      proto.String(fmt.Sprintf("login-%d", nowNano%1000)),
-						GravatarId: proto.String(fmt.Sprintf("grav-%d", nowNano%1000000)),
-						AvatarUrl:  proto.String(fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)),
-						Url:        proto.String(fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)),
-					},
-					CreatedAt: proto.Int64(nowNano),
-					Id:        proto.String(fmt.Sprintf("id%d", nowNano)),
-					Other:     proto.String("other"),
-				}
-			},
-		},
-		{
-			// Only set a single top-level field in a larger message.
-			name: "GithubArchiveProto2_Sparse",
-			setterF: func() protoreflect.ProtoMessage {
-				nowNano := time.Now().UnixNano()
-				return &testdata.GithubArchiveMessageProto2{
-					Id: proto.String(fmt.Sprintf("id%d", nowNano)),
-				}
-			},
-		},
-		{
-			name: "GithubArchiveProto3",
-			setterF: func() protoreflect.ProtoMessage {
-				nowNano := time.Now().UnixNano()
-				return &testdata.GithubArchiveMessageProto3{
-					Type:    &wrapperspb.StringValue{Value: "SomeEvent"},
-					Public:  &wrapperspb.BoolValue{Value: nowNano%2 == 0},
-					Payload: &wrapperspb.StringValue{Value: fmt.Sprintf("stuff %d", nowNano)},
-					Repo: &testdata.GithubArchiveRepoProto3{
-						Id:   &wrapperspb.Int64Value{Value: nowNano},
-						Name: &wrapperspb.StringValue{Value: "staticname"},
-						Url:  &wrapperspb.StringValue{Value: fmt.Sprintf("foo.com/%d", nowNano)},
-					},
-					Actor: &testdata.GithubArchiveEntityProto3{
-						Id:         &wrapperspb.Int64Value{Value: nowNano % 1000},
-						Login:      &wrapperspb.StringValue{Value: fmt.Sprintf("login-%d", nowNano%1000)},
-						GravatarId: &wrapperspb.StringValue{Value: fmt.Sprintf("grav-%d", nowNano%1000000)},
-						AvatarUrl:  &wrapperspb.StringValue{Value: fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)},
-						Url:        &wrapperspb.StringValue{Value: fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)},
-					},
-					Org: &testdata.GithubArchiveEntityProto3{
-						Id:         &wrapperspb.Int64Value{Value: nowNano % 1000},
-						Login:      &wrapperspb.StringValue{Value: fmt.Sprintf("login-%d", nowNano%1000)},
-						GravatarId: &wrapperspb.StringValue{Value: fmt.Sprintf("grav-%d", nowNano%1000000)},
-						AvatarUrl:  &wrapperspb.StringValue{Value: fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)},
-						Url:        &wrapperspb.StringValue{Value: fmt.Sprintf("https://something.com/img/%d", nowNano%10000000)},
-					},
-					CreatedAt: &wrapperspb.Int64Value{Value: nowNano},
-					Id:        &wrapperspb.StringValue{Value: fmt.Sprintf("id%d", nowNano)},
-					Other:     &wrapperspb.StringValue{Value: "other"},
-				}
-			},
-		},
-		{
-			// Only set a single field in a larger message.
-			name: "GithubArchiveProto3_Sparse",
-			setterF: func() protoreflect.ProtoMessage {
-				nowNano := time.Now().UnixNano()
-				return &testdata.GithubArchiveMessageProto3{
-					Id: &wrapperspb.StringValue{Value: fmt.Sprintf("id%d", nowNano)},
-				}
-			},
-		},
-	} {
-		b.Run(bm.name, func(b *testing.B) {
-			var totalBytes int64
-			for n := 0; n < b.N; n++ {
-				m := bm.setterF()
-				out, err := proto.Marshal(m)
-				if err != nil {
-					b.Errorf("%q %q: Marshal: %v", bm.name, bm.syntax, err)
-				}
-				totalBytes = totalBytes + int64(len(out))
-				staticBytes = out
-			}
-			b.Logf("N=%d, avg bytes/message: %d", b.N, totalBytes/int64(b.N))
-		})
-	}
 }
