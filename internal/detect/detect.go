9b9808c4a1700e6b9333f9be5c0a08e5f4894222
Cody Oss <codyoss@google.com>
2021-08-13 10:26:36 -0600

    add tests

--- internal/detect/detect.go
+++ internal/detect/detect.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package detect is used find information from the environment.
 package detect
 
@@ -7,11 +21,20 @@ import (
 	"fmt"
 	"os"
 
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/transport"
 )
 
-const projectIDSentinel = "*detect-project-id*"
+const (
+	projectIDSentinel = "*detect-project-id*"
+	envProjectID      = "GOOGLE_CLOUD_PROJECT"
+)
+
+var (
+	adcLookupFunc func(context.Context, ...option.ClientOption) (*google.Credentials, error) = transport.Creds
+	envLookupFunc func(string) string                                                        = os.Getenv
+)
 
 // ProjectID tries to detect the project ID from the environment if the sentinel
 // value, "*detect-project-id*", is sent. It looks in the following order:
@@ -23,16 +46,16 @@ func ProjectID(ctx context.Context, projectID string, emulatorEnvVar string, opt
 		return projectID, nil
 	}
 	// 1. Try a well known environment variable
-	if id := os.Getenv("GOOGLE_CLOUD_PROJECT"); id != "" {
+	if id := envLookupFunc(envProjectID); id != "" {
 		return id, nil
 	}
 	// 2. Try ADC
-	creds, err := transport.Creds(ctx, opts...)
+	creds, err := adcLookupFunc(ctx, opts...)
 	if err != nil {
 		return "", fmt.Errorf("fetching creds: %v", err)
 	}
 	// 3. If ADC does not work, and the environment is emulated, return a const value.
-	if creds.ProjectID == "" && emulatorEnvVar != "" && os.Getenv(emulatorEnvVar) != "" {
+	if creds.ProjectID == "" && emulatorEnvVar != "" && envLookupFunc(emulatorEnvVar) != "" {
 		return "emulated-project", nil
 	}
 	// 4. If 1-3 don't work, error out
