07343afc15085b164cc41d202d13f9d46f5c0d02
Rajitha Avantsa <7590492+anrajitha@users.noreply.github.com>
2021-09-29 02:26:06 +0530

    feat(storage): add projectNumber field to bucketAttrs. (#4805)
    * feat(storage): add projectNumber field to bucketAttrs.
    
    * Added check to integration Test.
    
    * Added check to integration Test.
    
    * added check in creating/deleting with attributes
    
    Co-authored-by: Brenna N Epp <brennae@google.com>

--- storage/integration_test.go
+++ storage/integration_test.go
@@ -265,6 +265,9 @@ func TestIntegration_BucketMethods(t *testing.T) {
 	if attrs.LocationType == "" {
 		t.Error("got an empty LocationType")
 	}
+	if attrs.ProjectNumber == 0 {
+		t.Errorf("got a zero ProjectNumber")
+	}
 	h.mustDeleteBucket(b)
 
 	// Test Create and Delete with attributes.
@@ -330,6 +333,9 @@ func TestIntegration_BucketMethods(t *testing.T) {
 	}
 	if attrs.LocationType == "" {
 		t.Error("got an empty LocationType")
+	}
+	if attrs.ProjectNumber == 0 {
+		t.Errorf("got a zero ProjectNumber")
 	}
 	h.mustDeleteBucket(b)
 }
