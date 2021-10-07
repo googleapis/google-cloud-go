e51aff98da95063a5d47c7f62171eb1faa445cfb
Noah Dietz <noahdietz@users.noreply.github.com>
2021-10-07 16:12:32 -0700

    chore(storage): temporarily skip grpc tests (#4964)
    First step of #4963.

--- storage/integration_test.go
+++ storage/integration_test.go
@@ -776,6 +776,7 @@ func TestIntegration_ObjectsRangeReader(t *testing.T) {
 }
 
 func TestIntegration_ObjectReadGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to upload test data and a gRPC client to test with.
@@ -836,6 +837,7 @@ func TestIntegration_ObjectReadGRPC(t *testing.T) {
 }
 
 func TestIntegration_ObjectReadChunksGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to upload test data and a gRPC client to test with.
@@ -900,6 +902,7 @@ func TestIntegration_ObjectReadChunksGRPC(t *testing.T) {
 }
 
 func TestIntegration_ObjectReadRelativeToEndGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to upload test data and a gRPC client to test with.
@@ -963,6 +966,7 @@ func TestIntegration_ObjectReadRelativeToEndGRPC(t *testing.T) {
 }
 
 func TestIntegration_ObjectReadPartialContentGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to upload test data and a gRPC client to test with.
@@ -1016,6 +1020,7 @@ func TestIntegration_ObjectReadPartialContentGRPC(t *testing.T) {
 }
 
 func TestIntegration_ConditionalDownloadGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to upload test data and a gRPC client to test with.
@@ -1052,6 +1057,7 @@ func TestIntegration_ConditionalDownloadGRPC(t *testing.T) {
 }
 
 func TestIntegration_SimpleWriteGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to read test data and a gRPC client to test write
@@ -1110,6 +1116,7 @@ func TestIntegration_SimpleWriteGRPC(t *testing.T) {
 }
 
 func TestIntegration_CancelWriteGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to verify test and a gRPC client to test writing.
@@ -1159,6 +1166,7 @@ func TestIntegration_CancelWriteGRPC(t *testing.T) {
 }
 
 func TestIntegration_MultiMessageWriteGRPC(t *testing.T) {
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to read test data and a gRPC client to test write
@@ -1219,7 +1227,7 @@ func TestIntegration_MultiMessageWriteGRPC(t *testing.T) {
 }
 
 func TestIntegration_MultiChunkWriteGRPC(t *testing.T) {
-	// t.Skip()
+	t.Skip("https://github.com/googleapis/google-cloud-go/issues/4963")
 	ctx := context.Background()
 
 	// Create an HTTP client to read test data and a gRPC client to test write
