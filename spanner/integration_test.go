dc942bba70c0c1e10bb9cef49f39e89b6bf1d2db
Rahul Yadav <irahul@google.com>
2021-10-12 10:25:09 +0530

    fix(spanner): fix flaky TestIntegration_StartBackupOperation test

--- spanner/integration_test.go
+++ spanner/integration_test.go
@@ -3171,8 +3171,8 @@ func TestIntegration_StartBackupOperation(t *testing.T) {
 	skipEmulatorTest(t)
 	t.Parallel()
 
-	// Backups can be slow, so use a 30 minute timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+	// Backups can be slow, so use 1 hour timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
 	defer cancel()
 	_, testDatabaseName, cleanup := prepareIntegrationTest(ctx, t, DefaultSessionPoolConfig, backuDBStatements)
 	defer cleanup()
