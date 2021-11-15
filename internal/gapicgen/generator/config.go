571701c4325e67588e4b3871093dd4e1a04b035a
Cody Oss <codyoss@google.com>
2021-11-15 10:39:46 -0700

    feat(contactcenterinsights): to v1
    Release-As: 1.0.0

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -1289,8 +1289,7 @@ var microgenGapicConfigs = []*microgenConfig{
 		importPath:            "cloud.google.com/go/contactcenterinsights/apiv1",
 		gRPCServiceConfigPath: "contactcenterinsights_grpc_service_config.json",
 		apiServiceConfigPath:  "contactcenterinsights_v1.yaml",
-		// GA after 2021/10/11
-		releaseLevel: "beta",
+		releaseLevel: "ga",
 	},
 	{
 		inputDirectoryPath:    "google/cloud/orchestration/airflow/service/v1",
