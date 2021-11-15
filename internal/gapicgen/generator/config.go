8a8158d6d9380b0533ec83f7c93b1ddee37b7c86
Cody Oss <codyoss@google.com>
2021-11-15 10:35:41 -0700

    feat(accesscontextmanager): to v1

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -1280,8 +1280,7 @@ var microgenGapicConfigs = []*microgenConfig{
 		importPath:            "cloud.google.com/go/accesscontextmanager/apiv1",
 		gRPCServiceConfigPath: "accesscontextmanager_grpc_service_config.json",
 		apiServiceConfigPath:  "accesscontextmanager_v1.yaml",
-		// GA after 2021/10/11
-		releaseLevel: "beta",
+		releaseLevel:          "ga",
 	},
 	{
 		inputDirectoryPath:    "google/cloud/contactcenterinsights/v1",
