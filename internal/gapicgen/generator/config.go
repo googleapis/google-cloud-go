164bd78706c4e15923c5acf3ae4e6257ee21006e
Cody Oss <6331106+codyoss@users.noreply.github.com>
2022-01-25 13:12:39 -0700

    feat(networkconnectivity): to v1 (#5141)
    Release-As: 1.0.0

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -806,8 +806,7 @@ var microgenGapicConfigs = []*microgenConfig{
 		importPath:            "cloud.google.com/go/networkconnectivity/apiv1",
 		gRPCServiceConfigPath: "networkconnectivity_v1_grpc_service_config.json",
 		apiServiceConfigPath:  "networkconnectivity_v1.yaml",
-		// GA after 2021/10/11
-		releaseLevel: "beta",
+		releaseLevel:          "ga",
 	},
 	{
 		inputDirectoryPath:    "google/cloud/networkconnectivity/v1alpha1",
