ab881288034daf9bd00d202b99c37f9eb3177357
Cody Oss <6331106+codyoss@users.noreply.github.com>
2022-01-25 13:13:05 -0700

    feat(deploy): to v1 (#5140)
    Release-As: 1.0.0

--- deploy/CHANGES.md
+++ deploy/CHANGES.md
@@ -1,5 +1,6 @@
 # Changes
 
+
 ## v0.1.0
 
 - feat(deploy): start generating clients
--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -1313,8 +1313,7 @@ var microgenGapicConfigs = []*microgenConfig{
 		importPath:            "cloud.google.com/go/deploy/apiv1",
 		gRPCServiceConfigPath: "clouddeploy_grpc_service_config.json",
 		apiServiceConfigPath:  "clouddeploy_v1.yaml",
-		// GA after 2021/11/08
-		releaseLevel: "beta",
+		releaseLevel:          "ga",
 	},
 	{
 		inputDirectoryPath:    "google/cloud/ids/v1",
