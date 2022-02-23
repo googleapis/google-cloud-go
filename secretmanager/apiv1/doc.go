22201c94671f6e01d2652536c0a80e3c9dfa56f8
Cody Oss <codyoss@google.com>
2022-02-23 09:53:11 -0700

    remove edited files

--- secretmanager/apiv1/doc.go
+++ secretmanager/apiv1/doc.go
@@ -91,7 +91,7 @@ import (
 type clientHookParams struct{}
 type clientHook func(context.Context, clientHookParams) ([]option.ClientOption, error)
 
-var versionClient string
+const versionClient = "20220219"
 
 func insertMetadata(ctx context.Context, mds ...metadata.MD) context.Context {
 	out, _ := metadata.FromOutgoingContext(ctx)
