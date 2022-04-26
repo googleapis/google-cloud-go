49c008318a46b223b61da345cf6c2c1d3cd3418f
Thanh-Mai Diep <tmdiep@google.com>
2022-04-26 06:07:56 +0000

    Fix integration test

--- pubsublite/pscompat/integration_test.go
+++ pubsublite/pscompat/integration_test.go
@@ -34,7 +34,6 @@ import (
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/xerrors"
 	"google.golang.org/api/option"
-	"google.golang.org/grpc/codes"
 
 	vkit "cloud.google.com/go/pubsublite/apiv1"
 	pb "google.golang.org/genproto/googleapis/cloud/pubsublite/v1"
@@ -218,18 +217,16 @@ func waitForPublishResults(t *testing.T, pubResults []*pubsub.PublishResult) {
 	cancel()
 }
 
-func parseMessageMetadata(ctx context.Context, t *testing.T, result *pubsub.PublishResult) *MessageMetadata {
+func parseMessageMetadata(ctx context.Context, t *testing.T, result *pubsub.PublishResult) (*MessageMetadata, error) {
 	id, err := result.Get(ctx)
 	if err != nil {
-		t.Fatalf("Failed to publish message: %v", err)
-		return nil
+		return nil, err
 	}
 	metadata, err := ParseMessageMetadata(id)
 	if err != nil {
 		t.Fatalf("Failed to parse message metadata: %v", err)
-		return nil
 	}
-	return metadata
+	return metadata, err
 }
 
 func makeMsgTracker(msgs []string) *test.MsgTracker {
@@ -586,15 +583,15 @@ func TestIntegration_PublishSubscribeSinglePartition(t *testing.T) {
 
 		cancel()
 
-		wantCode := codes.Canceled
+		wantErr := context.Canceled
 		result := publisher.Publish(ctx, &pubsub.Message{Data: []byte("cancel_publisher_context")})
-		if _, err := result.Get(ctx); !test.ErrorHasCode(err, wantCode) {
-			t.Errorf("Publish() got err: %v, want code: %v", err, wantCode)
+		if _, gotErr := result.Get(ctx); !xerrors.Is(gotErr, wantErr) {
+			t.Errorf("Publish() got err: %v, want err: %v", gotErr, wantErr)
 		}
 
 		publisher.Stop()
-		if err := xerrors.Unwrap(publisher.Error()); !test.ErrorHasCode(err, wantCode) {
-			t.Errorf("Error() got err: %v, want code: %v", err, wantCode)
+		if gotErr := xerrors.Unwrap(publisher.Error()); !xerrors.Is(gotErr, wantErr) {
+			t.Errorf("Error() got err: %v, want err: %v", gotErr, wantErr)
 		}
 	})
 
@@ -612,8 +609,8 @@ func TestIntegration_PublishSubscribeSinglePartition(t *testing.T) {
 			cancel()
 		})
 
-		if err, wantCode := xerrors.Unwrap(subsErr), codes.Canceled; !test.ErrorHasCode(err, wantCode) {
-			t.Errorf("Receive() got err: %v, want code: %v", err, wantCode)
+		if gotErr, wantErr := xerrors.Unwrap(subsErr), context.Canceled; !xerrors.Is(gotErr, wantErr) {
+			t.Errorf("Receive() got err: %v, want err: %v", gotErr, wantErr)
 		}
 	})
 
@@ -622,7 +619,8 @@ func TestIntegration_PublishSubscribeSinglePartition(t *testing.T) {
 	// correctly by publishers.
 	t.Run("IncreasePartitions", func(t *testing.T) {
 		// Create the publisher client with the initial single partition.
-		const pollPeriod = 5 * time.Second
+		const pollPeriod = 10 * time.Second
+		const configPropagationDelay = 30 * time.Second
 		pubSettings := DefaultPublishSettings
 		pubSettings.configPollPeriod = pollPeriod // Poll updates more frequently
 		publisher := publisherClient(context.Background(), t, pubSettings, topicPath)
@@ -637,15 +635,25 @@ func TestIntegration_PublishSubscribeSinglePartition(t *testing.T) {
 			t.Errorf("Failed to increase partitions: %v", err)
 		}
 
-		// Wait for the publisher client to receive the updated partition count.
-		time.Sleep(3 * pollPeriod)
+		// Wait for the new config to propagate.
+		time.Sleep(configPropagationDelay)
 
 		// Publish 2 messages, which should be routed to different partitions
 		// (round robin).
 		result1 := publisher.Publish(ctx, &pubsub.Message{Data: []byte("increase-partitions-1")})
 		result2 := publisher.Publish(ctx, &pubsub.Message{Data: []byte("increase-partitions-2")})
-		metadata1 := parseMessageMetadata(ctx, t, result1)
-		metadata2 := parseMessageMetadata(ctx, t, result2)
+		// Tolerate test flakiness, as the new config may not have propagated on the
+		// server.
+		metadata1, err := parseMessageMetadata(ctx, t, result1)
+		if err != nil {
+			t.Logf("Warning: failed to publish message: %v. Publisher error: %v", err, publisher.Error())
+			return
+		}
+		metadata2, err := parseMessageMetadata(ctx, t, result2)
+		if err != nil {
+			t.Logf("Warning: failed to publish message: %v. Publisher error: %v", err, publisher.Error())
+			return
+		}
 		if metadata1.Partition == metadata2.Partition {
 			t.Errorf("Messages were published to the same partition = %d. Expected different partitions", metadata1.Partition)
 		}
