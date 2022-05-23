723faf055e2e392fcc2ac2af900aee75e72f7f0c
Chris Smith <chrisdsmith@google.com>
2022-05-23 12:41:31 -0600

    feat(vision/v2): Release breaking changes as v2 module (#6072)
    BREAKING CHANGE: removed fields from SafeSearchAnnotation in googleapis/go-genproto#809

--- internal/gapicgen/generator/config_test.go
+++ internal/gapicgen/generator/config_test.go
@@ -37,6 +37,7 @@ var packagePathExceptions = map[string]bool{
 	"cloud.google.com/go/firestore/apiv1/admin":        true,
 	"cloud.google.com/go/recaptchaenterprise/v2/apiv1": true,
 	"cloud.google.com/go/storage/internal/apiv2":       true,
+	"cloud.google.com/go/vision/v2/apiv1":              true,
 }
 
 // TestMicrogenConfigs validates config entries.
