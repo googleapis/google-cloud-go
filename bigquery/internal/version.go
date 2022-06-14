b302f321f9e7ac9d8bfc0e096d9a54e122216faa
Seth Hollyman <shollyman@google.com>
2022-06-14 04:12:03 +0000

    chore(bigquery): cleanup some lint errors
    This PR addresses linter/tricorder errors surfaced when importing this
    library internally.  See cl/454723109 for examples.

--- bigquery/internal/version.go
+++ bigquery/internal/version.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This internal package is used to manage versioning of the released library.
 package internal
 
 // Version is the current tagged release of the library.
