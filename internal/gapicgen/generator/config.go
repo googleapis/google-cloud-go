eb31729977e79b78fdfffc3880d96d88f027c486
Chris Smith <chrisdsmith@google.com>
2022-06-21 15:52:01 -0600

    chore(binaryauthorization): release 1.0.0
    Release-As: 1.0

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -1430,8 +1430,7 @@ var MicrogenGapicConfigs = []*MicrogenConfig{
 		ImportPath:            "cloud.google.com/go/binaryauthorization/apiv1",
 		GRPCServiceConfigPath: "binaryauthorization_grpc_service_config.json",
 		ApiServiceConfigPath:  "binaryauthorization_v1.yaml",
-		// GA after 2022/06/05
-		ReleaseLevel: "beta",
+		ReleaseLevel:          "ga",
 	},
 	{
 		InputDirectoryPath:    "google/cloud/datastream/v1",
