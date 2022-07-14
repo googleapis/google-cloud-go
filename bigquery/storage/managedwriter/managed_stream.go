097b29e69b10d43613d7d1ab990422eab6ca5b58
Seth Hollyman <shollyman@google.com>
2022-07-14 02:25:55 +0000

    fix(bigquery/storage/managedwriter): improve network reconnection
    Issuing a sufficiently large single append request is enough to trigger
    the server backend to close an existing grpc stream.  This PR addresses
    the problem by allowing a failed request to signal that subsequent
    requests should request a new grpc stream connection.
    
    This PR also adds an integration test that induces the failure by
    issuing a large request, and ensures subsequent requests succeed.
    
    Towards: https://github.com/googleapis/google-cloud-go/issues/6321

--- bigquery/storage/managedwriter/managed_stream.go
+++ bigquery/storage/managedwriter/managed_stream.go
@@ -84,6 +84,7 @@ type ManagedStream struct {
 
 	mu          sync.Mutex
 	arc         *storagepb.BigQueryWrite_AppendRowsClient // current stream connection
+	reconnect   bool                                      // Request a reconnect before issuing another send.
 	err         error                                     // terminal error
 	pending     chan *pendingWrite                        // writes awaiting status
 	streamSetup *sync.Once                                // handles amending the first request in a new stream
@@ -184,6 +185,11 @@ func (ms *ManagedStream) getStream(arc *storagepb.BigQueryWrite_AppendRowsClient
 		return nil, nil, ms.err
 	}
 
+	// Previous activity on the stream indicated it is not healthy, so propagate that as a reconnect.
+	if ms.reconnect {
+		forceReconnect = true
+		ms.reconnect = false
+	}
 	// Always return the retained ARC if the arg differs.
 	if arc != ms.arc && !forceReconnect {
 		return ms.arc, ms.pending, nil
@@ -297,6 +303,11 @@ func (ms *ManagedStream) lockingAppend(requestCtx context.Context, pw *pendingWr
 		err = (*arc).Send(pw.request)
 	}
 	if err != nil {
+		// Transient connection loss.  If we got io.EOF from a send, we want subsequent appends to
+		// reconnect the network connection for the stream.
+		if err == io.EOF {
+			ms.reconnect = true
+		}
 		return 0, err
 	}
 	// Compute numRows, once we pass ownership to the channel the request may be
