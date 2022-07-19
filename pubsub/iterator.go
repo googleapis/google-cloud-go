a4d9bfce126dfef4df9076f7c51ba290271d2b85
Noah Dietz <noahdietz@users.noreply.github.com>
2022-07-19 12:03:11 -0700

    Revert "fix(pubsub): make receipt modack call async (#6335)"
    This reverts commit 01596d4a8861129be890d735b34841e71f362a58.

--- pubsub/iterator.go
+++ pubsub/iterator.go
@@ -246,13 +246,11 @@ func (it *messageIterator) receive(maxToPull int32) ([]*Message, error) {
 	}
 	deadline := it.ackDeadline()
 	it.mu.Unlock()
-	go func() {
-		if len(ackIDs) > 0 {
-			// Don't check the return value of this since modacks are fire and forget,
-			// meaning errors should not be propagated to the client.
-			it.sendModAck(ackIDs, deadline)
+	if len(ackIDs) > 0 {
+		if !it.sendModAck(ackIDs, deadline) {
+			return nil, it.err
 		}
-	}()
+	}
 	return msgs, nil
 }
 
