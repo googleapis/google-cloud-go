9ec2c37fbab4aa7f2f38bb1c375af7ec3bb6f53e
Cathy Ouyang <cathyo@google.com>
2022-08-26 11:18:27 -0700

    revert changes to NewRangeReader after testbench fix

--- storage/http_client.go
+++ storage/http_client.go
@@ -849,7 +849,7 @@ func (c *httpStorageClient) NewRangeReader(ctx context.Context, params *newRange
 					start > 0 && params.length != 0 &&
 					res.StatusCode != http.StatusPartialContent
 
-			if partialContentNotSatisfied && res.StatusCode != http.StatusOK {
+			if partialContentNotSatisfied {
 				res.Body.Close()
 				return errors.New("storage: partial request not satisfied")
 			}
