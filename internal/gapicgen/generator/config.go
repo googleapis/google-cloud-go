d75375b99816811db95f967c2bc184bf9133e1b2
Chris Smith <chrisdsmith@google.com>
2022-09-01 14:55:49 -0600

    feat(iam): start generating apiv2

--- internal/gapicgen/generator/config.go
+++ internal/gapicgen/generator/config.go
@@ -1704,6 +1704,15 @@ var MicrogenGapicConfigs = []*MicrogenConfig{
 		// GA after 2022/10/01
 		ReleaseLevel: "beta",
 	},
+	{
+		InputDirectoryPath:    "google/iam/v2",
+		Pkg:                   "iam",
+		ImportPath:            "cloud.google.com/go/iam/apiv2",
+		GRPCServiceConfigPath: "iam_grpc_service_config.json",
+		ApiServiceConfigPath:  "iam_v2.yaml",
+		// GA after 2022/10/01
+		ReleaseLevel: "beta",
+	},
 
 	// Non-Cloud APIs
 	{
