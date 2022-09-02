5f42545a860e1125cb608a632b7dbb70668e70e0
Anshul Goyal <anshulgoyal@google.com>
2022-09-02 16:15:10 +0530

    feat: PG_JSONB protoutils change

--- spanner/protoutils.go
+++ spanner/protoutils.go
@@ -78,7 +78,7 @@ func pgNumericType() *sppb.Type {
 }
 
 func jsonType() *sppb.Type {
-	return &sppb.Type{Code: sppb.TypeCode_JSON}
+	return &sppb.Type{Code: sppb.TypeCode_JSON, TypeAnnotation: sppb.TypeAnnotationCode_PG_JSONB}
 }
 
 func bytesProto(b []byte) *proto3.Value {
