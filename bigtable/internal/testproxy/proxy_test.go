5b6641e68f448d6dd11ecf172093e6f879cc5cf0
Eric Schmidt <erschmid@google.com>
2022-09-02 16:13:03 -0700

    feat: added RemoveClient and test

--- bigtable/internal/testproxy/proxy_test.go
+++ bigtable/internal/testproxy/proxy_test.go
@@ -32,6 +32,7 @@ func bufDialer(context.Context, string) (net.Conn, error) {
 }
 
 func TestCreateClient(t *testing.T) {
+	cid := "testCreateClient"
 	ctx := context.Background()
 	conn, err := grpc.DialContext(ctx, "testproxy", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
 	if err != nil {
@@ -41,7 +42,7 @@ func TestCreateClient(t *testing.T) {
 	client := pb.NewCloudBigtableV2TestProxyClient(conn)
 
 	req := &pb.CreateClientRequest{
-		ClientId:   "unitTestClient",
+		ClientId:   cid,
 		ProjectId:  "fakeProject",
 		DataTarget: "fakeTarget",
 		InstanceId: "fakeInstance",
@@ -51,5 +52,50 @@ func TestCreateClient(t *testing.T) {
 	if err != nil {
 		t.Fatalf("testproxy test: CreateClient() failed: %v", err)
 	}
-	t.Logf("Response: %+v", resp)
+	t.Logf("testproxy test: CreateClient() response: %+v", resp)
+
+	// Teardown
+	_, err = client.RemoveClient(ctx, &pb.RemoveClientRequest{
+		ClientId:  cid,
+		CancelAll: true,
+	})
+
+	if err != nil {
+		t.Errorf("testproxy test: CreateClient() teardown failed: %v", err)
+	}
+}
+
+func TestRemoveClient(t *testing.T) {
+	// Setup
+	cid := "testRemoveClient"
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "testproxy", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("testproxy test: failed to dial testproxy: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewCloudBigtableV2TestProxyClient(conn)
+
+	req := &pb.CreateClientRequest{
+		ClientId:   cid,
+		ProjectId:  "fakeProject",
+		DataTarget: "fakeTarget",
+		InstanceId: "fakeInstance",
+	}
+
+	_, err = client.CreateClient(ctx, req)
+	if err != nil {
+		t.Fatalf("testproxy test: failed to create client: %v", err)
+	}
+
+	// Test
+	resp, err := client.RemoveClient(ctx, &pb.RemoveClientRequest{
+		ClientId:  cid,
+		CancelAll: true,
+	})
+
+	if err != nil {
+		t.Errorf("testproxy test: RemoveClient() failed: %v", err)
+	}
+	t.Logf("testproxy test: RemoveClient() response: %+v", resp)
 }
