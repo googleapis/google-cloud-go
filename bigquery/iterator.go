de3dff22bc829b3c2772c1ef5c8161000ab00dbf
Alvaro Viebrantz <aviebrantz@google.com>
2022-10-25 16:24:12 -0400

    fix(bigquery): iterator value loader refactor issue

--- bigquery/iterator.go
+++ bigquery/iterator.go
@@ -131,25 +131,46 @@ type pageFetcher func(ctx context.Context, _ *rowSource, _ Schema, startIndex ui
 // NullDateTime. You can also use a *[]Value or *map[string]Value to read from a
 // table with NULLs.
 func (it *RowIterator) Next(dst interface{}) error {
+	vl, err := resolveDefaultValueLoader(dst)
+	if err != nil {
+		return err
+	}
 	if err := it.nextFunc(); err != nil {
 		return err
 	}
 	row := it.rows[0]
 	it.rows = it.rows[1:]
 
-	vl, err := resolveValueLoader(dst, it.Schema, it.structLoader)
-	if err != nil {
-		return err
+	if vl == nil {
+		// This can only happen if dst is a pointer to a struct. We couldn't
+		// set vl above because we need the schema.
+		if err := it.structLoader.set(dst, it.Schema); err != nil {
+			return err
+		}
+		vl = &it.structLoader
 	}
 	return vl.Load(row, it.Schema)
 }
 
 // ResolveValueLoader returns a ValueLoader from the destination type and table schema
 func ResolveValueLoader(dst interface{}, schema Schema) (ValueLoader, error) {
-	return resolveValueLoader(dst, schema, structLoader{})
+	vl, err := resolveDefaultValueLoader(dst)
+	if err != nil {
+		return nil, err
+	}
+	if vl == nil {
+		sloader := structLoader{}
+		// This can only happen if dst is a pointer to a struct. We couldn't
+		// set vl above because we need the schema.
+		if err := sloader.set(dst, schema); err != nil {
+			return nil, err
+		}
+		vl = &sloader
+	}
+	return vl, nil
 }
 
-func resolveValueLoader(dst interface{}, schema Schema, sloader structLoader) (ValueLoader, error) {
+func resolveDefaultValueLoader(dst interface{}) (ValueLoader, error) {
 	var vl ValueLoader
 	switch dst := dst.(type) {
 	case ValueLoader:
@@ -162,14 +183,6 @@ func resolveValueLoader(dst interface{}, schema Schema, sloader structLoader) (V
 		if !isStructPtr(dst) {
 			return nil, fmt.Errorf("bigquery: cannot convert %T to ValueLoader (need pointer to []Value, map[string]Value, or struct)", dst)
 		}
-	}
-	if vl == nil {
-		// This can only happen if dst is a pointer to a struct. We couldn't
-		// set vl above because we need the schema.
-		if err := sloader.set(dst, schema); err != nil {
-			return nil, err
-		}
-		vl = &sloader
 	}
 	return vl, nil
 }
