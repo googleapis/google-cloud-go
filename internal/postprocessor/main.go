4a4eef1b508248a8fb6ed32a87ed2f793909ced7
Adriana <guadriana@google.com>
2022-10-26 11:34:44 -0400

    Make edits according to code review

--- internal/postprocessor/main.go
+++ internal/postprocessor/main.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -33,9 +47,6 @@ func main() {
 }
 
 func run(srcPrefix, dstPrefix string) error {
-	var srcPath string
-	var dstPath string
-
 	filepath.WalkDir(srcPrefix, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -45,10 +56,9 @@ func run(srcPrefix, dstPrefix string) error {
 			return nil
 		}
 
-		srcPath = path
-		dstPath = filepath.Join(dstPrefix, strings.TrimPrefix(path, srcPrefix))
+		dstPath := filepath.Join(dstPrefix, strings.TrimPrefix(path, srcPrefix))
 
-		if err := copyFiles(srcPath, dstPath); err != nil {
+		if err := copyFiles(path, dstPath); err != nil {
 			return err
 		}
 
