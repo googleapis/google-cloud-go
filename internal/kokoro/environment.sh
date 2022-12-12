89de7f71770f8728401f18f900823011ebd0c698
Daniel Sanche <d.sanche14@gmail.com>
2022-12-12 09:50:47 -0800

    install auth plugin

--- internal/kokoro/environment.sh
+++ internal/kokoro/environment.sh
@@ -86,6 +86,8 @@ if [[ $ENVIRONMENT == *"kubernetes"* ]]; then
   mkdir -p ~/.local/bin
   mv ./kubectl ~/.local/bin
   export PATH=$PATH:~/.local/bin/
+  # install auth plugin
+  apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 fi
 
 # If Functions, use python3.8, since that's what's in go116 container
