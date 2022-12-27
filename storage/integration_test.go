9745eb4d2edc63e63d7c3ae17a0baf2aede21b92
Chris Cotter <cjcotter@google.com>
2022-12-27 17:02:04 -0500

    fmt

--- storage/integration_test.go
+++ storage/integration_test.go
@@ -265,8 +265,8 @@ func initTransportClients(ctx context.Context, t *testing.T, opts ...option.Clie
 // of an existing bucket to use, a bucket name to use for bucket creation, and
 // the client to use.
 func multiTransportTest(ctx context.Context, t *testing.T,
-		test func(*testing.T, context.Context, string, string, *Client),
-		opts ...option.ClientOption) {
+	test func(*testing.T, context.Context, string, string, *Client),
+	opts ...option.ClientOption) {
 	for transport, client := range initTransportClients(ctx, t, opts...) {
 		t.Run(transport, func(t *testing.T) {
 			defer client.Close()
@@ -5147,8 +5147,8 @@ func retryOnTransient400and403(err error) bool {
 	var e *googleapi.Error
 	var ae *apierror.APIError
 	return ShouldRetry(err) ||
-			/* http */ errors.As(err, &e) && (e.Code == 400 || e.Code == 403) ||
-			/* grpc */ errors.As(err, &ae) && (ae.GRPCStatus().Code() == codes.InvalidArgument || ae.GRPCStatus().Code() == codes.PermissionDenied)
+		/* http */ errors.As(err, &e) && (e.Code == 400 || e.Code == 403) ||
+		/* grpc */ errors.As(err, &ae) && (ae.GRPCStatus().Code() == codes.InvalidArgument || ae.GRPCStatus().Code() == codes.PermissionDenied)
 }
 
 func skipGRPC(reason string) context.Context {
