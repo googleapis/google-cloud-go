166054ff58165a61db4364187fdd3bb62492dd7f
Seth Hollyman <shollyman@google.com>
2022-12-28 19:19:32 +0000

    switch to gax.Sleep

--- bigquery/storage/managedwriter/connection.go
+++ bigquery/storage/managedwriter/connection.go
@@ -182,7 +182,7 @@ func (cp *connectionPool) processRetry(pw *pendingWrite, srcConn *connection, ap
 			pw.markDone(appendResp, err, srcConn.fc)
 			return
 		}
-		time.Sleep(pause)
+		gax.Sleep(pause)
 		err = pw.writer.appendWithRetry(pw)
 		if err != nil {
 			// Re-enqueue failed, send it through the loop again.
