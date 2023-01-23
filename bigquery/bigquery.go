26c04f4cd5083b4aa3c219500572d3af2f291645
Alvaro Viebrantz <aviebrantz@google.com>
2023-01-23 16:22:12 -0400

    feat(bigquery): use storage api for query jobs (#6822)
    Initial work on using the Storage API for fetching results of a query. This is more efficient because it can download data in parallel by splitting the read session and using Arrow as a more efficient format. The API surface for users stay the same, with them being able to transform query results into user defined structs. Under the hood the library will take care of converting data represented in Arrow to the user defined struct.
    
    One thing to note is that this introduces the first external dependency on the Apache Arrow Go library.
    
    Initially we are gonna use it as an experimental feature and explicit ask users to create a `bqStorage.BigQueryReadClient`. 
    
    Proposed by issue https://togithub.com/googleapis/google-cloud-go/issues/3880 and work on the Python library https://medium.com/google-cloud/announcing-google-cloud-bigquery-version-1-17-0-1fc428512171

--- bigquery/bigquery.go
+++ bigquery/bigquery.go
@@ -57,6 +57,7 @@ type Client struct {
 
 	projectID string
 	bqs       *bq.Service
+	rc        *readClient
 }
 
 // DetectProjectID is a sentinel value that instructs NewClient to detect the
@@ -97,6 +98,21 @@ func NewClient(ctx context.Context, projectID string, opts ...option.ClientOptio
 	return c, nil
 }
 
+// EnableStorageReadClient sets up Storage API connection to be used when fetching
+// large datasets from tables, jobs or queries.
+// Calling this method twice will return an error.
+func (c *Client) EnableStorageReadClient(ctx context.Context, opts ...option.ClientOption) error {
+	if c.rc != nil {
+		return fmt.Errorf("failed: storage read client already set up")
+	}
+	rc, err := newReadClient(ctx, c.projectID, opts...)
+	if err != nil {
+		return err
+	}
+	c.rc = rc
+	return nil
+}
+
 // Project returns the project ID or number for this instance of the client, which may have
 // either been explicitly specified or autodetected.
 func (c *Client) Project() string {
@@ -107,6 +123,12 @@ func (c *Client) Project() string {
 // Close should be called when the client is no longer needed.
 // It need not be called at program exit.
 func (c *Client) Close() error {
+	if c.rc != nil {
+		err := c.rc.close()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
