428e46a9ed11f4a102c49090550c1c1be0c76e5a
Brenna N Epp <brennae@google.com>
2023-02-15 16:20:11 -0800

    chore(storage): adjust GRPC reader remain - off by one (#7420)

--- storage/grpc_client.go
+++ storage/grpc_client.go
@@ -861,13 +861,6 @@ func (c *grpcStorageClient) NewRangeReader(ctx context.Context, params *newRange
 		ctx = setUserProjectMetadata(ctx, s.userProject)
 	}
 
-	// A negative length means "read to the end of the object", but the
-	// read_limit field it corresponds to uses zero to mean the same thing. Thus
-	// we coerce the length to 0 to read to the end of the object.
-	if params.length < 0 {
-		params.length = 0
-	}
-
 	b := bucketResourceName(globalProjectAlias, params.bucket)
 	req := &storagepb.ReadObjectRequest{
 		Bucket:                    b,
@@ -890,15 +883,20 @@ func (c *grpcStorageClient) NewRangeReader(ctx context.Context, params *newRange
 
 		cc, cancel := context.WithCancel(ctx)
 
-		start := params.offset + seen
+		req.ReadOffset = params.offset + seen
+
+		// A negative length means "read to the end of the object", but the
+		// read_limit field it corresponds to uses zero to mean the same thing. Thus
+		// we coerce the length to 0 to read to the end of the object.
+		if params.length < 0 {
+			params.length = 0
+		}
 
 		// Only set a ReadLimit if length is greater than zero, because zero
 		// means read it all.
 		if params.length > 0 {
 			req.ReadLimit = params.length - seen
 		}
-
-		req.ReadOffset = start
 
 		if err := applyCondsProto("gRPCReader.reopen", params.gen, params.conds, req); err != nil {
 			cancel()
@@ -971,7 +969,7 @@ func (c *grpcStorageClient) NewRangeReader(ctx context.Context, params *newRange
 	cr := msg.GetContentRange()
 	if cr != nil {
 		r.Attrs.StartOffset = cr.GetStart()
-		r.remain = cr.GetEnd() - cr.GetStart() + 1
+		r.remain = cr.GetEnd() - cr.GetStart()
 	} else {
 		r.remain = size
 	}
