fe1c6fb18cca6b35604c353e802273c2bd18b5fd
Seth Hollyman <shollyman@google.com>
2023-03-08 21:45:46 +0000

    proto imports

--- bigquery/storage/managedwriter/testdata/validation_proto2.pb.go
+++ bigquery/storage/managedwriter/testdata/validation_proto2.pb.go
@@ -21,11 +21,12 @@
 package testdata
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	_ "google.golang.org/genproto/googleapis/cloud/bigquery/storage/v1"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
