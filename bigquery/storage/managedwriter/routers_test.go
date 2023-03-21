aad14ee833b25d85b0905fabf47858a52f8b89cd
Seth Hollyman <shollyman@google.com>
2023-03-21 20:11:21 +0000

    add routing benchmark

--- bigquery/storage/managedwriter/routers_test.go
+++ bigquery/storage/managedwriter/routers_test.go
@@ -16,6 +16,7 @@ package managedwriter
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"cloud.google.com/go/bigquery/storage/apiv1/storagepb"
@@ -112,6 +113,131 @@ func TestSharedRouter_Basic(t *testing.T) {
 	}
 	if _, err := pool.router.pickConnection(pw); err == nil {
 		t.Errorf("pickConnection: expected error, got success")
+	}
+}
+
+func BenchmarkRouting(b *testing.B) {
+
+	for _, bm := range []struct {
+		desc                string
+		router              poolRouter
+		numWriters          int
+		numMultiplexWriters int
+	}{
+		{
+			desc:                "SimpleRouter",
+			router:              newSimpleRouter(""),
+			numWriters:          1,
+			numMultiplexWriters: 1,
+		},
+		{
+			desc:                "SimpleRouter",
+			router:              newSimpleRouter(""),
+			numWriters:          10,
+			numMultiplexWriters: 10,
+		},
+		{
+			desc:                "SharedRouterNoMultiplex",
+			router:              newSharedRouter(false, 0),
+			numWriters:          1,
+			numMultiplexWriters: 1,
+		},
+		{
+			desc:                "SharedRouterNoMultiplex",
+			router:              newSharedRouter(false, 0),
+			numWriters:          10,
+			numMultiplexWriters: 10,
+		},
+		{
+			desc:                "SharedRouterMultiplex1conn",
+			router:              newSharedRouter(true, 1),
+			numWriters:          1,
+			numMultiplexWriters: 1,
+		},
+		{
+			desc:                "SharedRouterMultiplex1conn",
+			router:              newSharedRouter(true, 1),
+			numWriters:          10,
+			numMultiplexWriters: 10,
+		},
+		{
+			desc:                "SharedRouterMultiplex1conn",
+			router:              newSharedRouter(true, 1),
+			numWriters:          50,
+			numMultiplexWriters: 50,
+		},
+		{
+			desc:                "SharedRouterMultiplex10conn",
+			router:              newSharedRouter(true, 10),
+			numWriters:          50,
+			numMultiplexWriters: 50,
+		},
+	} {
+
+		ctx, cancel := context.WithCancel(context.Background())
+		pool := &connectionPool{
+			ctx:    ctx,
+			cancel: cancel,
+			open: func(opts ...gax.CallOption) (storagepb.BigQueryWrite_AppendRowsClient, error) {
+				return &testAppendRowsClient{}, nil
+			},
+		}
+		if err := pool.activateRouter(bm.router); err != nil {
+			b.Errorf("%q: activateRouter: %v", bm.desc, err)
+		}
+
+		// setup both explicit and default stream writers.
+		var writers []*ManagedStream
+		for i := 0; i < bm.numWriters; i++ {
+			wCtx, wCancel := context.WithCancel(ctx)
+			writer := &ManagedStream{
+				id:             newUUID(writerIDPrefix),
+				streamSettings: &streamSettings{streamID: "projects/foo/datasets/bar/tables/baz/streams/abc123"},
+				ctx:            wCtx,
+				cancel:         wCancel,
+				retry:          newStatelessRetryer(),
+			}
+			writers = append(writers, writer)
+		}
+		for i := 0; i < bm.numMultiplexWriters; i++ {
+			wCtx, wCancel := context.WithCancel(ctx)
+			writer := &ManagedStream{
+				id:             newUUID(writerIDPrefix),
+				streamSettings: &streamSettings{streamID: "projects/foo/datasets/bar/tables/baz/streams/_default"},
+
+				ctx:    wCtx,
+				cancel: wCancel,
+				retry:  newStatelessRetryer(),
+			}
+			writers = append(writers, writer)
+		}
+
+		for k, writer := range writers {
+			if err := pool.addWriter(writer); err != nil {
+				b.Errorf("addWriter %d: %v", k, err)
+			}
+		}
+		b.Logf("%d writers %d %d", len(writers), bm.numWriters, bm.numMultiplexWriters)
+
+		benchName := fmt.Sprintf("%s_%dexwriters_%dmpwriters", bm.desc, bm.numWriters, bm.numMultiplexWriters)
+		b.Log(benchName)
+		b.Run(benchName, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				// route each writer once
+				writer := writers[0]
+				pw := newPendingWrite(context.Background(), writer, &storagepb.AppendRowsRequest{}, nil, "", "")
+				if _, err := bm.router.pickConnection(pw); err != nil {
+					b.Errorf("pickConnection: %v", err)
+				}
+			}
+		})
+
+		for _, writer := range writers {
+			writer.Close()
+		}
+		pool.Close()
+
 	}
 
 }
