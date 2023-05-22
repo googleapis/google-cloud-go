2ff1cb779ad4a34205bb2185e99bd61ed4cd7978
Cody Oss <codyoss@google.com>
2023-05-22 10:13:30 -0500

    chore: fixup imports after version bump

--- internal/gapicgen/git/github.go
+++ internal/gapicgen/git/github.go
@@ -26,7 +26,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/internal/gapicgen/execv"
-	"github.com/google/go-github/v50/github"
+	"github.com/google/go-github/v52/github"
 	"golang.org/x/oauth2"
 )
 
