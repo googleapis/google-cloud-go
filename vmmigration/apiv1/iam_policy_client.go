a1b299e3f6eb01fd807eda65d2f1130589342c6f
Noah Dietz <ndietz@google.com>
2023-06-14 17:17:54 +0000

    chore: cleanup deadcode

--- vmmigration/apiv1/iam_policy_client.go
+++ vmmigration/apiv1/iam_policy_client.go
@@ -230,9 +230,6 @@ type iamPolicyGRPCClient struct {
 	// Connection pool of gRPC connections to the service.
 	connPool gtransport.ConnPool
 
-	// flag to opt out of default deadlines via GOOGLE_API_GO_EXPERIMENTAL_DISABLE_DEFAULT_DEADLINE
-	disableDeadlines bool
-
 	// Points back to the CallOptions field of the containing IamPolicyClient
 	CallOptions **IamPolicyCallOptions
 
@@ -281,11 +278,6 @@ func NewIamPolicyClient(ctx context.Context, opts ...option.ClientOption) (*IamP
 		clientOpts = append(clientOpts, hookOpts...)
 	}
 
-	disableDeadlines, err := checkDisableDeadlines()
-	if err != nil {
-		return nil, err
-	}
-
 	connPool, err := gtransport.DialPool(ctx, append(clientOpts, opts...)...)
 	if err != nil {
 		return nil, err
@@ -294,8 +286,7 @@ func NewIamPolicyClient(ctx context.Context, opts ...option.ClientOption) (*IamP
 
 	c := &iamPolicyGRPCClient{
 		connPool:         connPool,
-		disableDeadlines: disableDeadlines,
-		iamPolicyClient:  iampb.NewIAMPolicyClient(connPool),
+				iamPolicyClient:  iampb.NewIAMPolicyClient(connPool),
 		CallOptions:      &client.CallOptions,
 		operationsClient: longrunningpb.NewOperationsClient(connPool),
 		locationsClient:  locationpb.NewLocationsClient(connPool),
