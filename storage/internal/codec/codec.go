4975ba5c55b05bf9d5ebadc4916b825aeb0266d6
Chris Cotter <cjcotter@google.com>
2024-01-29 20:32:29 +0000

    bug fix, add print logging

--- storage/internal/codec/codec.go
+++ storage/internal/codec/codec.go
@@ -3,7 +3,9 @@ package codec
 import (
 	"fmt"
 	"log"
+	"reflect"
 
+	"cloud.google.com/go/storage/internal/apiv2/storagepb"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -11,7 +13,8 @@ import (
 // Replaces standard implementation at https://pkg.go.dev/google.golang.org/grpc/encoding/proto
 
 // Name is the name registered for the proto compressor.
-const Name = "proto"
+// Only "" works for now; seems to be a bug.
+const Name = ""
 
 // func init() {
 // 	encoding.RegisterCodec(ReadObjectCodec{})
@@ -22,7 +25,7 @@ type ReadObjectCodec struct{}
 
 func (ReadObjectCodec) Marshal(v any) ([]byte, error) {
 	vv, ok := v.(proto.Message)
-	log.Printf("marshaling %v", vv.ProtoReflect().Descriptor())
+	// log.Printf("marshaling %v", vv.ProtoReflect().Descriptor())
 	if !ok {
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
 	}
@@ -30,8 +33,15 @@ func (ReadObjectCodec) Marshal(v any) ([]byte, error) {
 }
 
 func (ReadObjectCodec) Unmarshal(data []byte, v any) error {
+	log.Printf("type: %v", reflect.TypeOf(v))
+	r, ok := v.(*storagepb.ReadObjectResponse)
+	if ok {
+		// Special case for avoiding buffer copies in downloads.
+		log.Printf("read object resp %v", r.ContentRange)
+	}
+
 	vv, ok := v.(proto.Message)
-	log.Printf("marshaling %v", vv.ProtoReflect().Descriptor())
+	// log.Printf("marshaling %v", vv.ProtoReflect().Descriptor())
 
 	if !ok {
 		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
