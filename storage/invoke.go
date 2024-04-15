5c3944c49766000b4d91b6b0eb00651017d8df44
Chris Cotter <cjcotter@google.com>
2024-04-15 19:25:12 +0000

    fix wrapping on last attempt and invoke test

--- storage/invoke.go
+++ storage/invoke.go
@@ -70,7 +70,7 @@ func run(ctx context.Context, call func(ctx context.Context) error, retry *retry
 	return internal.Retry(ctx, bo, func() (stop bool, err error) {
 		ctxWithHeaders := setInvocationHeaders(ctx, invocationID, attempts)
 		err = call(ctxWithHeaders)
-		if retry.maxAttempts != nil && attempts >= *retry.maxAttempts {
+		if err != nil && retry.maxAttempts != nil && attempts >= *retry.maxAttempts {
 			return true, fmt.Errorf("storage: retry failed after %v attempts; last error: %w", *retry.maxAttempts, err)
 		}
 		attempts++
