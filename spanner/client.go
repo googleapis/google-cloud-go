googleapis/google-cloud-go

commit 480f9a0ea8e159299dd3f909e2c0d8b5e771c580

feat(bigtable/spanner): remove grpclb (#9186)
Co-authored-by: Cody Oss <6331106+codyoss@users.noreply.github.com>
Co-authored-by: Baha Aiman <bahaaiman@google.com>

spanner/client.go
@@ -351,7 +351,6 @@ func allClientOpts(numChannels int, compression string, userOpts ...option.Clien
 	clientDefaultOpts := []option.ClientOption{
 		option.WithGRPCConnectionPool(numChannels),
 		option.WithUserAgent(fmt.Sprintf("spanner-go/v%s", internal.Version)),
-		internaloption.EnableDirectPath(true),
 		internaloption.AllowNonDefaultServiceAccount(true),
 	}
 	if enableDirectPathXds, _ := strconv.ParseBool(os.Getenv("GOOGLE_SPANNER_ENABLE_DIRECT_ACCESS")); enableDirectPathXds {

bigtable/bigtable.go
@@ -85,9 +85,7 @@ func NewClientWithConfig(ctx context.Context, project, instance string, config C
 		// Set the max size to correspond to server-side limits.
 		option.WithGRPCDialOption(grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(1<<28), grpc.MaxCallRecvMsgSize(1<<28))),
 	)
-	// Attempts direct access to spanner service over gRPC to improve throughput,
-	// whether the attempt is allowed is totally controlled by service owner.
-	o = append(o, internaloption.EnableDirectPath(true))
+
 	// Allow non-default service account in DirectPath.
 	o = append(o, internaloption.AllowNonDefaultServiceAccount(true))
 	o = append(o, opts...)

commit ec99f4cccf40676cb8416fecba628bd5e451aa74

fix: set EnableDirectPath to when DP is turned on for Spanner.
This is broken by https://github.com/googleapis/google-cloud-go/pull/9186.
We need to explicitly set `EnableDirectPath` to true.

spanner/client.go
@@ -354,7 +354,7 @@ func allClientOpts(numChannels int, compression string, userOpts ...option.Clien
 		internaloption.AllowNonDefaultServiceAccount(true),
 	}
 	if enableDirectPathXds, _ := strconv.ParseBool(os.Getenv("GOOGLE_SPANNER_ENABLE_DIRECT_ACCESS")); enableDirectPathXds {
-		clientDefaultOpts = append(clientDefaultOpts, internaloption.EnableDirectPathXds())
+		clientDefaultOpts = append(clientDefaultOpts, internaloption.EnableDirectPath(true), internaloption.EnableDirectPathXds())
 	}
 	if compression == "gzip" {
 		userOpts = append(userOpts, option.WithGRPCDialOption(grpc.WithDefaultCallOptions(