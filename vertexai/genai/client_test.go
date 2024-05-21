c1c6064eaa42ab54677199665b6321a48c6aee69
Jonathan Amsterdam <jba@google.com>
2024-05-21 14:51:58 -0400

    chore(vertexai): use cross-language tests
    Replace the tests for inferFullModelName and inferLocation
    with cross-language tests.
    
    These tests are written in YAML so they can be parsed and run
    in any language with a suitable implementation.
    
    This is the first use of my new cross-language test design
    and implementation, github.com/jba/xltest.

--- vertexai/genai/client_test.go
+++ vertexai/genai/client_test.go
@@ -27,6 +27,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/jba/xltest/go/xltest"
 	"google.golang.org/api/iterator"
 )
 
@@ -612,44 +613,19 @@ func TestIntFloatConversions(t *testing.T) {
 }
 
 func TestInferFullModelName(t *testing.T) {
-	for _, test := range []struct {
-		name string
-		want string
-	}{
-		{"xyz", "projects/proj/locations/loc/publishers/google/models/xyz"},
-		{"models/abc", "projects/proj/locations/loc/publishers/google/models/abc"},
-		{"publishers/foo/xyz", "projects/proj/locations/loc/publishers/foo/xyz"},
-		{"x/y/z", "x/y/z"},
-	} {
-		t.Run(test.name, func(t *testing.T) {
-			got := inferFullModelName("proj", "loc", test.name)
-			if got != test.want {
-				t.Errorf("got %q, want %q", got, test.want)
-			}
-		})
+	tst, err := xltest.ReadFile(filepath.Join("testdata", "model_name_test.yaml"))
+	if err != nil {
+		t.Fatal(err)
 	}
+	tst.Run(t, func(s string) string {
+		return inferFullModelName("proj", "loc", s)
+	}, nil)
 }
 
 func TestInferLocation(t *testing.T) {
-	for _, test := range []struct {
-		name                             string
-		arg                              string
-		cloudRegionEnv, cloudMlRegionEnv string
-		want                             string
-	}{
-		{"arg passed", "us-west4", "abc", "def", "us-west4"},
-		{"first env", "", "abc", "", "abc"},
-		{"second env", "", "", "klm", "klm"},
-		{"default", "", "", "", defaultLocation},
-		{"first env precedence", "", "101", "klm", "101"},
-	} {
-		t.Run(test.name, func(t *testing.T) {
-			t.Setenv("GOOGLE_CLOUD_REGION", test.cloudRegionEnv)
-			t.Setenv("CLOUD_ML_REGION", test.cloudMlRegionEnv)
-			got := inferLocation(test.arg)
-			if got != test.want {
-				t.Errorf("got %q, want %q", got, test.want)
-			}
-		})
+	tst, err := xltest.ReadFile(filepath.Join("testdata", "location_test.yaml"))
+	if err != nil {
+		t.Fatal(err)
 	}
+	tst.Run(t, inferLocation, nil)
 }
--- vertexai/go.mod
+++ vertexai/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	cloud.google.com/go v0.113.0
 	cloud.google.com/go/aiplatform v1.67.0
+	github.com/jba/xltest/go/xltest v0.1.1
 	google.golang.org/api v0.180.0
 	google.golang.org/genproto v0.0.0-20240401170217-c3f982113cda
 	google.golang.org/protobuf v1.34.1
@@ -21,9 +22,11 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
@@ -40,4 +43,5 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20240506185236-b8a5c65736ae // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240506185236-b8a5c65736ae // indirect
 	google.golang.org/grpc v1.63.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
