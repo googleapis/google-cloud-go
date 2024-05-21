675505b85c557e998fa20def2d9760485a46223c
Sri Harsha CH <sriharshach@google.com>
2024-05-21 09:50:20 +0000

    fix(spanner): fix vet

--- spanner/testdata/protos/singer.pb.go
+++ spanner/testdata/protos/singer.pb.go
@@ -24,7 +24,7 @@ package protos
 import (
 	reflect "reflect"
 	sync "sync"
-	
+
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
