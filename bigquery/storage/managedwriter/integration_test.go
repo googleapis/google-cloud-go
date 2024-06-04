35355ea3ea1f0b5ffc1d2f3fb891d28bd908728d
Seth Hollyman <shollyman@google.com>
2024-06-04 21:21:48 +0000

    testing(bigquery/storage/managedwriter): dynamic schema test fixes
    This PR changes the dynamic schema testing to look more like a realistic
    example.  Rather than build schemas from a reference bigquery.Schema, we
    use the GetWriteStream method to fetch schema for a stream, and build
    the necessary descriptor / descriptorproto based on the RPC.

--- bigquery/storage/managedwriter/integration_test.go
+++ bigquery/storage/managedwriter/integration_test.go
@@ -96,14 +96,18 @@ func setupTestDataset(ctx context.Context, t *testing.T, bqc *bigquery.Client, l
 	}, nil
 }
 
-// setupDynamicDescriptors aids testing when not using a supplied proto
-func setupDynamicDescriptors(t *testing.T, schema bigquery.Schema) (protoreflect.MessageDescriptor, *descriptorpb.DescriptorProto) {
-	convertedSchema, err := adapt.BQSchemaToStorageTableSchema(schema)
+// setupDynamicDescriptors aids testing when not using a supplied proto.
+func setupDynamicDescriptors(t *testing.T, ctx context.Context, c *Client, streamName string) (protoreflect.MessageDescriptor, *descriptorpb.DescriptorProto) {
+
+	resp, err := c.GetWriteStream(ctx, &storagepb.GetWriteStreamRequest{
+		Name: streamName,
+		View: storagepb.WriteStreamView_FULL,
+	})
 	if err != nil {
-		t.Fatalf("adapt.BQSchemaToStorageTableSchema: %v", err)
+		t.Fatalf("couldn't get write stream (%q): %v", streamName, err)
 	}
 
-	descriptor, err := adapt.StorageSchemaToProto2Descriptor(convertedSchema, "root")
+	descriptor, err := adapt.StorageSchemaToProto2Descriptor(resp.GetTableSchema(), "root")
 	if err != nil {
 		t.Fatalf("adapt.StorageSchemaToDescriptor: %v", err)
 	}
@@ -400,7 +404,8 @@ func testDefaultStreamDynamicJSON(ctx context.Context, t *testing.T, mwClient *C
 		t.Fatalf("failed to create test table %s: %v", testTable.FullyQualifiedName(), err)
 	}
 
-	md, descriptorProto := setupDynamicDescriptors(t, testdata.GithubArchiveSchema)
+	defStreamName := fmt.Sprintf("%s/streams/_default", TableParentFromParts(testTable.ProjectID, testTable.DatasetID, testTable.TableID))
+	md, descriptorProto := setupDynamicDescriptors(t, ctx, mwClient, defStreamName)
 
 	ms, err := mwClient.NewManagedStream(ctx,
 		WithDestinationTable(TableParentFromParts(testTable.ProjectID, testTable.DatasetID, testTable.TableID)),
