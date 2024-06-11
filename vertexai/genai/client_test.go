73e86bac9c3b1a5c987edd6d3f14af48d2cdb6e5
Eli Bendersky <eliben@gmail.com>
2024-06-11 06:57:44 -0700

    fix(vertexai): don't add empty Text parts to session history
    Fixes #10309

--- vertexai/genai/client_test.go
+++ vertexai/genai/client_test.go
@@ -284,6 +284,30 @@ func TestLive(t *testing.T) {
 			}
 			checkMatch(t, responseString(res), "(it's|it is|weather) .*cold")
 		})
+		t.Run("funcall-stream", func(t *testing.T) {
+			session := model.StartChat()
+			iter := session.SendMessageStream(ctx, Text("What is the weather like in New York?"))
+
+			for {
+				_, err := iter.Next()
+				if err == iterator.Done {
+					break
+				}
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			res, err := session.SendMessage(ctx, FunctionResponse{
+				Name: weatherTool.FunctionDeclarations[0].Name,
+				Response: map[string]any{
+					"weather_there": "cold",
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			checkMatch(t, responseString(res), "(it's|it is|weather) .*cold")
+		})
 		t.Run("funcall-none", func(t *testing.T) {
 			model.ToolConfig = &ToolConfig{
 				FunctionCallingConfig: &FunctionCallingConfig{
