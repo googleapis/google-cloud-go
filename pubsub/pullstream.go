05157a991a07623f0983927895bbe58b0ab2d3ce
Alex Hong <9397363+hongalex@users.noreply.github.com>
2024-06-26 11:52:06 -0700

    fix issue with subscription name argument

--- pubsub/pullstream.go
+++ pubsub/pullstream.go
@@ -42,7 +42,7 @@ type pullStream struct {
 // for testing
 type streamingPullFunc func(context.Context, ...gax.CallOption) (pb.Subscriber_StreamingPullClient, error)
 
-func newPullStream(ctx context.Context, streamingPull streamingPullFunc, clientID, subName string, maxOutstandingMessages, maxOutstandingBytes int, maxDurationPerLeaseExtension time.Duration) *pullStream {
+func newPullStream(ctx context.Context, streamingPull streamingPullFunc, subName, clientID string, maxOutstandingMessages, maxOutstandingBytes int, maxDurationPerLeaseExtension time.Duration) *pullStream {
 	ctx = withSubscriptionKey(ctx, subName)
 	hds := []string{"x-goog-request-params", fmt.Sprintf("%s=%v", "subscription", url.QueryEscape(subName))}
 	ctx = gax.InsertMetadataIntoOutgoingContext(ctx, hds...)
