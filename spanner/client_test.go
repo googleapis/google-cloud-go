dae357015aba3c0de1d5499bbd3906f1c0cbf6b7
Egon Elbre <egonelbre@gmail.com>
2024-08-22 16:44:46 +0300

    fix(spanner): check error before using client in test

--- spanner/client_test.go
+++ spanner/client_test.go
@@ -4725,10 +4725,10 @@ func TestClient_EmulatorWithCredentialsFile(t *testing.T) {
 		"localhost:1234",
 		opts...,
 	)
-	defer client.Close()
 	if err != nil {
 		t.Fatalf("Failed to create a client with credentials file when running against an emulator: %v", err)
 	}
+	defer client.Close()
 }
 
 func TestBatchReadOnlyTransaction_QueryOptions(t *testing.T) {
