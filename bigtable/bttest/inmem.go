a515e1ff4f602b75444b4cffdccce95012eed196
Ron Gal <rongal@google.com>
2024-08-23 19:31:48 +0000

    fix vet

--- bigtable/bttest/inmem.go
+++ bigtable/bttest/inmem.go
@@ -334,13 +334,12 @@ func (s *server) ModifyColumnFamilies(ctx context.Context, req *btapb.ModifyColu
 			})
 		} else if modify := mod.GetUpdate(); modify != nil {
 			newcf := newColumnFamily(req.Name+"/columnFamilies/"+mod.Id, 0, modify)
-
-			if cf, ok := tbl.families[mod.Id]; !ok {
+			cf, ok := tbl.families[mod.Id]
+			if  !ok {
 				return nil, fmt.Errorf("no such family %q", mod.Id)
-			} else {
-				if cf.valueType != newcf.valueType {
-					return nil, status.Errorf(codes.InvalidArgument, "Immutable fields 'value_type' cannot be updated")
-				}
+			} 
+			if !Equal(cf.valueType, newcf.valueType) {
+				return nil, status.Errorf(codes.InvalidArgument, "Immutable fields 'value_type' cannot be updated")
 			}
 
 			// assume that we ALWAYS want to replace by the new setting
