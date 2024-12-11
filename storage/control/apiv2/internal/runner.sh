4a9291edea6b4d66280d22e4424af9285406c047
Frank Natividad <franknatividad@google.com>
2024-12-11 11:03:16 -0800

    uncomment patch section

--- storage/control/apiv2/internal/runner.sh
+++ storage/control/apiv2/internal/runner.sh
@@ -2,10 +2,10 @@ set -ex
 # git clone https://github.com/googleapis/googleapis.git ../../../../.googleapis
 git checkout main ../storage_control_client.go
 pushd ../../../../
-# git -C .googleapis clean -f -d -x
-# git -C .googleapis restore .
-# git -C .googleapis pull
-# env -C .googleapis/ patch -p1 < storage/control/apiv2/internal/merge_storage_v2.patch
+git -C .googleapis clean -f -d -x
+git -C .googleapis restore .
+git -C .googleapis pull
+env -C .googleapis/ patch -p1 < storage/control/apiv2/internal/merge_storage_v2.patch
 env -C .googleapis/ bazelisk fetch //google/storage/control/v2:control_go_proto //google/storage/control/v2:control_go_gapic //google/storage/control/v2:gapi-cloud-storage-control-v2-go
 env -C .googleapis/ bazelisk build //google/storage/control/v2:control_go_proto //google/storage/control/v2:control_go_gapic //google/storage/control/v2:gapi-cloud-storage-control-v2-go
 bazel_output=$(env -C .googleapis/ bazelisk info output_path)
