3d3a43028a92a78b00f5e140d83e68e24f445132
Madison Hall <madisonhall38@gmail.com>
2025-03-17 11:31:19 -0400

    Add OwnerEntity to BucketAttrs

--- storage/bucket_test.go
+++ storage/bucket_test.go
@@ -70,6 +70,7 @@ func TestBucketAttrsToRawBucket(t *testing.T) {
 		Autoclass:             &Autoclass{Enabled: true, TerminalStorageClass: "NEARLINE"},
 		SoftDeletePolicy:      &SoftDeletePolicy{RetentionDuration: time.Hour},
 		HierarchicalNamespace: &HierarchicalNamespace{Enabled: true},
+		// OwnerEntity:           storagepb.Owner{Entity: "project-owner-projectId"}.Entity,
 		Lifecycle: Lifecycle{
 			Rules: []LifecycleRule{{
 				Action: LifecycleAction{
