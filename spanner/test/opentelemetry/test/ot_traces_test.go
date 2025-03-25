47eb9b09f32b2d13a6a6188cd06e9f34f5fd6913
Emmanuel T Odeke <emmanuel@orijtech.com>
2025-03-25 21:19:18 +0300

    chore(spanner): add x-goog-spanner-request-id value to associated span as attribute
    This change ensures that the associated the span in the
    context invoking the RPC to Cloud Spanner, contains the attribute
    "x_goog_spanner_request_id" containing our value, to ensure continuity
    and more enhanced debugging.

--- spanner/test/opentelemetry/test/ot_traces_test.go
+++ spanner/test/opentelemetry/test/ot_traces_test.go
@@ -22,10 +22,12 @@ package test
 import (
 	"context"
 	"errors"
+	"regexp"
 	"testing"
 
 	"cloud.google.com/go/spanner"
 	stestutil "cloud.google.com/go/spanner/internal/testutil"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 )
 
@@ -68,6 +70,88 @@ func TestSpannerTracesWithOpenTelemetry(t *testing.T) {
 	spanName := "cloud.google.com/go/spanner.Query"
 	if !findSpan(spans, spanName) {
 		t.Errorf("Expected span %s not found", spanName)
+	}
+}
+
+func TestSpanAnnotatedWithXGoogSpannerRequestID_unary(t *testing.T) {
+	testSpanAnnotatedWithXGoogSpannerRequestID(t, "cloud.google.com/go/spanner.BatchCreateSessions")
+}
+
+func TestSpanAnnotatedWithXGoogSpannerRequestID_streaming(t *testing.T) {
+	testSpanAnnotatedWithXGoogSpannerRequestID(t, "cloud.google.com/go/spanner.RowIterator")
+}
+
+func testSpanAnnotatedWithXGoogSpannerRequestID(t *testing.T, targetSpanName string) {
+	ctx := context.Background()
+	te := newOpenTelemetryTestExporter(false, true)
+
+	t.Cleanup(func() {
+		te.Unregister(ctx)
+	})
+
+	minOpened := uint64(1)
+	server, client, teardown := setupMockedTestServerWithConfig(t, spanner.ClientConfig{
+		SessionPoolConfig: spanner.SessionPoolConfig{
+			MinOpened: minOpened,
+		},
+	})
+	defer teardown()
+
+	waitFor(t, func() error {
+		if isMultiplexEnabled {
+			if uint64(server.TestSpanner.TotalSessionsCreated()) == minOpened+1 {
+				return nil
+			}
+		}
+		if uint64(server.TestSpanner.TotalSessionsCreated()) == minOpened {
+			return nil
+		}
+		return errors.New("not yet initialized")
+	})
+
+	iter := client.Single().Query(context.Background(), spanner.NewStatement(stestutil.SelectSingerIDAlbumIDAlbumTitleFromAlbums))
+	iter.Do(func(r *spanner.Row) error {
+		return nil
+	})
+	spans := te.spans()
+	if len(spans) == 0 {
+		t.Fatal("No spans were exported")
+	}
+
+	// Find the unary span "/BatchCreateSession".
+	var targetSpan tracetest.SpanStub
+	found := false
+	for _, span := range spans {
+		if span.Name == targetSpanName {
+			targetSpan = span
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Fatalf("Could not find target span with name: %q", targetSpanName)
+	}
+
+	var headerKey attribute.Key = "x_goog_spanner_request_id"
+	ourHeaderFound := false
+	foundValue := ""
+	for _, attrKV := range targetSpan.Attributes {
+		if attrKV.Key == headerKey {
+			foundValue = attrKV.Value.AsString()
+			ourHeaderFound = foundValue != ""
+			break
+		}
+	}
+
+	if !ourHeaderFound {
+		t.Fatalf("did not find our header: %q", headerKey)
+	}
+
+	// It must match the desired format of x-goog-spanner-request-id.
+	var reg = regexp.MustCompile("\\d+\\.([a-z0-9]{16})(\\.\\d+){4}")
+	if !reg.MatchString(foundValue) {
+		t.Fatalf("Regex=%q did not match %q", reg, foundValue)
 	}
 }
 
