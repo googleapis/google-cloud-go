7a9aa9ec939c1fc11beb6ec63b290bdaad84dded
Baha Aiman <bahaaiman@google.com>
2025-03-31 19:19:35 +0000

    remove debug statement

--- bigtable/bigtable.go
+++ bigtable/bigtable.go
@@ -45,7 +45,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -808,8 +807,6 @@ func (bs *BoundStatement) execute(ctx context.Context, f func(ResultRow) bool, m
 					// first ResumeToken received
 					finalizedStmt = candFinalizedStmt
 					finalizedStmt.initializeMetadataAndMap()
-					bytes, _ := protojson.MarshalOptions{AllowPartial: true, UseEnumNumbers: true, Multiline: true}.Marshal(finalizedStmt.metadata)
-					fmt.Println("finalizedStmt.metadata: " + string(bytes))
 					receivedResumeToken = true
 				}
 
