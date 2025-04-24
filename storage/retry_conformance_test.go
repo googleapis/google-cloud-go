4b6a430364d237ba445451a03862855637464e89
Chris Cotter <cjcotter@google.com>
2025-04-24 20:09:29 +0000

    test(storage): re-enable download in conf test
    Re-enable this check and remove TODO due to fix in
    testbench (googleapis/storage-testbench#722)

--- storage/retry_conformance_test.go
+++ storage/retry_conformance_test.go
@@ -654,9 +654,7 @@ var methods = map[string][]retryFunc{
 			}
 
 			// Don't reuse obj, in case preconditions were set on the write request.
-			// TODO: switch to using NewReader instead of NewRangeReader once emulator
-			// issue with CRC32C for appendable objects is fixed.
-			r, err := b.Object(obj.ObjectName()).NewRangeReader(ctx, 0, 3*MiB)
+			r, err := b.Object(obj.ObjectName()).NewReader(ctx)
 			defer r.Close()
 			if err != nil {
 				return fmt.Errorf("obj.NewReader: %v", err)
