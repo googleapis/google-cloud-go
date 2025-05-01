55343b393cf6948ee10242760ccb7310eaaa73a6
Marc Dougherty <muncus@users.noreply.github.com>
2025-05-01 08:16:45 -0700

    docs(vertexai): steer new users toward google.golang.org/genai

---
+++ vertexai/genai/doc.go
@@ -0,0 +1,5 @@
+// Package genai is a client for the generative VertexAI model.
+//
+// New users are encouraged to use the Google GenAI Go SDK available at
+// [google.golang.org/genai]
+package genai
