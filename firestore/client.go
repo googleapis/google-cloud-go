eb6b2a36666ecb7e20e306aecd4c52b1ac61b0a5
Baha Aiman <bahaaiman@google.com>
2025-05-21 08:52:04 +0000

    rename

--- firestore/client.go
+++ firestore/client.go
@@ -184,37 +184,37 @@ func (c *Client) Doc(path string) *DocumentRef {
 	return doc
 }
 
-// DocFromFullPath creates a reference to a document from its full resource name.
-// The resource name must be in the format:
+// DocFromFullPath creates a reference to a document from its full, absolute path,
+// also known as its Google Cloud resource name.
+// The path must be in the format:
 // "projects/{projectID}/databases/{databaseID}/documents/{collectionID}/{documentID}/..."
-//
 // This method returns nil if:
-//   - The resourceName is empty.
-//   - The resourceName does not match the expected resource name format (e.g., missing "projects/" or "/documents/").
-//   - The projectID or databaseID in the resourceName do not match the client's configuration.
-//   - The resourceName refers to a collection instead of a document (i.e., has an odd number of segments after "/documents/").
-//   - The resourceName contains any empty path segments.
-func (c *Client) DocFromFullPath(resourceName string) *DocumentRef {
-	if resourceName == "" {
+//   - The fullPath is empty.
+//   - The fullPath does not match the expected resource name format (e.g., missing "projects/" or "/documents/").
+//   - The projectID or databaseID in the fullPath do not match the client's configuration.
+//   - The fullPath refers to a collection instead of a document (i.e., has an odd number of segments after "/documents/").
+//   - The fullPath contains any empty path segments.
+func (c *Client) DocFromFullPath(fullPath string) *DocumentRef {
+	if fullPath == "" {
 		return nil
 	}
 
 	const documentsPrefix = "/documents/"
-	if !strings.HasPrefix(resourceName, "projects/") || !strings.Contains(resourceName, documentsPrefix) {
+	if !strings.HasPrefix(fullPath, "projects/") || !strings.Contains(fullPath, documentsPrefix) {
 		return nil
 	}
-	parts := strings.SplitN(resourceName, documentsPrefix, 2)
+	parts := strings.SplitN(fullPath, documentsPrefix, 2)
 	if len(parts) != 2 {
 		return nil
 	}
 
-	actualDBPathFromResourceName := parts[0]
+	actualDBPathFromFullPath := parts[0]
 	expectedDBPath := c.path()
-	if actualDBPathFromResourceName != expectedDBPath {
+	if actualDBPathFromFullPath != expectedDBPath {
 		return nil
 	}
 
-	_, docRef := c.idsToRef(strings.Split(parts[1], "/"), actualDBPathFromResourceName)
+	_, docRef := c.idsToRef(strings.Split(parts[1], "/"), actualDBPathFromFullPath)
 	return docRef
 }
 
