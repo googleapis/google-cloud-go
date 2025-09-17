509b2221c5bf76d2ad33355cff563843a10fe1fb
rahul yadav <rahulyadavsep92@gmail.com>
2025-09-17 23:49:56 +0530

    test(spanner): increase context timeout to allow one executeSql to be executed

--- spanner/transaction_test.go
+++ spanner/transaction_test.go
@@ -2191,7 +2191,7 @@ func TestReadWriteTransactionUsesNewContextForRollback(t *testing.T) {
 	})
 	defer teardown()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	defer cancel()
 
 	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, transaction *ReadWriteTransaction) error {
