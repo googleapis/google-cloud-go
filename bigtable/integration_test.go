fdcdde809db710633e3102440d11b3365bdd1fd4
Sushan Bhattarai <sushantsusan@google.com>
2025-09-26 12:21:10 -0400

    feat(bigtable): add an e2e flow for direct access with instructions (#12939)
    This function attempts to establish a connection to the Bigtable
    instance using
    settings that force the use of DirectAccess. It then checks if the
    underlying
    gRPC connection is indeed using a DirectPath IP address.

--- bigtable/integration_test.go
+++ bigtable/integration_test.go
@@ -58,8 +58,6 @@ import (
 )
 
 const (
-	directPathIPV6Prefix      = "[2001:4860:8040"
-	directPathIPV4Prefix      = "34.126"
 	timeUntilResourceCleanup  = time.Hour * 12 // 12 hours
 	prefixOfInstanceResources = "bt-it-"
 	prefixOfClusterResources  = "bt-c-"
