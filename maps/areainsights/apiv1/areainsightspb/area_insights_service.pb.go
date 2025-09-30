4c8438c7347d1b8f880bb5d2aec161eab10c957b
Chris Smith <chrisdsmith@google.com>
2025-09-30 10:01:03 -0600

    chore(maps): migrate maps to Librarian
    * Includes repo-config.yaml entries for special cases.
    
    refs: googleapis/librarian#888

--- maps/areainsights/apiv1/areainsightspb/area_insights_service.pb.go
+++ maps/areainsights/apiv1/areainsightspb/area_insights_service.pb.go
@@ -22,6 +22,9 @@ package areainsightspb
 
 import (
 	context "context"
+	reflect "reflect"
+	sync "sync"
+
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	latlng "google.golang.org/genproto/googleapis/type/latlng"
 	grpc "google.golang.org/grpc"
@@ -29,8 +32,6 @@ import (
 	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
--- maps/places/apiv1/placespb/review.pb.go
+++ maps/places/apiv1/placespb/review.pb.go
@@ -21,14 +21,15 @@
 package placespb
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "google.golang.org/genproto/googleapis/type/date"
 	localized_text "google.golang.org/genproto/googleapis/type/localized_text"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
