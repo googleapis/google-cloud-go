a33f60406fa55825605da460a7484401ef8db86a
Chris Smith <chrisdsmith@google.com>
2025-09-30 10:27:27 -0600

    chore: migrate alloydb apigateway apigeeconnect apihub to Librarian
    refs: googleapis/librarian#888

--- alloydb/apiv1alpha/alloydbpb/service.pb.go
+++ alloydb/apiv1alpha/alloydbpb/service.pb.go
@@ -21,6 +21,9 @@
 package alloydbpb
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	longrunningpb "cloud.google.com/go/longrunning/autogen/longrunningpb"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	status "google.golang.org/genproto/googleapis/rpc/status"
@@ -30,8 +33,6 @@ import (
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	fieldmaskpb "google.golang.org/protobuf/types/known/fieldmaskpb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
