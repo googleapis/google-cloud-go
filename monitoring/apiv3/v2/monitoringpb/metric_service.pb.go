a4960ef37cba56177aec24dce393eba7925bb73c
Jon Skeet <jonskeet@google.com>
2025-10-15 19:30:37 +0100

    chore: migrate 8 modules (cloudbuild - vmmigration) to librarian (#13149)
    - cloudbuild
    - datacatalog
    - datastream
    - dialogflow
    - iam
    - monitoring
    - run
    - vmmigration

--- monitoring/apiv3/v2/monitoringpb/metric_service.pb.go
+++ monitoring/apiv3/v2/monitoringpb/metric_service.pb.go
@@ -21,6 +21,9 @@
 package monitoringpb
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	metric "google.golang.org/genproto/googleapis/api/metric"
 	monitoredres "google.golang.org/genproto/googleapis/api/monitoredres"
@@ -28,8 +31,6 @@ import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
