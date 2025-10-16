75d7341b74f28a5a02723ffe309488637b15ab4e
Jon Skeet <jonskeet@google.com>
2025-10-16 14:48:03 +0000

    chore: prepare code for migration

--- aiplatform/apiv1/aiplatformpb/genai_tuning_service.pb.go
+++ aiplatform/apiv1/aiplatformpb/genai_tuning_service.pb.go
@@ -21,13 +21,14 @@
 package aiplatformpb
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	longrunningpb "cloud.google.com/go/longrunning/autogen/longrunningpb"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
