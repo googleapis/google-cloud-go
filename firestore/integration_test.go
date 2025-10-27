9b67878767b3e258f8b93186a5ea8d3c220b678b
Baha Aiman <bahaaiman@google.com>
2025-10-27 05:25:39 +0000

    feat(firestore): [PQ] Add all Aggregation and Timestamp functions

--- firestore/integration_test.go
+++ firestore/integration_test.go
@@ -3565,3 +3565,511 @@ func TestIntegration_FindNearest(t *testing.T) {
 		})
 	}
 }
+
+func TestIntegration_PipelineFunctions(t *testing.T) {
+	if testParams[firestoreEditionKey].(firestoreEdition) != editionEnterprise {
+		t.Skip("Skipping pipeline queries tests since the firestore edition of", testParams[databaseIDKey].(string), "database is not enterprise")
+	}
+	t.Run("timestampFuncs", timestampFuncs)
+	t.Run("arithmeticFuncs", arithmeticFuncs)
+	t.Run("aggregateFuncs", aggregateFuncs)
+	t.Run("comparisonFuncs", comparisonFuncs)
+}
+
+func timestampFuncs(t *testing.T) {
+	t.Parallel()
+	client := integrationClient(t)
+	coll := client.Collection(collectionIDs.New())
+	h := testHelper{t}
+	now := time.Now()
+	docRef1 := coll.NewDoc()
+	h.mustCreate(docRef1, map[string]interface{}{
+		"timestamp":   now,
+		"unixMicros":  now.UnixNano() / 1000,
+		"unixMillis":  now.UnixNano() / 1e6,
+		"unixSeconds": now.Unix(),
+	})
+	defer deleteDocuments([]*DocumentRef{docRef1})
+
+	tests := []struct {
+		name     string
+		pipeline *Pipeline
+		want     map[string]interface{}
+	}{
+		{
+			name: "TimestampAdd day",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(TimestampAdd("timestamp", "day", 1).As("timestamp_plus_day")),
+			want: map[string]interface{}{"timestamp_plus_day": now.AddDate(0, 0, 1).Truncate(time.Microsecond)},
+		},
+		{
+			name: "TimestampAdd hour",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(TimestampAdd("timestamp", "hour", 1).As("timestamp_plus_hour")),
+			want: map[string]interface{}{"timestamp_plus_hour": now.Add(time.Hour).Truncate(time.Microsecond)},
+		},
+		{
+			name: "TimestampAdd minute",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(TimestampAdd("timestamp", "minute", 1).As("timestamp_plus_minute")),
+			want: map[string]interface{}{"timestamp_plus_minute": now.Add(time.Minute).Truncate(time.Microsecond)},
+		},
+		{
+			name: "TimestampAdd second",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(TimestampAdd("timestamp", "second", 1).As("timestamp_plus_second")),
+			want: map[string]interface{}{"timestamp_plus_second": now.Add(time.Second).Truncate(time.Microsecond)},
+		},
+		{
+			name: "TimestampSubtract",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(TimestampSubtract("timestamp", "hour", 1).As("timestamp_minus_hour")),
+			want: map[string]interface{}{"timestamp_minus_hour": now.Add(-time.Hour).Truncate(time.Microsecond)},
+		},
+		{
+			name: "TimestampToUnixMicros",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(FieldOf("timestamp").TimestampToUnixMicros().As("timestamp_micros")),
+			want: map[string]interface{}{"timestamp_micros": now.UnixNano() / 1000},
+		},
+		{
+			name: "TimestampToUnixMillis",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(FieldOf("timestamp").TimestampToUnixMillis().As("timestamp_millis")),
+			want: map[string]interface{}{"timestamp_millis": now.UnixNano() / 1e6},
+		},
+		{
+			name: "TimestampToUnixSeconds",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(FieldOf("timestamp").TimestampToUnixSeconds().As("timestamp_seconds")),
+			want: map[string]interface{}{"timestamp_seconds": now.Unix()},
+		},
+		{
+			name: "UnixMicrosToTimestamp - constant",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(UnixMicrosToTimestamp(ConstantOf(now.UnixNano() / 1000)).As("timestamp_from_micros")),
+			want: map[string]interface{}{"timestamp_from_micros": now.Truncate(time.Microsecond)},
+		},
+		{
+			name: "UnixMicrosToTimestamp - fieldname",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(UnixMicrosToTimestamp("unixMicros").As("timestamp_from_micros")),
+			want: map[string]interface{}{"timestamp_from_micros": now.Truncate(time.Microsecond)},
+		},
+		{
+			name: "UnixMillisToTimestamp",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(UnixMillisToTimestamp(ConstantOf(now.UnixNano() / 1e6)).As("timestamp_from_millis")),
+			want: map[string]interface{}{"timestamp_from_millis": now.Truncate(time.Millisecond)},
+		},
+		{
+			name: "UnixSecondsToTimestamp",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(UnixSecondsToTimestamp("unixSeconds").As("timestamp_from_seconds")),
+			want: map[string]interface{}{"timestamp_from_seconds": now.Truncate(time.Second)},
+		},
+		{
+			name: "CurrentTimestamp",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Select(CurrentTimestamp().As("current_timestamp")),
+			want: map[string]interface{}{"current_timestamp": time.Now().Truncate(time.Microsecond)},
+		},
+	}
+
+	ctx := context.Background()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			iter := test.pipeline.Execute(ctx)
+			defer iter.Stop()
+
+			docs, err := iter.GetAll()
+			if err != nil {
+				t.Fatalf("GetAll: %v", err)
+			}
+			if len(docs) != 1 {
+				t.Fatalf("expected 1 doc, got %d", len(docs))
+			}
+			got, err := docs[0].Data()
+			if err != nil {
+				t.Fatalf("Data: %v", err)
+			}
+
+			margin := 0 * time.Microsecond
+			if test.name == "CurrentTimestamp" {
+				margin = 5 * time.Second
+			}
+			if diff := testutil.Diff(got, test.want, cmpopts.EquateApproxTime(margin)); diff != "" {
+				t.Errorf("got: %v, want: %v, diff: %s", got, test.want, diff)
+			}
+		})
+	}
+}
+
+func arithmeticFuncs(t *testing.T) {
+	t.Parallel()
+	h := testHelper{t}
+	client := integrationClient(t)
+	coll := client.Collection(collectionIDs.New())
+	docRef1 := coll.NewDoc()
+	h.mustCreate(docRef1, map[string]interface{}{
+		"a": int(1),
+		"b": int(2),
+		"c": -3,
+		"d": 4.5,
+		"e": -5.5,
+	})
+	defer deleteDocuments([]*DocumentRef{docRef1})
+
+	tests := []struct {
+		name     string
+		pipeline *Pipeline
+		want     map[string]interface{}
+	}{
+		{
+			name:     "Add - left FieldOf, right FieldOf",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add(FieldOf("a"), FieldOf("b")).As("add")),
+			want:     map[string]interface{}{"add": int64(3)},
+		},
+		{
+			name:     "Add - left FieldOf, right ConstantOf",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add(FieldOf("a"), ConstantOf(2)).As("add")),
+			want:     map[string]interface{}{"add": int64(3)},
+		},
+		{
+			name:     "Add - left FieldOf, right constant",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add(FieldOf("a"), 5).As("add")),
+			want:     map[string]interface{}{"add": int64(6)},
+		},
+		{
+			name:     "Add - left fieldname, right constant",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add("a", 5).As("add")),
+			want:     map[string]interface{}{"add": int64(6)},
+		},
+		{
+			name:     "Add - left fieldpath, right constant",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add(FieldPath([]string{"a"}), 5).As("add")),
+			want:     map[string]interface{}{"add": int64(6)},
+		},
+		{
+			name:     "Add - left fieldpath, right expression",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Add(FieldPath([]string{"a"}), Add(FieldOf("b"), FieldOf("d"))).As("add")),
+			want:     map[string]interface{}{"add": float64(7.5)},
+		},
+		{
+			name:     "Subtract",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Subtract("a", FieldOf("b")).As("subtract")),
+			want:     map[string]interface{}{"subtract": int64(-1)},
+		},
+		{
+			name:     "Multiply",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Multiply("a", 5).As("multiply")),
+			want:     map[string]interface{}{"multiply": int64(5)},
+		},
+		{
+			name:     "Divide",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Divide("a", FieldOf("d")).As("divide")),
+			want:     map[string]interface{}{"divide": float64(1 / 4.5)},
+		},
+		{
+			name:     "Mod",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Mod("a", FieldOf("b")).As("mod")),
+			want:     map[string]interface{}{"mod": int64(1)},
+		},
+		{
+			name:     "Pow",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Pow("a", FieldOf("b")).As("pow")),
+			want:     map[string]interface{}{"pow": float64(1)},
+		},
+		{
+			name:     "Abs - fieldname",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Abs("c").As("abs")),
+			want:     map[string]interface{}{"abs": int64(3)},
+		},
+		{
+			name:     "Abs - fieldPath",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Abs(FieldPath([]string{"c"})).As("abs")),
+			want:     map[string]interface{}{"abs": int64(3)},
+		},
+		{
+			name:     "Abs - Expr",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Abs(Add(FieldOf("b"), FieldOf("d"))).As("abs")),
+			want:     map[string]interface{}{"abs": float64(6.5)},
+		},
+		{
+			name:     "Ceil",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Ceil("d").As("ceil")),
+			want:     map[string]interface{}{"ceil": float64(5)},
+		},
+		{
+			name:     "Floor",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Floor("d").As("floor")),
+			want:     map[string]interface{}{"floor": float64(4)},
+		},
+		{
+			name:     "Round",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Round("d").As("round")),
+			want:     map[string]interface{}{"round": float64(5)},
+		},
+		{
+			name:     "Sqrt",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Sqrt("d").As("sqrt")),
+			want:     map[string]interface{}{"sqrt": math.Sqrt(4.5)},
+		},
+		{
+			name:     "Log",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Log("d", 2).As("log")),
+			want:     map[string]interface{}{"log": math.Log2(4.5)},
+		},
+		{
+			name:     "Log10",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Log10("d").As("log10")),
+			want:     map[string]interface{}{"log10": math.Log10(4.5)},
+		},
+		{
+			name:     "Ln",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Ln("d").As("ln")),
+			want:     map[string]interface{}{"ln": math.Log(4.5)},
+		},
+		{
+			name:     "Exp",
+			pipeline: client.Pipeline().Collection(coll.ID).Select(Exp("d").As("exp")),
+			want:     map[string]interface{}{"exp": math.Exp(4.5)},
+		},
+	}
+
+	ctx := context.Background()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			iter := test.pipeline.Execute(ctx)
+			defer iter.Stop()
+
+			docs, err := iter.GetAll()
+			if err != nil {
+				t.Fatalf("GetAll: %v", err)
+			}
+			if len(docs) != 1 {
+				t.Fatalf("expected 1 doc, got %d", len(docs))
+			}
+			got, err := docs[0].Data()
+			if err != nil {
+				t.Fatalf("Data: %v", err)
+			}
+			if diff := testutil.Diff(got, test.want); diff != "" {
+				t.Errorf("got: %v, want: %v, diff +want -got: %s", got, test.want, diff)
+			}
+		})
+	}
+}
+
+func aggregateFuncs(t *testing.T) {
+	t.Parallel()
+	h := testHelper{t}
+	client := integrationClient(t)
+	coll := client.Collection(collectionIDs.New())
+	docRef1 := coll.NewDoc()
+	h.mustCreate(docRef1, map[string]interface{}{
+		"a": 1,
+	})
+	docRef2 := coll.NewDoc()
+	h.mustCreate(docRef2, map[string]interface{}{
+		"a": 2,
+	})
+	docRef3 := coll.NewDoc()
+	h.mustCreate(docRef3, map[string]interface{}{
+		"b": 2,
+	})
+	defer deleteDocuments([]*DocumentRef{docRef1, docRef2, docRef3})
+
+	tests := []struct {
+		name     string
+		pipeline *Pipeline
+		want     map[string]interface{}
+	}{
+		{
+			name: "Sum - fieldname arg",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Sum("a").As("sum_a")),
+			want: map[string]interface{}{"sum_a": int64(3)},
+		},
+		{
+			name: "Sum - fieldpath arg",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Sum(FieldPath([]string{"a"})).As("sum_a")),
+			want: map[string]interface{}{"sum_a": int64(3)},
+		},
+		{
+			name: "Sum - FieldOf Expr",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Sum(FieldOf("a")).As("sum_a")),
+			want: map[string]interface{}{"sum_a": int64(3)},
+		},
+		{
+			name: "Sum - FieldOfPath Expr",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Sum(FieldOfPath(FieldPath([]string{"a"}))).As("sum_a")),
+			want: map[string]interface{}{"sum_a": int64(3)},
+		},
+		{
+			name: "Avg",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Average("a").As("avg_a")),
+			want: map[string]interface{}{"avg_a": float64(1.5)},
+		},
+		{
+			name: "Count",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(Count("a").As("count_a")),
+			want: map[string]interface{}{"count_a": int64(2)},
+		},
+		{
+			name: "CountAll",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Aggregate(CountAll().As("count_all")),
+			want: map[string]interface{}{"count_all": int64(3)},
+		},
+	}
+
+	ctx := context.Background()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			iter := test.pipeline.Execute(ctx)
+			defer iter.Stop()
+
+			docs, err := iter.GetAll()
+			if err != nil {
+				t.Fatalf("GetAll: %v", err)
+			}
+			if len(docs) != 1 {
+				t.Fatalf("expected 1 doc, got %d", len(docs))
+			}
+			got, err := docs[0].Data()
+			if err != nil {
+				t.Fatalf("Data: %v", err)
+			}
+			if diff := testutil.Diff(got, test.want); diff != "" {
+				t.Errorf("got: %v, want: %v, diff +want -got: %s", got, test.want, diff)
+			}
+		})
+	}
+}
+
+func comparisonFuncs(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	client := integrationClient(t)
+	now := time.Now()
+	coll := client.Collection(collectionIDs.New())
+	doc1data := map[string]interface{}{
+		"timestamp": now,
+		"a":         1,
+		"b":         2,
+		"c":         -3,
+		"d":         4.5,
+		"e":         -5.5,
+	}
+	_, err := coll.Doc("doc1").Create(ctx, doc1data)
+	if err != nil {
+		t.Fatalf("Create: %v", err)
+	}
+	doc2data := map[string]interface{}{
+		"timestamp": now,
+		"a":         2,
+		"b":         2,
+		"c":         -3,
+		"d":         4.5,
+		"e":         -5.5,
+	}
+	_, err = coll.Doc("doc2").Create(ctx, doc2data)
+	if err != nil {
+		t.Fatalf("Create: %v", err)
+	}
+	defer deleteDocuments([]*DocumentRef{coll.Doc("doc1"), coll.Doc("doc2")})
+
+	doc1want := map[string]interface{}{"a": int64(1), "b": int64(2), "c": int64(-3), "d": float64(4.5), "e": float64(-5.5), "timestamp": now.Truncate(time.Microsecond)}
+
+	tests := []struct {
+		name     string
+		pipeline *Pipeline
+		want     []map[string]interface{}
+	}{
+		{
+			name: "Equal",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Where(Equal("a", 1)),
+			want: []map[string]interface{}{doc1want},
+		},
+		{
+			name: "NotEqual",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Where(NotEqual("a", 2)),
+			want: []map[string]interface{}{doc1want},
+		},
+		{
+			name: "LessThan",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Where(LessThan("a", 2)),
+			want: []map[string]interface{}{doc1want},
+		},
+		{
+			name: "Equivalent",
+			pipeline: client.Pipeline().
+				Collection(coll.ID).
+				Where(Equivalent("a", 1)),
+			want: []map[string]interface{}{doc1want},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			iter := test.pipeline.Execute(ctx)
+			defer iter.Stop()
+
+			docs, err := iter.GetAll()
+			if err != nil {
+				t.Fatalf("GetAll: %v", err)
+			}
+			if len(docs) != len(test.want) {
+				t.Fatalf("expected %d doc(s), got %d", len(test.want), len(docs))
+			}
+
+			var gots []map[string]interface{}
+			for _, doc := range docs {
+				got, err := doc.Data()
+				if err != nil {
+					t.Fatalf("Data: %v", err)
+				}
+				if ts, ok := got["timestamp"].(time.Time); ok {
+					got["timestamp"] = ts.Truncate(time.Microsecond)
+				}
+				gots = append(gots, got)
+			}
+
+			if diff := testutil.Diff(gots, test.want); diff != "" {
+				t.Errorf("got: %v, want: %v, diff +want -got: %s", gots, test.want, diff)
+			}
+		})
+	}
+}
