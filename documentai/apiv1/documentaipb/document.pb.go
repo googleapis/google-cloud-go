d58b8fcf18502486156afc0da348e60079864c87
Cloud SDK Librarian <cloud-sdk-librarian-robot@google.com>
2026-01-28 08:04:10 +0000

    feat: generate libraries

--- documentai/apiv1/documentaipb/document.pb.go
+++ documentai/apiv1/documentaipb/document.pb.go
@@ -510,10 +510,10 @@ type Document struct {
 	// document in the processing order. This field can be used for comparing the
 	// entity extraction results at different stages of the processing.
 	EntitiesRevisions []*Document_EntitiesRevision `protobuf:"bytes,22,rep,name=entities_revisions,json=entitiesRevisions,proto3" json:"entities_revisions,omitempty"`
-	// The entity revision id that `document.entities` field is based on.
+	// The entity revision ID that `document.entities` field is based on.
 	// If this field is set and `entities_revisions` is not empty, the entities in
 	// `document.entities` field are the entities in the entity revision with this
-	// id and `document.entity_validation_output` field is the
+	// ID and `document.entity_validation_output` field is the
 	// `entity_validation_output` field in this entity revision.
 	EntitiesRevisionId string `protobuf:"bytes,23,opt,name=entities_revision_id,json=entitiesRevisionId,proto3" json:"entities_revision_id,omitempty"`
 }
