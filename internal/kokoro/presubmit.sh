b89e10485e902fb0e01e840fe7dc2cf4d82b8039
Noah Dietz <ndietz@google.com>
2026-02-04 23:48:01 +0000

    fetch the local remote, remote preview

--- internal/kokoro/presubmit.sh
+++ internal/kokoro/presubmit.sh
@@ -40,7 +40,7 @@ cd $GOCLOUD_HOME
 # (main) which means we need to explicitly fetch preview if evaluating a pull
 # request against it so that git diff functions appropriately.
 if [[ $KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH_google_cloud_go == "preview" ]]; then
-  git fetch origin preview
+  git fetch origin origin/preview
 fi
 
 try3() { eval "$*" || eval "$*" || eval "$*"; }
