d786e299ae5e6022d4232d1b7c3ef853e7d12421
Seth Hollyman <shollyman@google.com>
2026-02-06 21:27:36 +0000

    feat: update image to us-central1-docker.pkg.dev/cloud-sdk-librarian-prod/images-prod/librarian-go@sha256:19bb93e8f1f916c61b597db2bad65dc432f79baaabb210499d7d0e4ad1dffe29

--- bigquery/v2/apiv2/job_client.go
+++ bigquery/v2/apiv2/job_client.go
@@ -62,7 +62,6 @@ func defaultJobGRPCClientOptions() []option.ClientOption {
 		internaloption.WithDefaultAudience("https://bigquery.googleapis.com/"),
 		internaloption.WithDefaultScopes(DefaultAuthScopes()...),
 		internaloption.EnableJwtWithScope(),
-		internaloption.AllowHardBoundTokens("MTLS_S2A"),
 		internaloption.EnableNewAuthLibrary(),
 		option.WithGRPCDialOption(grpc.WithDefaultCallOptions(
 			grpc.MaxCallRecvMsgSize(math.MaxInt32))),
--- bigquery/v2/apiv2/table_client.go
+++ bigquery/v2/apiv2/table_client.go
@@ -61,7 +61,6 @@ func defaultTableGRPCClientOptions() []option.ClientOption {
 		internaloption.WithDefaultAudience("https://bigquery.googleapis.com/"),
 		internaloption.WithDefaultScopes(DefaultAuthScopes()...),
 		internaloption.EnableJwtWithScope(),
-		internaloption.AllowHardBoundTokens("MTLS_S2A"),
 		internaloption.EnableNewAuthLibrary(),
 		option.WithGRPCDialOption(grpc.WithDefaultCallOptions(
 			grpc.MaxCallRecvMsgSize(math.MaxInt32))),
