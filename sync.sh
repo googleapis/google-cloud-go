0fb68fbc4791ad4c577074fb875512c4f302c427
Noah Dietz <ndietz@google.com>
2026-02-11 22:32:13 +0000

    chore(preview): add sync.sh

---
+++ sync.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Script to facilitate syncrhonization from the main to the preview branch.
+
+git checkout main -- \
+    .gemini \
+    .github/workflows/action_syntax.yml \
+    .github/workflows/vet.yml \
+    .github/workflows/vet.sh \
+    .github/CODEOWNERS \
+    .github/header-checker-lint.yml \
+    internal/godocfx \
+    internal/kokoro \
+    internal/testutil \
+    internal/uid \
+    internal/README.md \
+    internal/version.go \
+    third_party \
+    .gitignore \
+    CODE_OF_CONDUCT.md \
+    CONTRIBUTING.md \
+    debug.md \
+    doc.go \
+    LICENSE \
+    README.md \
+    SECURITY.md
