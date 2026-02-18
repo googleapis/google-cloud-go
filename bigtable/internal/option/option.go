e90abf1a472b6972ba138dd180ed59ca904bacb9
Sushan Bhattarai <sushantsusan@google.com>
2026-02-17 23:28:56 -0500

    feat(bigtable): add connection recycler which recycles before max conn age is reached

--- bigtable/internal/option/option.go
+++ bigtable/internal/option/option.go
@@ -271,3 +271,27 @@ func DefaultMetricsReporterConfig() MetricsReporterConfig {
 		ReportingInterval: 1 * time.Minute,
 	}
 }
+
+
+// ConnectionRecycleConfig controls the behavior of the connection recycler.
+type ConnectionRecycleConfig struct {
+	// MaxAge is the base lifespan of a connection.
+	MaxAge time.Duration
+	// Jitter is the random buffer added to MaxAge which can allow for connection to be recycled.
+	Jitter time.Duration
+	// RunFrequency determines how often the recycler checks for expired connections.
+	RunFrequency time.Duration
+	// Enabled determines if the recycler is active.
+	Enabled bool
+}
+
+// DefaultConnectionRecycleConfig returns the default configuration:
+// MaxAge: 45 minutes, Jitter: 5 minutes, RunFrequency: 1 minute, Enabled: true.
+func DefaultConnectionRecycleConfig() ConnectionRecycleConfig {
+	return ConnectionRecycleConfig{
+		MaxAge:       45 * time.Minute,
+		Jitter:       5 * time.Minute,
+		RunFrequency: 1 * time.Minute,
+		Enabled:      true,
+	}
+}
