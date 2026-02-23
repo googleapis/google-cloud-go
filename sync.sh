4c8f1fd5ddeee220d1971baa346b67f275ac0de5
Noah Dietz <ndietz@google.com>
2026-02-23 22:17:01 +0000

    chore(preview): remove extraneous godocfx

--- sync.sh
+++ sync.sh
@@ -9,7 +9,6 @@ git checkout main -- \
     .github/workflows/vet.sh \
     .github/CODEOWNERS \
     .github/header-checker-lint.yml \
-    internal/godocfx \
     internal/kokoro \
     internal/testutil \
     internal/uid \
