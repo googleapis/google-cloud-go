de21ae161c126836670df63071739c988f8ed2b6
Baha Aiman <bahaaiman@google.com>
2026-03-25 17:40:53 +0000

    removing debug logs

--- firestore/pipeline_result.go
+++ firestore/pipeline_result.go
@@ -26,7 +26,6 @@ import (
 	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -269,8 +268,6 @@ func (it *streamPipelineResultIterator) next() (_ *PipelineResult, err error) {
 			}
 		}()
 		req, err := it.p.toExecutePipelineRequest()
-		bytes, _ := protojson.MarshalOptions{AllowPartial: true, UseEnumNumbers: true, Multiline: true}.Marshal(req)
-		fmt.Println(string(bytes))
 		if err != nil {
 			return nil, err
 		}
