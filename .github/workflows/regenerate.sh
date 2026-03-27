75c373c5e0c8463318b30498b0d94ad2043e7231
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-03-27 15:48:30 -0400

    ci: add a workflow

---
+++ .github/workflows/regenerate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ev
+
+echo "==== Install protoc ===="
+curl -fsSL --retry 5 --retry-delay 15 -o /tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v25.7/protoc-25.7-linux-x86_64.zip
+sha256sum -c <(echo 24e2ed32060b7c990d5eb00d642fde04869d7f77c6d443f609353f097799dd42 /tmp/protoc.zip)
+env -C /usr/local unzip -x /tmp/protoc.zip
+protoc --version
+
+echo "==== Regenerate all the code ===="
+version=$(sed -n 's/^version: *//p' /workspace/librarian.yaml)
+go run github.com/googleapis/librarian/cmd/librarian@${version} generate --all
+
+# If there is any difference between the generated code and the
+# committed code that is an error. All the inputs should be pinned,
+# including the generator version and the googleapis SHA.
+git diff --exit-code
