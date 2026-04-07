d2431ee317ed82cb1d6f93be9fbdd74f31d6d8f5
Seth Hollyman <shollyman@google.com>
2026-04-07 16:14:29 +0000

    reduce pr surface to exclude vertexai

--- vertexai/internal/sentencepiece/internal/cmd/dumper/main.go
+++ vertexai/internal/sentencepiece/internal/cmd/dumper/main.go
@@ -20,6 +20,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"unicode"
@@ -42,7 +43,7 @@ func main() {
 		log.Fatal("Need MODELPATH env var to run")
 	}
 
-	b, err := os.ReadFile(modelPath)
+	b, err := ioutil.ReadFile(modelPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -81,7 +82,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		b, err := os.ReadFile(*fEncodeFile)
+		b, err := ioutil.ReadFile(*fEncodeFile)
 		if err != nil {
 			log.Fatal(err)
 		}
